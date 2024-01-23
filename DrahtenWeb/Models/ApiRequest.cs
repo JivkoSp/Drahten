4f4e1964242df1d3bc3250ca2f3bdd5f22fdab84
JivkoSp <atjspasov@gmail.com>
2024-01-23 16:52:01 +0200

    Added ApiRequest.cs for representation of http request in DrahtenWeb/Models/

---
+++ DrahtenWeb/Models/ApiRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace DrahtenWeb.Models
+{
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    public class ApiRequest
+    {
+        public ApiType? ApiType { get; set; }
+        public string? Url { get; set; }
+        public string? Data { get; set; }
+        public string? AccessToken { get; set; }
+    }
+}
