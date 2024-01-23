1293558d04573ce2c9f73685125b9616928b3259
JivkoSp <atjspasov@gmail.com>
2024-01-23 16:26:17 +0200

    Test endpoint added in HomeController with the purpose of testing the UserService.

--- DrahtenWeb/Controllers/HomeController.cs
+++ DrahtenWeb/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using DrahtenWeb.Models;
+using DrahtenWeb.Services;
+using DrahtenWeb.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,33 @@ namespace DrahtenWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserService _userService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserService userService)
         {
             _logger = logger;
+            _userService = userService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        //Testing the api gateway with user service endpoint.
+        //TODO: Remove this endpoint after testing.
+        public async Task SendGet()
+        {
+            var response = await _userService.GetEndpointAsync<string>("");
+
+            if(response != null)
+            {
+                Console.WriteLine($"Response from USER SERVICE: {response}");
+            }
+            else
+            {
+                Console.WriteLine($"Response from USER SERVICE: Null");
+            }
         }
 
         public IActionResult Privacy()
