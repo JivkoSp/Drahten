86cf6b2b981d70f3f4b9cb64661f2a093ad66213
JivkoSp <atjspasov@gmail.com>
2024-01-23 16:46:45 +0200

    Added ResponseDto in DrahtenWeb/Dtos/

---
+++ DrahtenWeb/Dtos/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.Dtos
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = false;
+        public object? Result { get; set; }
+        public string SuccessMessage { get; set; } = string.Empty;
+        public List<string>? ErrorMessages { get; set; }
+    }
+}
