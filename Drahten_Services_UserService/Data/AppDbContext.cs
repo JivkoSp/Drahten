ac901ee4bc0ef478c5d8902aff331fd021bab3fe
JivkoSp <atjspasov@gmail.com>
2024-01-23 22:22:47 +0200

    Added dbcontext - Data/AppDbContext.cs

---
+++ Drahten_Services_UserService/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Drahten_Services_UserService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
