2d62c18548f5abb1ff6e0009ae32b3d7d703923c
JivkoSp <atjspasov@gmail.com>
2024-01-24 23:55:11 +0200

    Added navigation property for ArticleLikes in  Drahten_Services_UserService/Models/Article.cs.

--- Drahten_Services_UserService/Models/Article.cs
+++ Drahten_Services_UserService/Models/Article.cs
@@ -10,6 +10,7 @@
         public int TopicId { get; set; }
         public virtual Topic? Topic { get; set; }
         public virtual ICollection<UserArticle>? UserArticles { get; set; }
-        public virtual HashSet<ArticleComment>? ArticleComments { get; set; }
+        public virtual ICollection<ArticleComment>? ArticleComments { get; set; }
+        public virtual ICollection<ArticleLike>? ArticleLikes { get; set; }
     }
 }
