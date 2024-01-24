526dae7fdd1988aae6f3f517014761798e6b2ee8
JivkoSp <atjspasov@gmail.com>
2024-01-25 00:00:06 +0200

    Changed navigation property in Drahten_Services_UserService/Models/User.cs.

--- Drahten_Services_UserService/Models/User.cs
+++ Drahten_Services_UserService/Models/User.cs
@@ -8,10 +8,8 @@
         //Relationships
         public virtual HashSet<UserTopic>? Topics { get; set; }
         public virtual HashSet<UserArticle>? UserArticles { get; set; }
-        public virtual HashSet<TopicLike>? TopicLikes { get; set; }
         public virtual HashSet<ArticleComment>? ArticleComments { get; set; }
+        public virtual HashSet<ArticleLike>? ArticleLikes { get; set; }
         public virtual HashSet<ContactRequest>? ContactRequests { get; set; }
-        public virtual HashSet<ViewedArticle>? ViewedArticles { get; set; }
-        public virtual HashSet<SearchedTopicData>? SearchedTopicData { get; set; }
     }
 }
