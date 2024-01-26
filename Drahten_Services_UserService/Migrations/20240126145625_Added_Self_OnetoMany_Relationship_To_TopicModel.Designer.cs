5d69efce7206ec6db7e3a3f00d0e05a025733629
JivkoSp <atjspasov@gmail.com>
2024-01-26 17:20:26 +0200

    Added migration for self one-to-many relationship in Drahten_Services_UserService/Models/Topic.cs.

--- Drahten_Services_UserService/Migrations/20240126145625_Added_Self_OnetoMany_Relationship_To_TopicModel.Designer.cs
+++ Drahten_Services_UserService/Migrations/20240126145625_Added_Self_OnetoMany_Relationship_To_TopicModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Drahten_Services_UserService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Drahten_Services_UserService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240126145625_Added_Self_OnetoMany_Relationship_To_TopicModel")]
+    partial class Added_Self_OnetoMany_Relationship_To_TopicModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
