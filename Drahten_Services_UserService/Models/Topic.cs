03aca3875ddf3b232e08ef92a69df32747c8fecc
JivkoSp <atjspasov@gmail.com>
2024-01-26 17:14:48 +0200

    Added self one-to-many relationship in  Drahten_Services_UserService/Models/Topic.cs. The purpose of this relationship is to represent hierarchy of topics.

--- Drahten_Services_UserService/Models/Topic.cs
+++ Drahten_Services_UserService/Models/Topic.cs
@@ -7,6 +7,11 @@
         public string TopicName { get; set; } = string.Empty;
 
         //Relationships
+        //Self relationship [one-to-many], represents hierarchy of topics - START
+        public int? ParentTopicId { get; set; }
+        public virtual Topic? Parent { get; set; }
+        public virtual ICollection<Topic>? Children { get; set; }
+        //Self relationship [one-to-many], represents hierarchy of topics - END
         public virtual HashSet<UserTopic>? Users { get; set; }
         public virtual HashSet<SearchedTopicDataPrivateHist>? SearchedTopicDataPrivateHist { get; set; }
         public virtual HashSet<SearchedTopicDataPublicHist>? SearchedTopicDataPublicHist { get; set; }
