88054b483b00b7e0e53881755ef3dd3f5b9da34d
JivkoSp <atjspasov@gmail.com>
2024-01-28 14:36:58 +0200

    Added dto for the topic entity type and dto for the api response in Drahten_Services_UserService/Dtos/

---
+++ Drahten_Services_UserService/Dtos/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Drahten_Services_UserService.Dtos
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = false;
+        public object? Result { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+    }
+}
