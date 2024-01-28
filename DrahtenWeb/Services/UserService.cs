2bc31443d30d5fa0a8909de8d54d5dde46a86822
JivkoSp <atjspasov@gmail.com>
2024-01-28 14:25:19 +0200

    Removed method from  DrahtenWeb/Services/UserService.cs. Reason: The method was removed from the IUserService interface.

--- DrahtenWeb/Services/UserService.cs
+++ DrahtenWeb/Services/UserService.cs
@@ -10,20 +10,6 @@ namespace DrahtenWeb.Services
            
         }
 
-        //The purpose of this method is to test the api gateway.
-        //TOOD: Must be removed after the testing is done.
-        public async Task<T> GetEndpointAsync<T>(string accessToken)
-        {
-            var response = await SendAsync<T>(new ApiRequest
-            {
-                ApiType = ApiType.GET,
-                Url = "https://localhost:7076/api/user/",
-                AccessToken = accessToken
-            });
-
-            return response;
-        }
-
         public async Task<TEntity> GetUserTopics<TEntity>(string accessToken)
         {
             var response = await SendAsync<TEntity>(new ApiRequest {
