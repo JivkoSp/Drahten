abb8c7a8692757fd02421c2c47e554a87b416e10
JivkoSp <atjspasov@gmail.com>
2024-01-30 11:28:35 +0200

    Changed the Data property type in DrahtenWeb/Models/ApiRequest.cs from string to object.

--- DrahtenWeb/Models/ApiRequest.cs
+++ DrahtenWeb/Models/ApiRequest.cs
@@ -12,7 +12,7 @@
     {
         public ApiType? ApiType { get; set; }
         public string? Url { get; set; }
-        public string? Data { get; set; }
+        public object? Data { get; set; }
         public string? AccessToken { get; set; }
     }
 }
