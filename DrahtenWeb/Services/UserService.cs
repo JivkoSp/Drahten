12f1b9f7d3d68b26147428d436aa5f4bf76414e3
JivkoSp <atjspasov@gmail.com>
2024-01-30 11:33:39 +0200

    Added implementation for GetUserById(...), RegisterUser(...), RegisterUserTopic(...) in DrahtenWeb/Services/UserService.cs.

--- DrahtenWeb/Services/UserService.cs
+++ DrahtenWeb/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using DrahtenWeb.Models;
+﻿using DrahtenWeb.Dtos;
+using DrahtenWeb.Models;
 using DrahtenWeb.Services.IServices;
 
 namespace DrahtenWeb.Services
@@ -10,7 +11,45 @@ namespace DrahtenWeb.Services
            
         }
 
-        public async Task<TEntity> GetUserTopics<TEntity>(string accessToken)
+        public async Task<TEntity> GetUserById<TEntity>(string userId, string accessToken)
+        {
+            var response = await SendAsync<TEntity>(new ApiRequest { 
+            
+                ApiType = ApiType.GET,
+                Url = $"https://localhost:7076/user-service/users/{userId}",
+                AccessToken = accessToken
+            });
+
+            return response;
+        }
+
+        public async Task<TEntity> RegisterUser<TEntity>(WriteUserDto user, string accessToken)
+        {
+            var response = await SendAsync<TEntity>(new ApiRequest { 
+                
+                ApiType = ApiType.POST,
+                Url = "https://localhost:7076/user-service/users/",
+                Data = user,
+                AccessToken = accessToken
+            });
+
+            return response;
+        }
+
+        public async Task<TEntity> RegisterUserTopic<TEntity>(WriteUserDto user, string accessToken)
+        {
+            var response = await SendAsync<TEntity>(new ApiRequest
+            {
+                ApiType = ApiType.POST,
+                Url = "https://localhost:7076/user-service/topics/",
+                Data = user,
+                AccessToken = accessToken
+            });
+
+            return response;
+        }
+
+        public async Task<TEntity> GetTopics<TEntity>(string accessToken)
         {
             var response = await SendAsync<TEntity>(new ApiRequest {
             
