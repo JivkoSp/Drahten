077eba52847ae9bc8b234699289d8918d94bd564
JivkoSp <atjspasov@gmail.com>
2024-01-30 10:41:39 +0200

    Added AutoMapper Profile types in  Drahten_Services_UserService/Profiles/.

---
+++ Drahten_Services_UserService/Profiles/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Drahten_Services_UserService.Dtos;
+using Drahten_Services_UserService.Models;
+
+namespace Drahten_Services_UserService.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, ReadUserDto>();
+            CreateMap<WriteUserDto, User>();
+        }
+    }
+}
---
+++ Drahten_Services_UserService/Profiles/UserTopicProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Drahten_Services_UserService.Dtos;
+using Drahten_Services_UserService.Models;
+
+namespace Drahten_Services_UserService.Profiles
+{
+    public class UserTopicProfile : Profile
+    {
+        public UserTopicProfile()
+        {
+            CreateMap<WriteUserDto, UserTopic>();
+        }
+    }
+}
