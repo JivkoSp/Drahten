8275d51ca850a026669eb04a5486d687645e2cff
JivkoSp <atjspasov@gmail.com>
2024-01-30 10:38:37 +0200

    Added new dto types in Drahten_Services_UserService/Dtos/.

---
+++ Drahten_Services_UserService/Dtos/WriteUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Drahten_Services_UserService.Dtos
+{
+    public class WriteUserDto
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int TopicId { get; set; }
+    }
+}
