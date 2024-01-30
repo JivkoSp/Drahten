d67700f14f78f1ccf56c45bdf29fea473853b5ce
JivkoSp <atjspasov@gmail.com>
2024-01-30 11:31:23 +0200

    Defined new methods in DrahtenWeb/Services/IServices/IUserService.cs. The methods are: GetUserById(...), RegisterUser(...), RegisterUserTopic(...).

--- DrahtenWeb/Services/IServices/IUserService.cs
+++ DrahtenWeb/Services/IServices/IUserService.cs
@@ -1,7 +1,12 @@
-﻿namespace DrahtenWeb.Services.IServices
+﻿using DrahtenWeb.Dtos;
+
+namespace DrahtenWeb.Services.IServices
 {
     public interface IUserService : IBaseService
     {
-        Task<TEntity> GetUserTopics<TEntity>(string accessToken);
+        Task<TEntity> GetUserById<TEntity>(string userId, string accessToken);
+        Task<TEntity> RegisterUser<TEntity>(WriteUserDto user, string accessToken);
+        Task<TEntity> RegisterUserTopic<TEntity>(WriteUserDto user, string accessToken);
+        Task<TEntity> GetTopics<TEntity>(string accessToken);
     }
 }
