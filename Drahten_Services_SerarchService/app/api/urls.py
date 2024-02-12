ffa643da296f6613454dc71f31c320a850ed37db
JivkoSp <atjspasov@gmail.com>
2024-02-13 01:24:31 +0200

    Change the url registration of the urls in Drahten_Services_SerarchService/app/api/urls.py. The urls are registered with DefaultRouter(). Added the following NEW routes: news/cybersecurity/europe/summarization/documents, news/cybersecurity/europe/guestions/documents, news/cybersecurity/europe/semantic-search/documents/data. This routes are relative to the root route: search_service/.

--- Drahten_Services_SerarchService/app/api/urls.py
+++ Drahten_Services_SerarchService/app/api/urls.py
@@ -2,10 +2,14 @@
 Definition of urls for Drahten_Services_SerarchService.
 """
 
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('news/cybersecurity/europe', views.CybersecurityNewsEuropeViewSet, basename='cybersec-news-europe')
+router.register('news/cybersecurity/europe/summarization/documents', views.CybersecurityNewsEuropeSummarizationViewSet, basename='cybersec-news-europe-summarization')
+router.register('news/cybersecurity/europe/guestions/documents', views.CybersecurityNewsEuropeQuestionViewSet, basename='cybersec-news-europe-questions')
+router.register('news/cybersecurity/europe/semantic-search/documents/data', views.CybersecurityNewsEuropeSemanticSearchDataViewSet, basename='cybersec-news-europe-questions-semanticsearch-data')
 
-urlpatterns = [
-   path('news/cybersecurity/europe/', )
-]
+#Telling django, that the router is now taking over the urlpatterns.
+urlpatterns = router.urls
