c167abf9bf0fec169ee8c105fad4429904fda2d6
JivkoSp <atjspasov@gmail.com>
2024-02-13 12:23:28 +0200

    Added NEW view model type: DrahtenWeb/ViewModels/ArticleViewModel.cs. This view model represents information for document, received from SearchService.

---
+++ DrahtenWeb/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DrahtenWeb.Dtos;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class ArticleViewModel
+    {
+        public string DocumentId { get; set; } = string.Empty;
+        public DocumentDto? Document { get; set; }
+    }
+}
