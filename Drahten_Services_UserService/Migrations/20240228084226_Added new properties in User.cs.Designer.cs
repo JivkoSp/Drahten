a2f572e443b72af38e3635d701130718808e39a0
JivkoSp <atjspasov@gmail.com>
2024-02-28 10:45:16 +0200

    Added NEW database migration for Drahten_Services_UserService/.

--- Drahten_Services_UserService/Migrations/20240228084226_Added new properties in User.cs.Designer.cs
+++ Drahten_Services_UserService/Migrations/20240228084226_Added new properties in User.cs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Drahten_Services_UserService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Drahten_Services_UserService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240228084226_Added new properties in User.cs")]
+    partial class AddednewpropertiesinUsercs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
