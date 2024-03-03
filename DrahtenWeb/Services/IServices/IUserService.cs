2319d4c80990aedadabdb4959ab2f30f8beb27eb
JivkoSp <atjspasov@gmail.com>
2024-03-03 16:50:52 +0200

    Added new method definitions in DrahtenWeb/Services/IServices/IUserService.cs. The new method definitions are: RegisterArticle(...), RegisterUserArticle(...), GetArticle(...), GetArticleLikes(...), RegisterArticleLike(...), GetArticleComments(...), RegisterArticleComment(...), RegisterArticleCommentThumbsUp(...), RegisterArticleCommentThumbsDown(...), GetArticleCommentThumbsUp(...), GetArticleCommentThumbsDown(...), DeleteArticleCommentThumbsUp(...), DeleteArticleCommentThumbsDown(...) and GetUsersRelatedToArticle(...).

--- DrahtenWeb/Services/IServices/IUserService.cs
+++ DrahtenWeb/Services/IServices/IUserService.cs
@@ -6,9 +6,23 @@ namespace DrahtenWeb.Services.IServices
     {
         Task<TEntity> GetUserById<TEntity>(string userId, string accessToken);
         Task<TEntity> RegisterUser<TEntity>(WriteUserDto user, string accessToken);
-        Task<TEntity> RegisterUserTopic<TEntity>(WriteUserDto user, string accessToken);
+        Task<TEntity> RegisterUserTopic<TEntity>(WriteUserTopicDto user, string accessToken);
         Task<TEntity> GetTopics<TEntity>(string accessToken);
         Task<TEntity> GetRootTopicWithChildren<TEntity>(int topicId, string accessToken);
         Task<TEntity> GetUserTopics<TEntity>(string userId, string accessToken);
+        Task<TEntity> RegisterArticle<TEntity>(WriteArticleDto article, string accessToken);
+        Task<TEntity> RegisterUserArticle<TEntity>(WriteUserArticleDto userArticle, string accessToken);
+        Task<TEntity> GetArticle<TEntity>(string articleId, string accessToken);
+        Task<TEntity> GetArticleLikes<TEntity>(string articleId, string accessToken);
+        Task<TEntity> RegisterArticleLike<TEntity>(WriteArticleLikeDto articleLike, string accessToken);
+        Task<TEntity> GetArticleComments<TEntity>(string articleId, string accessToken);
+        Task<TEntity> RegisterArticleComment<TEntity>(WriteArticleCommentDto articleComment, string accessToken);
+        Task<TEntity> RegisterArticleCommentThumbsUp<TEntity>(WriteArticleCommentThumbsUpDto articleCommentThumbsUp, string accessToken);
+        Task<TEntity> RegisterArticleCommentThumbsDown<TEntity>(WriteArticleCommentThumbsDownDto articleCommentThumbsDown, string accessToken);
+        Task<TEntity> GetArticleCommentThumbsUp<TEntity>(string articleId, int commentId, string accessToken);
+        Task<TEntity> GetArticleCommentThumbsDown<TEntity>(string articleId, int commentId, string accessToken);
+        Task<TEntity> DeleteArticleCommentThumbsUp<TEntity>(string articleId, int articleCommentId, string userId, string accessToken);
+        Task<TEntity> DeleteArticleCommentThumbsDown<TEntity>(string articleId, int articleCommentId, string userId, string accessToken);
+        Task<TEntity> GetUsersRelatedToArticle<TEntity>(string articleId, string accessToken);
     }
 }
