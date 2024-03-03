0bea8fcd1dd9a14f7d70028a2ab6b8f7d4a6cb1d
JivkoSp <atjspasov@gmail.com>
2024-03-03 17:11:25 +0200

    Added new viewmodel type in DrahtenWeb/ViewModels/. The viewmodel type is: ArticleInfoViewModel.cs

---
+++ DrahtenWeb/ViewModels/ArticleInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.ViewModels
+{
+    public class ArticleInfoViewModel
+    {
+        public int Comments { get; set; }
+        public int Views { get; set; }
+        public int Likes { get; set; }
+        public int DisLikes { get; set; }
+    }
+}
