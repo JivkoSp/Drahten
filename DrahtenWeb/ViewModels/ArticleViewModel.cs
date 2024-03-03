5dc8b74d7540b609397d44ef77b082f292a0c6ba
JivkoSp <atjspasov@gmail.com>
2024-03-03 17:14:43 +0200

    Added new properties in DrahtenWeb/ViewModels/ArticleViewModel.cs. The properties are: List<ReadArticleLikeDto>? ArticleLikes and List<ReadArticleCommentDto>? ArticleComments.

--- DrahtenWeb/ViewModels/ArticleViewModel.cs
+++ DrahtenWeb/ViewModels/ArticleViewModel.cs
@@ -6,5 +6,7 @@ namespace DrahtenWeb.ViewModels
     {
         public string DocumentId { get; set; } = string.Empty;
         public DocumentDto? Document { get; set; }
+        public List<ReadArticleLikeDto>? ArticleLikes { get; set; }
+        public List<ReadArticleCommentDto>? ArticleComments { get; set; }
     }
 }
