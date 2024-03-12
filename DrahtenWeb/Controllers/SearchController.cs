24c086e82114f467330ddc2657cb0ede7601be08
JivkoSp <atjspasov@gmail.com>
2024-03-12 13:53:07 +0200

    Added registration mechanism for searched topic data to the private history of user (the user who made the search) in CybersecurityNewsEuropeQuery(...) method from DrahtenWeb/Controllers/SearchController.cs

--- DrahtenWeb/Controllers/SearchController.cs
+++ DrahtenWeb/Controllers/SearchController.cs
@@ -14,10 +14,12 @@ namespace DrahtenWeb.Controllers
     public class SearchController : Controller
     {
         private readonly ISearchService _searchService;
+        private readonly IUserService _userService;
 
-        public SearchController(ISearchService searchService)
+        public SearchController(ISearchService searchService, IUserService userService)
         {
             _searchService = searchService;
+            _userService = userService;
         }
 
         [HttpGet]
@@ -45,19 +47,50 @@ namespace DrahtenWeb.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> CybersecurityNewsEuropeQuery(string query)
+        public async Task<IActionResult> CybersecurityNewsEuropeQuery(int topicId, string query)
         {
             try
             {
+                var response = new ResponseDto();
                 var documents = new List<NLPQueryAnswerDto>();
+                var searchedTopicsData = new List<ReadSearchedTopicDataHistoryDto>();
+
+                //Get the user id.
+                //Here the NameIdentifier claim type represents the user id.
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
                 var accessToken = await HttpContext.GetTokenAsync("access_token");
 
-                var response = await _searchService.GetAllMathingDocumentsNewsCybersecurityEurope<ResponseDto>(query, accessToken ?? "");
+                response = await _searchService.GetAllMathingDocumentsNewsCybersecurityEurope<ResponseDto>(query, accessToken ?? "");
 
                 if(response != null && response.IsSuccess)
                 {
                     documents = JsonConvert.DeserializeObject<List<NLPQueryAnswerDto>>(Convert.ToString(response.Result));
+                }
+
+                //Get all records for user id from SearchedTopicDataPrivateHist table.
+                //Check if the current query exist in the records
+                //If the current query does not exist register it.
+
+                response = await _userService.GetSearchedTopicsDataPrivateHistory<ResponseDto>(userId ?? "", accessToken ?? "");
+
+                searchedTopicsData = JsonConvert.DeserializeObject<List<ReadSearchedTopicDataHistoryDto>>(Convert.ToString(response.Result));
+
+                var searchedTopicDataList = searchedTopicsData?.Where(x => x.Topic?.TopicId == topicId).ToList();
+
+                var searchedTopicQuery = searchedTopicDataList?.FirstOrDefault(x => x.SearchedData == query);
+
+                if(searchedTopicQuery == null)
+                {
+                    var writeSearchedTopicDataHistoryDto = new WriteSearchedTopicDataHistoryDto
+                    {
+                        SearchedData = query,
+                        SearchTime = DateTime.Now,
+                        TopicId = topicId,
+                        HistoryId = userId ?? ""
+                    };
+
+                    await _userService.RegisterSearchedTopicsDataPrivateHistory<ResponseDto>(writeSearchedTopicDataHistoryDto, accessToken ?? "");
                 }
 
                 return new JsonResult(documents);
