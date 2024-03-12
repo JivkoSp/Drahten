c020792669170a95170934c3cc4f780e06a83b3a
JivkoSp <atjspasov@gmail.com>
2024-03-12 12:29:16 +0200

    Added NEW automapper profile types in Drahten_Services_UserService/Profiles/. The profile types are: PrivateHistoryProfile.cs, PublicHistoryProfile.cs, SearchedArticleDataHistProfile.cs, SearchedTopicDataHistProfile.cs, ViewedArticleHistProfile.cs.

---
+++ Drahten_Services_UserService/Profiles/PrivateHistoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Drahten_Services_UserService.Dtos;
+using Drahten_Services_UserService.Models;
+
+namespace Drahten_Services_UserService.Profiles
+{
+    public class PrivateHistoryProfile : Profile
+    {
+        public PrivateHistoryProfile()
+        {
+            CreateMap<WritePrivateHistoryDto, PrivateHistory>()
+                .ForMember(dest => dest.UserId, options => options.MapFrom(source => source.PrivateHistoryId));
+
+            CreateMap<PrivateHistory, ReadPrivateHistoryDto>()
+                .ForMember(dest => dest.PrivateHistoryId, options => options.MapFrom(source => source.UserId));
+        }
+    }
+}
---
+++ Drahten_Services_UserService/Profiles/PublicHistoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Drahten_Services_UserService.Dtos;
+using Drahten_Services_UserService.Models;
+
+namespace Drahten_Services_UserService.Profiles
+{
+    public class PublicHistoryProfile : Profile
+    {
+        public PublicHistoryProfile()
+        {
+            CreateMap<WritePublicHistoryDto, PublicHistory>()
+                .ForMember(dest => dest.UserId, options => options.MapFrom(source => source.PublicHistoryId));
+
+            CreateMap<PublicHistory, ReadPublicHistoryDto>()
+                .ForMember(dest => dest.PublicHistoryId, options => options.MapFrom(source => source.UserId));
+        }
+    }
+}
