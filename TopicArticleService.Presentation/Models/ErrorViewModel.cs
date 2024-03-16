f858fda04f3dfa519a3be976e5ff80733d4365b4
JivkoSp <atjspasov@gmail.com>
2024-03-16 13:01:24 +0200

    New service added: TopicArticleService. The service implements the clean architecture pattern and has the following layers (each layer is separate project): TopicArticleService.Domain; TopicArticleService.Application; TopicArticleService.Infrastructure and TopicArticleService.Presentation. The reason for this service is to divide the responsibilities (to separate the concerns) of UserService. This service will be responsible for actions related to Articles or Topics and nothing else.

---
+++ TopicArticleService.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopicArticleService.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
