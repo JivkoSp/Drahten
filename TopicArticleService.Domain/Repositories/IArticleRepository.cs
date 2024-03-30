a98b3a8ba0883aba6ec018bd0abb29a386c2e922
JivkoSp <atjspasov@gmail.com>
2024-03-30 18:05:53 +0200

    Added repositories that define the allowed actions and operations with domain entities in TopicArticleService.Domain/Repositories/. Added _README.txt file in the same directory.

---
+++ TopicArticleService.Domain/Repositories/IArticleRepository.cs
@@ -0,0 +1,13 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Repositories
+{
+    public interface IArticleRepository
+    {
+        Task<Article> GetArticleByIdAsync(ArticleID articleId);
+        Task AddArticleAsync(Article article);
+        Task UpdateArticleAsync(Article article);
+        Task DeleteArticleAsync(Article article);
+    }
+}
---
+++ TopicArticleService.Domain/Repositories/ITopicRepository.cs
@@ -0,0 +1,13 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Repositories
+{
+    public interface ITopicRepository
+    {
+        Task<Topic> GetTopicByIdAsync(TopicId topicId);
+        Task AddTopicAsync(Topic topic);
+        Task UpdateTopicAsync(Topic topic);
+        Task DeleteTopicAsync(Topic topic);
+    }
+}
