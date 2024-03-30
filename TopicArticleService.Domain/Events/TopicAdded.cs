8624380694248d4b88b43c9e7f7341e9f3185793
JivkoSp <atjspasov@gmail.com>
2024-03-30 17:47:47 +0200

    Added NEW event types in TopicArticleService.Domain/Events/. The event types are: TopicAdded.cs and TopicChildAdded.cs. Added _README.txt file in the same directory.

---
+++ TopicArticleService.Domain/Events/TopicAdded.cs
@@ -0,0 +1,6 @@
+﻿using TopicArticleService.Domain.Entities;
+
+namespace TopicArticleService.Domain.Events
+{
+    public record TopicAdded(Article Article, Topic Topic) : IDomainEvent;
+}
---
+++ TopicArticleService.Domain/Events/TopicChildAdded.cs
@@ -0,0 +1,6 @@
+﻿using TopicArticleService.Domain.Entities;
+
+namespace TopicArticleService.Domain.Events
+{
+    public record TopicChildAdded(Topic ParentTopic, Topic ChildTopic) : IDomainEvent;
+}
