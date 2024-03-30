d1bc6c11df09039a6c41f482879f25dbd7774baf
JivkoSp <atjspasov@gmail.com>
2024-03-30 20:18:14 +0200

    Added logging, application layer specific registrations with AddApplication() extension method and infrastructure layer specific registrations with AddInfrastructure(...) extension method in the DI container, in TopicArticleService.Presentation/Program.cs

--- TopicArticleService.Presentation/Program.cs
+++ TopicArticleService.Presentation/Program.cs
@@ -1,27 +1,40 @@
+using TopicArticleService.Application.Extensions;
+using TopicArticleService.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+// Add logging configuration
+builder.Logging.ClearProviders(); // Clear default logging providers
+
+builder.Logging.AddConsole(); // Add Console logger
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug); // Set minimum log level to Debug
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
