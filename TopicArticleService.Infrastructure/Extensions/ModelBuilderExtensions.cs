65d6c399e36ff3dcf8499418c6428351cf84ce7c
JivkoSp <atjspasov@gmail.com>
2024-03-30 20:02:00 +0200

    Added Extensions/ directory in TopicArticleService.Infrastructure/ with extension methods for IConfiguration, ModelBuilder and ServiceCollection.

---
+++ TopicArticleService.Infrastructure/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TopicArticleService.Infrastructure.Extensions
+{
+    internal static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Generic method, that seeds database with enumeration of one or more entity classes.
+        /// </summary>
+        /// <typeparam name="TEntity">Entity model class.</typeparam>
+        /// <param name="modelBuilder">The class, that the method extends on.</param>
+        /// <param name="entities">Input parameter - class, that implements IEnumerable.</param>
+        /// <returns>Does not return data.</returns>
+        public static void SeedData<TEntity>(this ModelBuilder modelBuilder, IEnumerable<TEntity> entities) where TEntity : class
+        {
+            modelBuilder.Entity<TEntity>().HasData(entities);
+        }
+    }
+}
---
+++ TopicArticleService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TopicArticleService.Application.Extensions;
+using TopicArticleService.Application.Services.ReadServices;
+using TopicArticleService.Application.Services.WriteServices;
+using TopicArticleService.Domain.Repositories;
+using TopicArticleService.Infrastructure.Automapper.Profiles;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+using TopicArticleService.Infrastructure.EntityFramework.Initialization;
+using TopicArticleService.Infrastructure.EntityFramework.Options;
+using TopicArticleService.Infrastructure.EntityFramework.Repositories;
+using TopicArticleService.Infrastructure.EntityFramework.Services;
+
+namespace TopicArticleService.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgresOptions = configuration.GetOptions<PostgresOptions>("Postgres");
+
+            services.AddDbContext<ReadDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddDbContext<WriteDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddHostedService<DbInitializer>();
+
+            services.AddScoped<IArticleRepository, PostgresArticleRepository>();
+
+            services.AddScoped<ITopicRepository, PostgresTopicRepository>();
+
+            services.AddScoped<IArticleReadService, PostgresArticleReadService>();
+
+            services.AddScoped<IUserReadService, PostgresUserReadService>();
+
+            services.AddScoped<IUserWriteService, PostgresUserWriteService>();
+
+            services.AddQueriesWithDispatcher();
+
+            services.AddAutoMapper(configAction => {
+
+                configAction.AddProfile<ArticleProfile>();
+                configAction.AddProfile<UserProfile>();
+                configAction.AddProfile<UserArticleProfile>();
+            });
+
+            return services;
+        }
+    }
+}
