4948323a9a22eebaa0f71835ebc107f3087d2faa
JivkoSp <atjspasov@gmail.com>
2024-03-30 18:50:38 +0200

    Added Automapper directory in TopicArticleService.Infrastructure/ that contains automapper profile types.

---
+++ TopicArticleService.Infrastructure/Automapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Infrastructure.EntityFramework.Models;
+
+namespace TopicArticleService.Infrastructure.Automapper.Profiles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleReadModel, ArticleDto>();
+        }
+    }
+}
---
+++ TopicArticleService.Infrastructure/Automapper/Profiles/UserArticleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Infrastructure.EntityFramework.Models;
+
+namespace TopicArticleService.Infrastructure.Automapper.Profiles
+{
+    public class UserArticleProfile : Profile
+    {
+        public UserArticleProfile()
+        {
+            CreateMap<UserArticleReadModel, UserArticleDto>()
+                .ForMember(dest => dest.UserDto, options => options.MapFrom(source => source.User))
+                .ForMember(dest => dest.ArticleDto, options => options.MapFrom(source => source.Article));
+        }
+    }
+}
