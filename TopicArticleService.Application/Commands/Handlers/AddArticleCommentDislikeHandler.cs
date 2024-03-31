47133bc9cea6a48bf1c5a842713834defabf4ddb
JivkoSp <atjspasov@gmail.com>
2024-03-31 23:51:37 +0300

    Added new command handler TopicArticleService.Application/Commands/Handlers/AddArticleCommentDislikeHandler.cs

---
+++ TopicArticleService.Application/Commands/Handlers/AddArticleCommentDislikeHandler.cs
@@ -0,0 +1,41 @@
+﻿using TopicArticleService.Application.Exceptions;
+using TopicArticleService.Domain.Factories;
+using TopicArticleService.Domain.Repositories;
+
+namespace TopicArticleService.Application.Commands.Handlers
+{
+    public class AddArticleCommentDislikeHandler : ICommandHandler<AddArticleCommentDislikeCommand>
+    {
+        private readonly IArticleRepository _articleRepository;
+        private readonly IArticleCommentDislikeFactory _articleCommentDislikeFactory;
+
+        public AddArticleCommentDislikeHandler(IArticleRepository articleRepository, IArticleCommentDislikeFactory articleCommentDislikeFactory)
+        {
+            _articleRepository = articleRepository;
+            _articleCommentDislikeFactory = articleCommentDislikeFactory;
+        }
+
+        public async Task HandleAsync(AddArticleCommentDislikeCommand command)
+        {
+            var article = await _articleRepository.GetArticleByIdAsync(command.ArticleId);
+
+            if (article == null)
+            {
+                throw new ArticleNotFoundException(command.ArticleId);
+            }
+
+            var articleComment = article.ArticleComments.SingleOrDefault(x => x.Id.Value == command.ArticleCommentId);
+
+            if (articleComment == null)
+            {
+                throw new ArticleCommentNotFoundException(command.ArticleCommentId);
+            }
+
+            var articleCommentDislike = _articleCommentDislikeFactory.Create(command.ArticleCommentId, command.UserId, command.DateTime);
+
+            articleComment.AddDislike(articleCommentDislike);
+
+            await _articleRepository.UpdateArticleAsync(article);
+        }
+    }
+}
