c5b90c75576707a74328151a739c3fdd03d87dcc
JivkoSp <atjspasov@gmail.com>
2024-03-31 23:42:18 +0300

    Added implementation for the HandleAsync(...) method in TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs.

--- TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs
+++ TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs
@@ -1,28 +1,42 @@
-﻿using TopicArticleService.Application.Services;
+﻿using TopicArticleService.Application.Exceptions;
+using TopicArticleService.Application.Services.ReadServices;
+using TopicArticleService.Domain.Factories;
 using TopicArticleService.Domain.Repositories;
 
 namespace TopicArticleService.Application.Commands.Handlers
 {
     public class AddArticleCommentLikeHandler : ICommandHandler<AddArticleCommentLikeCommand>
     {
-        private IArticleRepository _articleRepository;
+        private readonly IArticleRepository _articleRepository;
+        private readonly IArticleCommentLikeFactory _articleCommentLikeFactory;
 
-        //TODO:
-        //IArticleCommentRepository with
-        //GetArticleCommentByIdAsync
-        //AddArticleCommentAsync
-        //UpdateArticleCommentAsync
-        //DeleteArticleCommentAsync
-
-        public AddArticleCommentLikeHandler(IArticleRepository articleRepository)
+        public AddArticleCommentLikeHandler(IArticleRepository articleRepository, IArticleCommentLikeFactory articleCommentLikeFactory)
         {
             _articleRepository = articleRepository;
-
+            _articleCommentLikeFactory = articleCommentLikeFactory;
         }
 
-        public Task HandleAsync(AddArticleCommentLikeCommand command)
+        public async Task HandleAsync(AddArticleCommentLikeCommand command)
         {
-            throw new NotImplementedException();
+            var article = await _articleRepository.GetArticleByIdAsync(command.ArticleId);
+
+            if (article == null)
+            {
+                throw new ArticleNotFoundException(command.ArticleId);
+            }
+
+            var articleComment = article.ArticleComments.SingleOrDefault(x => x.Id.Value == command.ArticleCommentId);
+
+            if(articleComment == null)
+            {
+                throw new ArticleCommentNotFoundException(command.ArticleCommentId);
+            }
+
+            var articleCommentLike = _articleCommentLikeFactory.Create(command.ArticleCommentId, command.UserId, command.DateTime);
+
+            articleComment.AddLike(articleCommentLike);
+
+            await _articleRepository.UpdateArticleAsync(article);
         }
     }
 }
