ac22fa3869262a6c006070ae44d88fd7def617e4
JivkoSp <atjspasov@gmail.com>
2024-03-31 23:50:53 +0300

    Added new command type in TopicArticleService.Application/Commands/. The command is AddArticleCommentDislikeCommand.cs.

---
+++ TopicArticleService.Application/Commands/AddArticleCommentDislikeCommand.cs
@@ -0,0 +1,5 @@
+﻿
+namespace TopicArticleService.Application.Commands
+{
+    public record AddArticleCommentDislikeCommand(Guid ArticleId, Guid ArticleCommentId, string DateTime, Guid UserId) : ICommand;
+}
