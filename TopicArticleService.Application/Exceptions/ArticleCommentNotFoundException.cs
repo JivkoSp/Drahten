19a8f3f9d592353bdf8fd54d2613709bc993f4f3
JivkoSp <atjspasov@gmail.com>
2024-04-01 00:01:46 +0300

    Added new custom exception TopicArticleService.Application/Exceptions/ArticleCommentNotFoundException.cs

---
+++ TopicArticleService.Application/Exceptions/ArticleCommentNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TopicArticleService.Application.Exceptions
+{
+    internal class ArticleCommentNotFoundException : ApplicationException
+    {
+        internal ArticleCommentNotFoundException(Guid articleCommentId) 
+            : base(message: $"ArticleComment #{articleCommentId} was NOT found!")
+        {
+        }
+    }
+}
