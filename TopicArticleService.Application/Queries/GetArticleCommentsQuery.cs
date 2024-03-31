edaf1c772d70027a819ca7bf2bd7633f67fae862
JivkoSp <atjspasov@gmail.com>
2024-04-01 00:02:49 +0300

    Added new query type TopicArticleService.Application/Queries/GetArticleCommentsQuery.cs.

---
+++ TopicArticleService.Application/Queries/GetArticleCommentsQuery.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos;
+
+namespace TopicArticleService.Application.Queries
+{
+    public class GetArticleCommentsQuery : IQuery<List<ArticleCommentDto>>
+    {
+        public string ArticleId { get; set; }
+    }
+}
