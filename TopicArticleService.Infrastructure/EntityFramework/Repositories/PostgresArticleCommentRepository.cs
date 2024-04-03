6681667a051dc8f79283a569492f752f321e40e4
JivkoSp <atjspasov@gmail.com>
2024-04-03 11:54:11 +0300

    Added implementation for the newly added repository interface for article comment in the domain layer: TopicArticleService.Infrastructure/EntityFramework/Repositories/PostgresArticleCommentRepository.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/Repositories/PostgresArticleCommentRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.Repositories;
+using TopicArticleService.Domain.ValueObjects;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.Repositories
+{
+    internal sealed class PostgresArticleCommentRepository : IArticleCommentRepository
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresArticleCommentRepository(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public Task<ArticleComment> GetArticleCommentByIdAsync(ArticleCommentID articleCommentId)
+            => _writeDbContext.ArticleComments
+                .Include(x => x.ArticleCommentLikes)
+                .Include(x => x.ArticleCommentDislikes)
+                .SingleOrDefaultAsync(x => x.Id == articleCommentId);
+
+        public async Task UpdateArticleCommentAsync(ArticleComment articleComment)
+        {
+            _writeDbContext.ArticleComments.Update(articleComment);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
