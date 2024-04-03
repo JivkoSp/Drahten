273074770a9c82874dc7e9abdf13cf7446eddf96
JivkoSp <atjspasov@gmail.com>
2024-04-03 11:51:55 +0300

    Added new repository interface for the domain layer: TopicArticleService.Domain/Repositories/IArticleCommentRepository.cs

---
+++ TopicArticleService.Domain/Repositories/IArticleCommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Repositories
+{
+    public interface IArticleCommentRepository
+    {
+        Task<ArticleComment> GetArticleCommentByIdAsync(ArticleCommentID articleCommentId);
+        Task UpdateArticleCommentAsync(ArticleComment articleComment);
+    }
+}
