970fdaba3fc7a15419981e5a8f4c8ed2ef73f7ce
JivkoSp <atjspasov@gmail.com>
2024-04-04 23:54:41 +0300

    Added the following modifications to TopicArticleService.Domain/Entities/Article.cs: Added UserArticleAdded domain event to the AddUserArticle(...) method; Added ArticleCommentChildAdded domain event to the AddComment(...) method if the article comment being added has parent comment id; Added ArticleCommentChildRemoved event to the RemoveComment(...) method; Added deletion of child comments (if there is any) to the RemoveComment(...); Added ArticleDislikeRemoved domain event to the AddLike(...) method and changed the check if that article like already exists.

--- TopicArticleService.Domain/Entities/Article.cs
+++ TopicArticleService.Domain/Entities/Article.cs
@@ -74,12 +74,12 @@ namespace TopicArticleService.Domain.Entities
 
             _userArticles.Add(userArticle);
 
-            //TODO: Add event
+            AddEvent(new UserArticleAdded(this, userArticle));
         }
 
         public void AddComment(ArticleComment comment)
         {
-            var alreadyExists = _articleComments.Any(x => x.HasUserId(comment) == true 
+            var alreadyExists = _articleComments.Any(x => x.Id == comment.Id || x.HasSameUserId(comment) == true 
                     && x.GetParentCommentId() == comment.GetParentCommentId());
 
             if (alreadyExists)
@@ -89,28 +89,35 @@ namespace TopicArticleService.Domain.Entities
 
             _articleComments.Add(comment);
 
-            AddEvent(new ArticleCommentAdded(this, comment));
+            if(comment.GetParentCommentId() != null)
+            {
+                var parentComment = _articleComments.SingleOrDefault(x => x.Id == comment.GetParentCommentId());
+
+                AddEvent(new ArticleCommentChildAdded(parentComment, comment));
+            }
+            else
+            {
+                AddEvent(new ArticleCommentAdded(this, comment));
+            }
         }
 
-        public void RemoveComment(ArticleCommentID articleCommentId, ArticleCommentID parentArticleCommentId)
+        public void RemoveComment(ArticleCommentID articleCommentId)
         {
-            if(parentArticleCommentId != null)
-            {
-                var parentComment = _articleComments.FirstOrDefault(x => x.Id == parentArticleCommentId);
-
-                if (parentComment == null)
-                {
-                    throw new ArticleCommentParentNotFoundException(parentArticleCommentId, articleCommentId);
-                }
-
-                //parentComment.RemoveChildComment(articleCommentId);
-            }
-
-            var articleComment = _articleComments.FirstOrDefault(x => x.Id == articleCommentId);
+            var articleComment = _articleComments.SingleOrDefault(x => x.Id == articleCommentId);
 
             if(articleComment == null)
             {
                 throw new ArticleCommentNotFoundException(articleCommentId);
+            }
+
+            var articleCommentChildren = _articleComments.Where(x => x.GetParentCommentId() != null 
+                && x.GetParentCommentId() == articleCommentId).ToList();
+
+            foreach(var childComment in articleCommentChildren)
+            {
+                _articleComments.Remove(childComment);
+
+                AddEvent(new ArticleCommentChildRemoved(articleComment, childComment));
             }
 
             _articleComments.Remove(articleComment);
@@ -120,15 +127,15 @@ namespace TopicArticleService.Domain.Entities
 
         public void AddLike(ArticleLike like)
         {
-            var alreadyExists = _articleLikes.Contains(like);
+            var alreadyExists = _articleLikes.Any(x => x.UserID == like.UserID);
 
-            if(alreadyExists)
+            if (alreadyExists)
             {
                 throw new ArticleLikeAlreadyExistsException(Id, like.UserID);
             }
 
             //Find the user dislike in _dislikes.
-            var userDislike = _articleDislikes.FirstOrDefault(x => x.UserID == like.UserID);
+            var userDislike = _articleDislikes.SingleOrDefault(x => x.UserID == like.UserID);
 
             //Check if the user for this like has dislike in _dislikes.
             if(userDislike != null)
@@ -137,6 +144,8 @@ namespace TopicArticleService.Domain.Entities
 
                 //Delete the dislike from _dislikes.
                 _articleDislikes.Remove(userDislike);
+
+                AddEvent(new ArticleDislikeRemoved(this, userDislike));
             }
 
             _articleLikes.Add(like);
@@ -146,15 +155,15 @@ namespace TopicArticleService.Domain.Entities
 
         public void AddDislike(ArticleDislike dislike)
         {
-            var alreadyExists = _articleDislikes.Contains(dislike);
+            var alreadyExists = _articleDislikes.Any(x => x.UserID == dislike.UserID);
 
-            if(alreadyExists)
+            if (alreadyExists)
             {
                 throw new ArticleDislikeAlreadyExistsException(Id, dislike.UserID);
             }
 
             //Find the user like in _likes.
-            var userLike = _articleLikes.FirstOrDefault(x => x.UserID == dislike.UserID);
+            var userLike = _articleLikes.SingleOrDefault(x => x.UserID == dislike.UserID);
 
             //Check if the user for this dislike has like in _likes.
             if(userLike != null)
