8696a47f7dc63b999f7877ec0796dc1bdc4fdfd6
JivkoSp <atjspasov@gmail.com>
2024-04-04 23:58:15 +0300

    Added new domain event type TopicArticleService.Domain/Events/UserArticleAdded.cs

---
+++ TopicArticleService.Domain/Events/UserArticleAdded.cs
@@ -0,0 +1,7 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Events
+{
+    public record UserArticleAdded(Article Article, UserArticle UserArticle) : IDomainEvent;
+}
