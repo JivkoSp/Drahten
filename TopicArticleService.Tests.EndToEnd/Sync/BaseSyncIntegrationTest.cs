812c26b92622680edb453afe759fb6ef4944b72b
JivkoSp <atjspasov@gmail.com>
2024-04-09 22:10:30 +0300

    Added new class BaseSyncIntegrationTest.cs in TopicArticleService.Tests.EndToEnd/Sync/. This class inherits from IClassFixture<DrahtenApplicationFactory> and contains GetContent(...), Get(...) and Post(...) methods.

---
+++ TopicArticleService.Tests.EndToEnd/Sync/BaseSyncIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using TopicArticleService.Application.Commands;
+using TopicArticleService.Tests.EndToEnd.Factories;
+using Xunit;
+
+namespace TopicArticleService.Tests.EndToEnd.Sync
+{
+    public abstract class BaseSyncIntegrationTest : IClassFixture<DrahtenApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+
+        public BaseSyncIntegrationTest(DrahtenApplicationFactory factory)
+        {
+            factory.Server.AllowSynchronousIO = true;
+            _httpClient = factory.CreateClient();
+        }
+
+        protected static StringContent GetContent(object value)
+            => new StringContent(JsonConvert.SerializeObject(value), Encoding.UTF8, "application/json");
+
+        protected Task<HttpResponseMessage> Post<TCommand>(TCommand command, string requestUri)
+            where TCommand : ICommand
+            => _httpClient.PostAsync(requestUri, GetContent(command));
+
+        protected Task<HttpResponseMessage> Get(string requestUri)
+            => _httpClient.GetAsync(requestUri);
+    }
+}
