cdaac59fdf793aeb6421fed8cc09a282fe4eb928
JivkoSp <atjspasov@gmail.com>
2024-04-10 00:12:42 +0300

    Added two NEW automapper profile types in TopicArticleService.Infrastructure/Automapper/Profiles/. The profile types are ArticleDislikeProfile.cs and ArticleLikeProfile.cs

---
+++ TopicArticleService.Infrastructure/Automapper/Profiles/ArticleDislikeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Infrastructure.EntityFramework.Models;
+
+namespace TopicArticleService.Infrastructure.Automapper.Profiles
+{
+    internal class ArticleDislikeProfile : Profile
+    {
+        public ArticleDislikeProfile()
+        {
+            CreateMap<ArticleDislikeReadModel, ArticleDislikeDto>();
+        }
+    }
+}
