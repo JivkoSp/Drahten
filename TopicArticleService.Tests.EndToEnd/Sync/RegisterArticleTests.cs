9220e90713d310e87d58b7fc5db5e41d69ee97eb
JivkoSp <atjspasov@gmail.com>
2024-04-09 22:08:32 +0300

    Changed the name of the object responseDeserializedContent from Register_Article_Endpoint_Should_Add_Article_With_Given_ArticleId_To_The_Database(...) method in TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs to articleDto becouse the deserialized types is changed from CreateArticleCommand to ArticleDto. The RegisterArticleTests.cs now inherits from BaseSyncIntegrationTest instead directly from IClassFixture<DrahtenApplicationFactory> becouse the methods GetContent(...), Post(...) and Get(...) are now moved to the abstract class BaseSyncIntegrationTest. This is done to not repeat the same method definitions in all other unit tests.

--- TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs
+++ TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs
@@ -1,35 +1,22 @@
 ﻿using Newtonsoft.Json;
 using Shouldly;
 using System.Net;
-using System.Text;
 using TopicArticleService.Application.Commands;
+using TopicArticleService.Application.Dtos;
 using TopicArticleService.Tests.EndToEnd.Factories;
 using Xunit;
 
 namespace TopicArticleService.Tests.EndToEnd.Sync
 {
-    public class RegisterArticleTests : IClassFixture<DrahtenApplicationFactory>
+    public class RegisterArticleTests : BaseSyncIntegrationTest
     {
         #region GLOBAL ARRANGE
 
-        private readonly HttpClient _httpClient;
-
-        public RegisterArticleTests(DrahtenApplicationFactory factory)
+        public RegisterArticleTests(DrahtenApplicationFactory factory) : base(factory)
         {
-            factory.Server.AllowSynchronousIO = true;
-            _httpClient = factory.CreateClient();
         }
 
         #endregion
-
-        private static StringContent GetContent(object value)
-            => new StringContent(JsonConvert.SerializeObject(value), Encoding.UTF8, "application/json");
-
-        private Task<HttpResponseMessage> Post(CreateArticleCommand command, string requestUri)
-            => _httpClient.PostAsync(requestUri, GetContent(command));
-
-        private Task<HttpResponseMessage> Get(string requestUri)
-            => _httpClient.GetAsync(requestUri);
 
         //*** IMPORTANT *** - The CreateArticleCommand must have valid TopicId (TopicId for Topic that already exist in the database).
 
@@ -40,7 +27,7 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
         {
             //ARRANGE
             var command = new CreateArticleCommand(Guid.NewGuid(), "prev title TEST", "title TEST", "content TEST", 
-                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("fcf49b1c-e8d1-4134-a4cd-9ee01fac7383"));
+                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("888a5c96-7c7c-4f98-90b6-91a0c2d401b0"));
 
             //ACT
             var response = await Post(command, "/topic-article-service/articles");
@@ -61,7 +48,7 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
         {
             //ARRANGE
             var command = new CreateArticleCommand(Guid.NewGuid(), "prev title TEST", "title TEST", "content TEST",
-                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("fcf49b1c-e8d1-4134-a4cd-9ee01fac7383"));
+                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("888a5c96-7c7c-4f98-90b6-91a0c2d401b0"));
 
             //ACT
             await Post(command, "/topic-article-service/articles");
@@ -75,26 +62,26 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
 
             var responseSerializedContent = await response.Content.ReadAsStringAsync();
 
-            var responseDeserializedContent = JsonConvert.DeserializeObject<CreateArticleCommand>(responseSerializedContent);
+            var articleDto = JsonConvert.DeserializeObject<ArticleDto>(responseSerializedContent);
 
             //Comparing the values of the command object that is send to the /topic-article-service/articles POST endpoint
-            //with the values of the response that is received by the /topic-article-service/articles/{ArticleId} GET endpoint.
+            //with the values of the articleDto object that is received by the /topic-article-service/articles/{ArticleId} GET endpoint.
 
-            responseDeserializedContent.ArticleId.ShouldBe(command.ArticleId);
+            articleDto.ArticleId.ShouldBe(command.ArticleId.ToString());
 
-            responseDeserializedContent.PrevTitle.ShouldBe(command.PrevTitle);
+            articleDto.PrevTitle.ShouldBe(command.PrevTitle);
 
-            responseDeserializedContent.Title.ShouldBe(command.Title);
+            articleDto.Title.ShouldBe(command.Title);
 
-            responseDeserializedContent.Content.ShouldBe(command.Content);
+            articleDto.Content.ShouldBe(command.Content);
 
-            responseDeserializedContent.PublishingDate.ShouldBe(command.PublishingDate);
+            articleDto.PublishingDate.ShouldBe(command.PublishingDate);
 
-            responseDeserializedContent.Author.ShouldBe(command.Author);
+            articleDto.Author.ShouldBe(command.Author);
 
-            responseDeserializedContent.Link.ShouldBe(command.Link);
+            articleDto.Link.ShouldBe(command.Link);
 
-            responseDeserializedContent.TopicId.ShouldBe(command.TopicId);  
+            articleDto.TopicId.ShouldBe(command.TopicId);  
         }
     }
 }
