5275929ab6173095b3f98cc193b037be0e4bf6a8
JivkoSp <atjspasov@gmail.com>
2024-04-13 23:21:59 +0300

    Added NEW domain factory in UserService.Domain/Factories/. The factory is ContactRequestFactory.cs.

---
+++ UserService.Domain/Factories/ContactRequestFactory.cs
@@ -0,0 +1,11 @@
+﻿using UserService.Domain.Factories.Interfaces;
+using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Factories
+{
+    public sealed class ContactRequestFactory : IContactRequestFactory
+    {
+        public ContactRequest Create(UserID userId, DateTimeOffset dateTime)
+            => new ContactRequest(userId, dateTime);    
+    }
+}
---
+++ UserService.Domain/Factories/Interfaces/IContactRequestFactory.cs
@@ -0,0 +1,9 @@
+﻿using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Factories.Interfaces
+{
+    public interface IContactRequestFactory
+    {
+        ContactRequest Create(UserID userId, DateTimeOffset dateTime);
+    }
+}
