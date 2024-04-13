f2b0cc1adc8ca89e4e64b7bbdfedd4bf8ff83e8c
JivkoSp <atjspasov@gmail.com>
2024-04-13 22:27:27 +0300

    Added NEW domain event types in UserService.Domain/Events/. The event types are BannedUserAdded.cs, ContactRequestAdded.cs and UserTrackingAuditAdded.cs.

---
+++ UserService.Domain/Events/BannedUserAdded.cs
@@ -0,0 +1,7 @@
+﻿using UserService.Domain.Entities;
+using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Events
+{
+    public record BannedUserAdded(User User, BannedUser BannedUser) : IDomainEvent;
+}
