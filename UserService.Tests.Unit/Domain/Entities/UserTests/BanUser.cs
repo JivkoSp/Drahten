a5dfe20955d3dc4ec1c2b91661501d0bc2175786
JivkoSp <atjspasov@gmail.com>
2024-04-13 22:02:53 +0300

    Added xUnit project for unit testing the domain and the application layer of the UserService. The project has Domain directory that will include unit tests for the domain layer.

---
+++ UserService.Tests.Unit/Domain/Entities/UserTests/BanUser.cs
@@ -0,0 +1,63 @@
+﻿using Shouldly;
+using UserService.Domain.Entities;
+using UserService.Domain.Exceptions;
+using UserService.Domain.Factories;
+using UserService.Domain.Factories.Interfaces;
+using UserService.Domain.ValueObjects;
+using Xunit;
+
+namespace UserService.Tests.Unit.Domain.Entities.UserTests
+{
+    public sealed class BanUser
+    {
+        #region GLOBAL ARRANGE
+
+        private readonly IUserFactory _userFactory;
+        private readonly IBannedUserFactory _bannedUserFactory;
+
+        private User GetUser()
+        {
+            var user = _userFactory.Create(Guid.NewGuid(), "John Doe", "Johny", "johny@mail.com");
+
+            return user;
+        }
+
+        private BannedUser GetBannedUser()
+        {
+            var bannedUser = _bannedUserFactory.Create(Guid.NewGuid());
+
+            return bannedUser;
+        }
+
+        public BanUser()
+        {
+            _userFactory = new UserFactory();
+            _bannedUserFactory = new BannedUserFactory();
+        }
+
+        #endregion
+
+        //Should throw BannedUserAlreadyExistsException when the following condition is met:
+        //There is already banned user (BannedUser value object) with matching UserID.
+        [Fact]
+        public void Duplicate_BannedUser_Throws_BannedUserAlreadyExistsException()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var bannedUser = GetBannedUser();
+
+            user.BanUser(bannedUser);
+
+            //ACT
+            var exception = Record.Exception(() => user.BanUser(bannedUser));
+
+            //ASSERT
+            exception.ShouldNotBeNull();
+
+            exception.ShouldBeOfType<BannedUserAlreadyExistsException>();
+        }
+
+        
+    }
+}
