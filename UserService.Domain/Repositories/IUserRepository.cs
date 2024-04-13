29419cd0777329025ed46b7a4722395977185851
JivkoSp <atjspasov@gmail.com>
2024-04-13 21:55:58 +0300

    Added Repositories directory in UserService.Domain. The directory has IUserRepository inteface and _README.txt file.

---
+++ UserService.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserService.Domain.Entities;
+using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(UserID userId);
+        Task AddUserAsync(User user);  
+        Task UpdateUserAsync(User user);
+    }
+}
