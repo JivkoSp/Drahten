bc9d56758fe5a6b08cff6f844a7e025598b31586
JivkoSp <atjspasov@gmail.com>
2024-04-14 18:20:25 +0300

    Added NEW domain custom exception: ContactRequestNotFoundException.cs in UserService.Domain/Exceptions/.

---
+++ UserService.Domain/Exceptions/ContactRequestNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserService.Domain.Exceptions
+{
+    public sealed class ContactRequestNotFoundException : DomainException
+    {
+        internal ContactRequestNotFoundException(Guid userId, Guid contactRequestUserId) 
+            : base(message: $"No contact request for User #{contactRequestUserId} was found related to User #{userId}!")
+        {
+        }
+    }
+}
