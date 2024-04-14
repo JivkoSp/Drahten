b6e29f621c3a061375212de3c4bf3f019318773a
JivkoSp <atjspasov@gmail.com>
2024-04-14 18:43:24 +0300

    Added NEW domain unit tests for the RemoveContactRequest method (the method is part of the User entity) in UserService.Tests.Unit/Domain/Entities/UserTests/RemoveContactRequest.cs

--- UserService.Tests.Unit/Domain/Entities/UserTests/RemoveContactRequest.cs
+++ UserService.Tests.Unit/Domain/Entities/UserTests/RemoveContactRequest.cs
@@ -1,5 +1,6 @@
 ﻿using Shouldly;
 using UserService.Domain.Entities;
+using UserService.Domain.Events;
 using UserService.Domain.Exceptions;
 using UserService.Domain.Factories;
 using UserService.Domain.Factories.Interfaces;
@@ -54,6 +55,50 @@ namespace UserService.Tests.Unit.Domain.Entities.UserTests
             exception.ShouldNotBeNull();
 
             exception.ShouldBeOfType<ContactRequestNotFoundException>();
+        }
+
+        //Should remove contact request (ContactRequest value object) from internal collection of ContactRequest value objects
+        //and produce ContactRequestRemoved domain event.
+        //The ContactRequestRemoved domain event should contain:
+        //1. The same user entity that the contact request value object was removed from.
+        //2. The same contact request value object that was removed from the internal collection of ContactRequest value objects.
+        [Fact]
+        public void Removes_ContactRequest_And_Produces_ContactRequestRemoved_Domain_Event_On_Success()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var contactRequest = GetContactRequest();
+
+            user.AddContactRequest(contactRequest);
+
+            //ACT
+            var exception = Record.Exception(() => user.RemoveContactRequest(contactRequest));
+
+            //ASSERT
+            exception.ShouldBeNull();
+
+            user.DomainEvents.Count().ShouldBe(2);
+
+            user.ContactRequests.Count().ShouldBe(0);
+
+            var contactRequestAddedEvent = user.DomainEvents.FirstOrDefault(x =>
+                 x.GetType() == typeof(ContactRequestAdded)) as ContactRequestAdded;
+
+            var contactRequestRemovedEvent = user.DomainEvents.FirstOrDefault(x =>
+                x.GetType() == typeof(ContactRequestRemoved)) as ContactRequestRemoved;
+
+            contactRequestAddedEvent.ShouldNotBeNull();
+
+            contactRequestAddedEvent.User.ShouldBeSameAs(user);
+
+            contactRequestAddedEvent.ContactRequest.ShouldBeSameAs(contactRequest);
+
+            contactRequestRemovedEvent.ShouldNotBeNull();
+
+            contactRequestRemovedEvent.User.ShouldBeSameAs(user);
+
+            contactRequestRemovedEvent.ContactRequest.ShouldBeSameAs(contactRequest);
         }
     }
 }
