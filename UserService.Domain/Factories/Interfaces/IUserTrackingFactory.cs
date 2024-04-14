b97be1e766a428ec8ea3d4acc679b7f20da6868c
JivkoSp <atjspasov@gmail.com>
2024-04-14 16:04:19 +0300

    Added interface: IUserTrackingFactory.cs for NEW domain factory in UserService.Domain/Factories/Interfaces/.

---
+++ UserService.Domain/Factories/Interfaces/IUserTrackingFactory.cs
@@ -0,0 +1,9 @@
+﻿using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Factories.Interfaces
+{
+    public interface IUserTrackingFactory
+    {
+        UserTracking Create(UserID userId, string action, DateTimeOffset dateTime, string referrer);
+    }
+}
