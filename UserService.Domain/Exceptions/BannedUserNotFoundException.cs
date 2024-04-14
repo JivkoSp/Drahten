eef73be3fab67101acd4ede8ae66427c2698da3e
JivkoSp <atjspasov@gmail.com>
2024-04-14 16:21:59 +0300

    Added NEW domain custom exception: BannedUserNotFoundException.cs in UserService.Domain/Exceptions/.

---
+++ UserService.Domain/Exceptions/BannedUserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserService.Domain.Exceptions
+{
+    public sealed class BannedUserNotFoundException : DomainException
+    {
+        internal BannedUserNotFoundException(Guid userId, Guid bannedUserId) 
+            : base(message: $"No banned user #{bannedUserId} was found related to User #{userId}!")
+        {
+        }
+    }
+}
