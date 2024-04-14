a64d705ce8a544578c7511f61568b55c80f4eeb0
JivkoSp <atjspasov@gmail.com>
2024-04-14 18:21:41 +0300

    Added NEW domain event: ContactRequestRemoved.cs in UserService.Domain/Events/.

---
+++ UserService.Domain/Events/ContactRequestRemoved.cs
@@ -0,0 +1,7 @@
+﻿using UserService.Domain.Entities;
+using UserService.Domain.ValueObjects;
+
+namespace UserService.Domain.Events
+{
+    public record ContactRequestRemoved(User User, ContactRequest ContactRequest) : IDomainEvent;
+}
