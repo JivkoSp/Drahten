6f76a8f1a3310b9d2ce9ba5a53068b5607c68a5e
JivkoSp <atjspasov@gmail.com>
2024-04-15 12:37:26 +0300

    Added NEW custom domain exception: NullUserParameterException.cs in UserService.Domain/Exceptions/.

---
+++ UserService.Domain/Exceptions/NullUserParameterException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserService.Domain.Exceptions
+{
+    public sealed class NullUserParameterException : DomainException
+    {
+        internal NullUserParameterException() 
+            : base(message: $"The User cannot be initialized with one or more null parameters!")
+        {
+        }
+    }
+}
