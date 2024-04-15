709a77d09428f301dfdbeba72a2f95fae70e3f83
JivkoSp <atjspasov@gmail.com>
2024-04-15 16:08:49 +0300

    Added application layer custom exceptions in UserService.Application/Exceptions/.

---
+++ UserService.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UserService.Application.Exceptions
+{
+    public abstract class ApplicationException : Exception
+    {
+        public virtual string ErrorCode { get; set; }
+
+        public ApplicationException(string message) : base(message)
+        {
+        }
+    }
+}
