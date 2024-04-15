d90c9e7695f94a0a0cad4d59f406b6e52d31d0a2
JivkoSp <atjspasov@gmail.com>
2024-04-15 22:11:43 +0300

    Added Dtos/ directory in UserService.Application. The directory contains UserDto.cs and _README.txt file.

---
+++ UserService.Application/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UserService.Application.Dtos
+{
+    public class UserDto
+    {
+        public string UserId { get; set; }
+        public string UserFullName {  get; set; }
+        public string UserNickName { get; set; }
+        public string UserEmailAddress { get; set; }
+    }
+}
