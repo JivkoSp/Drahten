75fda82585ea116ecf1ed9b963335215152c3e7f
JivkoSp <atjspasov@gmail.com>
2024-04-18 23:56:13 +0300

    Added custom extension methods in UserService.Infrastructure/Extensions/IConfigurationExtensions.cs and UserService.Infrastructure/Extensions/IServiceCollectionExtensions.cs.

---
+++ UserService.Infrastructure/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace UserService.Infrastructure.Extensions
+{
+    internal static class IConfigurationExtensions
+    {
+        public static TOptions GetOptions<TOptions>(this IConfiguration configuration, string sectionName)
+            where TOptions : new()
+        {
+            var options = new TOptions();
+
+            configuration.GetSection(sectionName).Bind(options);
+
+            return options;
+        }
+    }
+}
---
+++ UserService.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserService.Application.Extensions;
+using UserService.Application.Services.ReadServices;
+using UserService.Domain.Repositories;
+using UserService.Infrastructure.Automapper.Profiles;
+using UserService.Infrastructure.EntityFramework.Contexts;
+using UserService.Infrastructure.EntityFramework.Initialization;
+using UserService.Infrastructure.EntityFramework.Options;
+using UserService.Infrastructure.EntityFramework.Repositories;
+using UserService.Infrastructure.EntityFramework.Services.ReadServices;
+
+namespace UserService.Infrastructure.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgresOptions = configuration.GetOptions<PostgresOptions>("Postgres");
+
+            services.AddDbContext<ReadDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddDbContext<WriteDbContext>(options => options.UseNpgsql(postgresOptions.ConnectionString));
+
+            services.AddHostedService<DbInitializer>();
+
+            services.AddScoped<IUserRepository, PostgresUserRepository>();
+
+            services.AddScoped<IUserReadService, PostgresUserReadService>();
+
+            services.AddQueriesWithDispatcher();
+
+            services.AddAutoMapper(configAction =>
+            {
+                configAction.AddProfile<UserProfle>();
+                configAction.AddProfile<BannedUserProfile>();
+                configAction.AddProfile<ContactRequestProfile>();
+            });
+
+            //services.AddSingleton<IExceptionToResponseMapper, ExceptionToResponseMapper>();
+
+            return services;
+        }
+    }
+}
