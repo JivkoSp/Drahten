9ea81a8ee634e13edded4439ec2cc62a0e94c981
JivkoSp <atjspasov@gmail.com>
2024-04-18 22:54:27 +0300

    Added NEW custom domain exception: InvalidBannedUserDateTimeException.cs in UserService.Domain/Exceptions/.

---
+++ UserService.Domain/Exceptions/InvalidBannedUserDateTimeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UserService.Domain.Exceptions
+{
+    public sealed class InvalidBannedUserDateTimeException : DomainException
+    {
+        public InvalidBannedUserDateTimeException() : base(message: "Invalid datetime for user ban!")
+        {
+        }
+    }
+}
