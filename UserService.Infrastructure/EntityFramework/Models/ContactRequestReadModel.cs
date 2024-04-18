158262a4d43fef87aace32b11e23d46ec5d15fb4
JivkoSp <atjspasov@gmail.com>
2024-04-18 23:28:25 +0300

    Added Message property of type string in UserService.Infrastructure/EntityFramework/Models/ContactRequestReadModel.cs.

--- UserService.Infrastructure/EntityFramework/Models/ContactRequestReadModel.cs
+++ UserService.Infrastructure/EntityFramework/Models/ContactRequestReadModel.cs
@@ -6,6 +6,7 @@ namespace UserService.Infrastructure.EntityFramework.Models
         //Composite primary key { IssuerUserId, ReceiverUserId }
         public string IssuerUserId { get; set; }
         public string ReceiverUserId { get; set; }
+        public string Message { get; set; }
         public DateTimeOffset DateTime {  get; set; }
         
         //Relationships
