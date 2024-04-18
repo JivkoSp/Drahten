a986016ffae1e627854d351b04c9ca4fc3cd869b
JivkoSp <atjspasov@gmail.com>
2024-04-18 23:42:26 +0300

    Added DbContext classes for the read and write side in UserService.Infrastructure/EntityFramework/Contexts/. The DbContext classes are: ReadDbContext.cs and WriteDbContext.cs.

---
+++ UserService.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration;
+using UserService.Infrastructure.EntityFramework.Models;
+
+namespace UserService.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class ReadDbContext : DbContext
+    {
+        public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserReadModel> Users { get; set; }
+        public DbSet<BannedUserReadModel> BannedUsers { get; set; }
+        public DbSet<ContactRequestReadModel> ContactRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("user-service");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new BannedUserConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactRequestConfiguration());
+            modelBuilder.ApplyConfiguration(new UserTrackingConfiguration());
+        }
+    }
+}
---
+++ UserService.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using UserService.Domain.Entities;
+using UserService.Domain.ValueObjects;
+using UserService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration;
+
+namespace UserService.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class WriteDbContext : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public WriteDbContext(DbContextOptions<WriteDbContext> options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ContactRequest> ContactRequests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLoggerFactory(_loggerFactory); // Enable logging
+                optionsBuilder.EnableSensitiveDataLogging(); // Include parameter values in logs
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("user-service");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new BannedUserConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactRequestConfiguration());
+            modelBuilder.ApplyConfiguration(new UserTrackingConfiguration());
+        }
+    }
+}
