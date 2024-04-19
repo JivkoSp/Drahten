61de19fe5ce84c5ec947e5d60213548bb485c4be
JivkoSp <atjspasov@gmail.com>
2024-04-19 13:16:25 +0300

    Renames unit test: UserService.Tests.Unit/Domain/Entities/UserTests/RemoveContactRequest.cs -> UserService.Tests.Unit/Domain/Entities/UserTests/RemoveReceivedContactRequest.cs. Reason: Changed the name of the method in the domain. The test file should reflect that change.

--- UserService.Tests.Unit/Domain/Entities/UserTests/RemoveReceivedContactRequest.cs
+++ UserService.Tests.Unit/Domain/Entities/UserTests/RemoveReceivedContactRequest.cs
@@ -25,7 +25,7 @@ namespace UserService.Tests.Unit.Domain.Entities.UserTests
 
         private ContactRequest GetContactRequest()
         {
-            var contactRequest = _contactRequestFactory.Create(Guid.NewGuid(), DateTimeOffset.Now);
+            var contactRequest = _contactRequestFactory.Create(Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now, null);
 
             return contactRequest;
         }
@@ -44,12 +44,12 @@ namespace UserService.Tests.Unit.Domain.Entities.UserTests
         public void ContactRequest_NotFound_Throws_ContactRequestNotFoundException()
         {
             //ARRANGE
-            var user = GetUser();
+            var issuer = GetUser();
 
             var contactRequest = GetContactRequest();
 
             //ACT
-            var exception = Record.Exception(() => user.RemoveContactRequest(contactRequest));
+            var exception = Record.Exception(() => issuer.RemoveIssuedContactRequest(contactRequest.ReceiverUserId));
 
             //ASSERT
             exception.ShouldNotBeNull();
@@ -57,46 +57,46 @@ namespace UserService.Tests.Unit.Domain.Entities.UserTests
             exception.ShouldBeOfType<ContactRequestNotFoundException>();
         }
 
-        //Should remove contact request (ContactRequest value object) from internal collection of ContactRequest value objects
+        //Should remove received contact request (ContactRequest value object) from internal collection of ContactRequest value objects
         //and produce ContactRequestRemoved domain event.
         //The ContactRequestRemoved domain event should contain:
-        //1. The same user entity that the contact request value object was removed from.
+        //1. The same user entity that the contact request value object was removed from (The receiver).
         //2. The same contact request value object that was removed from the internal collection of ContactRequest value objects.
         [Fact]
-        public void Removes_ContactRequest_And_Produces_ContactRequestRemoved_Domain_Event_On_Success()
+        public void Removes_Received_ContactRequest_And_Produces_ContactRequestRemoved_Domain_Event_On_Success()
         {
             //ARRANGE
-            var user = GetUser();
+            var receiver = GetUser();
 
             var contactRequest = GetContactRequest();
 
-            user.AddContactRequest(contactRequest);
+            receiver.AddContactRequest(contactRequest);
 
             //ACT
-            var exception = Record.Exception(() => user.RemoveContactRequest(contactRequest));
+            var exception = Record.Exception(() => receiver.RemoveReceivedContactRequest(contactRequest.IssuerUserId));
 
             //ASSERT
             exception.ShouldBeNull();
 
-            user.DomainEvents.Count().ShouldBe(2);
+            receiver.DomainEvents.Count().ShouldBe(2);
 
-            user.ContactRequests.Count().ShouldBe(0);
+            receiver.ReceivedContactRequests.Count().ShouldBe(0);
 
-            var contactRequestAddedEvent = user.DomainEvents.FirstOrDefault(x =>
+            var contactRequestAddedEvent = receiver.DomainEvents.FirstOrDefault(x =>
                  x.GetType() == typeof(ContactRequestAdded)) as ContactRequestAdded;
 
-            var contactRequestRemovedEvent = user.DomainEvents.FirstOrDefault(x =>
+            var contactRequestRemovedEvent = receiver.DomainEvents.FirstOrDefault(x =>
                 x.GetType() == typeof(ContactRequestRemoved)) as ContactRequestRemoved;
 
             contactRequestAddedEvent.ShouldNotBeNull();
 
-            contactRequestAddedEvent.User.ShouldBeSameAs(user);
+            contactRequestAddedEvent.User.ShouldBeSameAs(receiver);
 
             contactRequestAddedEvent.ContactRequest.ShouldBeSameAs(contactRequest);
 
             contactRequestRemovedEvent.ShouldNotBeNull();
 
-            contactRequestRemovedEvent.User.ShouldBeSameAs(user);
+            contactRequestRemovedEvent.User.ShouldBeSameAs(receiver);
 
             contactRequestRemovedEvent.ContactRequest.ShouldBeSameAs(contactRequest);
         }
