5457789580227cb80e0f3c2257a2b6c4915a46bc
JivkoSp <atjspasov@gmail.com>
2024-04-20 17:49:43 +0300

    Added Message property of type string to the UserService.Application/Dtos/ReceivedContactRequestByUserDto.cs dto

--- UserService.Application/Dtos/ReceivedContactRequestByUserDto.cs
+++ UserService.Application/Dtos/ReceivedContactRequestByUserDto.cs
@@ -3,6 +3,7 @@ namespace UserService.Application.Dtos
 {
     public class ReceivedContactRequestByUserDto
     {
+        public string Message { get; set; }
         public DateTimeOffset DateTime { get; set; }
         public UserDto ReceiverDto { get; set; }
     }
