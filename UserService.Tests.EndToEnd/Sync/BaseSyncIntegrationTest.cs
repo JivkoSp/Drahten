599ba306978c4a60e941b12f90f04faab81c9e2d
JivkoSp <atjspasov@gmail.com>
2024-04-20 11:22:00 +0300

    Added Sync/ directory in UserService.Tests.EndToEnd. The directory contains BaseSyncIntegrationTest.cs and RegisterUserTests.cs. The BaseSyncIntegrationTest.cs defines the operations that will be performed against the UserService api. This class also inherits from IClassFixture<UserServiceApplicationFactory>. The RegisterUserTests.cs defines tests for the RegisterUser endpoint of the UserService api.

---
+++ UserService.Tests.EndToEnd/Sync/BaseSyncIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using UserService.Application.Commands;
+using UserService.Tests.EndToEnd.Factories;
+using Xunit;
+
+namespace UserService.Tests.EndToEnd.Sync
+{
+    public abstract class BaseSyncIntegrationTest : IClassFixture<UserServiceApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+
+        private static StringContent GetContent(object value)
+           => new StringContent(JsonConvert.SerializeObject(value), Encoding.UTF8, "application/json");
+
+        public BaseSyncIntegrationTest(UserServiceApplicationFactory factory)
+        {
+            factory.Server.AllowSynchronousIO = true;
+            _httpClient = factory.CreateClient();
+        }
+
+        protected Task<HttpResponseMessage> Get(string requestUri)
+            => _httpClient.GetAsync(requestUri);
+
+        protected Task<HttpResponseMessage> Post<TCommand>(TCommand command, string requestUri)
+            where TCommand : ICommand
+            => _httpClient.PostAsync(requestUri, GetContent(command));
+
+        protected Task<HttpResponseMessage> Delete(string requestUri)
+             => _httpClient.DeleteAsync(requestUri);
+    }
+}
---
+++ UserService.Tests.EndToEnd/Sync/RegisterUserTests.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using Shouldly;
+using System.Net;
+using UserService.Application.Commands;
+using UserService.Application.Dtos;
+using UserService.Tests.EndToEnd.Factories;
+using Xunit;
+
+namespace UserService.Tests.EndToEnd.Sync
+{
+    public class RegisterUserTests : BaseSyncIntegrationTest
+    {
+        #region GLOBAL ARRANGE
+
+        public RegisterUserTests(UserServiceApplicationFactory factory) : base(factory)
+        {
+        }
+
+        #endregion
+
+        //Should return http status code 201 if User is created from the specified parameters in CreateUserCommand.
+        [Fact]
+        public async Task Register_User_Endpoint_Should_Return_Http_Status_Code_Created()
+        {
+            //ARRANGE
+            var createUserCommand = new CreateUserCommand(Guid.NewGuid(), "John Doe", "Johny", "john@mail.com");
+
+            //ACT
+            var response = await Post(createUserCommand, "/user-service/users");
+
+            //ASSERT
+            response.ShouldNotBeNull();
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+            response.Headers.Location.ShouldNotBeNull();
+
+            response.Headers.Location.ToString().ShouldBe($"/user-service/users");
+        }
+
+        //Should return http status code 200 when the previously created User is successfully received from the database.
+        [Fact]
+        public async Task Register_User_Endpoint_Should_Add_User_With_Given_UserId_To_The_Database()
+        {
+            //ARRANGE
+            var createUserCommand = new CreateUserCommand(Guid.NewGuid(), "John Doe", "Johny", "john@mail.com");
+
+            //ACT
+            await Post(createUserCommand, "/user-service/users");
+
+            var response = await Get($"/user-service/users/{createUserCommand.UserId}");
+
+            //ASSERT
+            response.ShouldNotBeNull();
+
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+            var responseSerializedContent = await response.Content.ReadAsStringAsync();
+
+            var userDto = JsonConvert.DeserializeObject<UserDto>(responseSerializedContent);
+
+            userDto.ShouldNotBeNull();
+
+            userDto.UserId.ShouldBe(createUserCommand.UserId.ToString());
+
+            userDto.UserFullName.ShouldBe(createUserCommand.UserFullName);
+
+            userDto.UserNickName.ShouldBe(createUserCommand.UserNickName);
+
+            userDto.UserEmailAddress.ShouldBe(createUserCommand.UserEmailAddress);
+        }
+    }
+}
