3d68ecefbdf92cec07837a3a6e1eea9db5876b59
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:37:26 +0300

    Added implementation for the ITopicReadService in TopicArticleService.Infrastructure/EntityFramework/Services/PostgreTopicReadServices.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/Services/PostgreTopicReadServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TopicArticleService.Application.Services.ReadServices;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.Services
+{
+    internal sealed class PostgreTopicReadServices : ITopicReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+
+        public PostgreTopicReadServices(ReadDbContext readDbContext)
+        {
+            _readDbContext = readDbContext;
+        }
+
+        public Task<bool> ExistsByIdAsync(Guid id)
+            => _readDbContext.Topics.AnyAsync(x => x.TopicId == id);
+    }
+}
