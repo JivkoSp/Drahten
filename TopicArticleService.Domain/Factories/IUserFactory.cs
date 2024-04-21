981c88e0c0899e4b9837c1b68d3b19e358fa4355
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:12:04 +0300

    Added NEW domain factory interface IUserFactory.cs and implementation for it UserFactory.cs in TopicArticleService.Domain/Factories/.

---
+++ TopicArticleService.Domain/Factories/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Factories
+{
+    public interface IUserFactory
+    {
+        User Create(UserID userId);
+    }
+}
---
+++ TopicArticleService.Domain/Factories/UserFactory.cs
@@ -0,0 +1,11 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Factories
+{
+    public sealed class UserFactory : IUserFactory
+    {
+        public User Create(UserID userId)
+            => new User(userId);
+    }
+}
