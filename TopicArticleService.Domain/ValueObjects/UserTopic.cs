3a375f7b5f5e95d93a50ea127fecc5951adcd0ab
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:08:24 +0300

    Added NEW value object: TopicArticleService.Domain/ValueObjects/UserTopic.cs

---
+++ TopicArticleService.Domain/ValueObjects/UserTopic.cs
@@ -0,0 +1,35 @@
+﻿using TopicArticleService.Domain.Exceptions;
+
+namespace TopicArticleService.Domain.ValueObjects
+{
+    public record UserTopic
+    {
+        public UserID UserId { get; }
+        public TopicId TopicId { get; }
+        public DateTimeOffset SubscriptionTime { get; }
+
+        private UserTopic() {}
+
+        public UserTopic(UserID userId, TopicId topicId, DateTimeOffset dateTime)
+        {
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if(topicId == null)
+            {
+                throw new NullTopicIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidUserTopicDateTimeException();
+            }
+
+            UserId = userId;
+            TopicId = topicId;
+            SubscriptionTime = dateTime;
+        }
+    }
+}
