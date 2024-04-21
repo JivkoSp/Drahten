5ddf77335c59743e793edad875340a01c2d05ef1
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:29:24 +0300

    Added entity framework configurations for the User domain entity in TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserConfiguration.cs; Added entity framework configuration for the UserTopic domain value object in TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserTopicConfiguration.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            //Table name
+            builder.ToTable("User");
+
+            //Primary key
+            builder.HasKey(key => key.Id);
+
+            //Property config
+            builder.Property(p => p.Id)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .HasColumnName("UserId");
+
+            //Relationships
+            builder.HasMany(p => p.SubscribedTopics);
+        }
+    }
+}
---
+++ TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserTopicConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class UserTopicConfiguration : IEntityTypeConfiguration<UserTopic>
+    {
+        public void Configure(EntityTypeBuilder<UserTopic> builder)
+        {
+            //Table name
+            builder.ToTable("UserTopic");
+
+            //Primary key
+            builder.HasKey(key => new { key.UserId, key.TopicId });
+
+            //Property config
+            builder.Property(p => p.SubscriptionTime)
+             .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .HasColumnName("UserId")
+                .IsRequired();
+
+            builder.Property(p => p.TopicId)
+                .HasConversion(id => id.Value, id => new TopicId(id))
+                .HasColumnName("TopicId")
+                .IsRequired();
+        }
+    }
+}
