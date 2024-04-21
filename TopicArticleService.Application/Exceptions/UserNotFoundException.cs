0fbf09b670ba7478a7ea13d49941d9d31784ec24
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:16:47 +0300

    Added NEW custom application exception type: TopicArticleService.Application/Exceptions/UserNotFoundException.cs.

---
+++ TopicArticleService.Application/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Application.Exceptions
+{
+    internal class UserNotFoundException : ApplicationException
+    {
+        public UserNotFoundException(Guid userId) 
+            : base(message: $"User #{userId} was NOT found!")
+        {
+        }
+    }
+}
