1dd22237af97b870d284e0c312dd336f307855d5
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:22:51 +0300

    Added NEW automapper profile type: TopicArticleService.Infrastructure/Automapper/Profiles/TopicProfile.cs.

---
+++ TopicArticleService.Infrastructure/Automapper/Profiles/TopicProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Infrastructure.EntityFramework.Models;
+
+namespace TopicArticleService.Infrastructure.Automapper.Profiles
+{
+    internal class TopicProfile : Profile
+    {
+        public TopicProfile()
+        {
+            CreateMap<TopicReadModel, TopicDto>();
+        }
+    }
+}
