00ccc125c8acd61c2235c92af6be365cbd9d6e0e
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:34:24 +0300

    Added implementation for the methods in TopicArticleService.Infrastructure/EntityFramework/Repositories/PostgresTopicRepository.cs.

--- TopicArticleService.Infrastructure/EntityFramework/Repositories/PostgresTopicRepository.cs
+++ TopicArticleService.Infrastructure/EntityFramework/Repositories/PostgresTopicRepository.cs
@@ -1,30 +1,43 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
 using TopicArticleService.Domain.Entities;
 using TopicArticleService.Domain.Repositories;
 using TopicArticleService.Domain.ValueObjects;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
 
 namespace TopicArticleService.Infrastructure.EntityFramework.Repositories
 {
     internal sealed class PostgresTopicRepository : ITopicRepository
     {
-        public Task AddTopicAsync(Topic topic)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly WriteDbContext _writeDbContext;
 
-        public Task DeleteTopicAsync(Topic topic)
+        public PostgresTopicRepository(WriteDbContext writeDbContext)
         {
-            throw new NotImplementedException();
+            _writeDbContext = writeDbContext;
         }
 
         public Task<Topic> GetTopicByIdAsync(TopicId topicId)
+            => _writeDbContext.Topics
+                .Include(x => x.TopicChildren)
+                .SingleOrDefaultAsync(x => x.Id == topicId);
+
+        public async Task AddTopicAsync(Topic topic)
         {
-            throw new NotImplementedException();
+            await _writeDbContext.Topics.AddAsync(topic);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+        public async Task UpdateTopicAsync(Topic topic)
+        {
+            _writeDbContext.Topics.Update(topic);
+
+            await _writeDbContext.SaveChangesAsync();
         }
 
-        public Task UpdateTopicAsync(Topic topic)
+        public async Task DeleteTopicAsync(Topic topic)
         {
-            throw new NotImplementedException();
+            _writeDbContext.Topics.Remove(topic);
+
+            await _writeDbContext.SaveChangesAsync();
         }
     }
 }
