9dba5c13a42cc1449f7301b3299189af87d316a6
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:12:54 +0300

    Added NEW repository interface: TopicArticleService.Domain/Repositories/IUserRepository.cs

---
+++ TopicArticleService.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using TopicArticleService.Domain.Entities;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(UserID userId);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+    }
+}
