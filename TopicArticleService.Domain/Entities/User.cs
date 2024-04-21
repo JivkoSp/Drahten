7209123ee77ba197aca7c270cd431be3d01d8d9f
JivkoSp <atjspasov@gmail.com>
2024-04-21 23:10:41 +0300

    Added NEW domain entity: TopicArticleService.Domain/Entities/User.cs.

---
+++ TopicArticleService.Domain/Entities/User.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using TopicArticleService.Domain.Events;
+using TopicArticleService.Domain.Exceptions;
+using TopicArticleService.Domain.ValueObjects;
+
+namespace TopicArticleService.Domain.Entities
+{
+    public class User : AggregateRoot<UserID>
+    {
+        private List<UserTopic> _subscribedTopics;
+
+        public IReadOnlyCollection<UserTopic> SubscribedTopics
+        {
+            get { return new ReadOnlyCollection<UserTopic>(_subscribedTopics); }
+        }
+
+        private User() {}
+
+        internal User(UserID userId)
+        {
+            Id = userId;
+            _subscribedTopics = new List<UserTopic>();
+        }
+        
+        public void SubscribeToTopic(UserTopic userTopic)
+        {
+            var alreadyExists = _subscribedTopics.Any(x => x.TopicId == userTopic.TopicId);    
+
+            if (alreadyExists)
+            {
+                throw new UserTopicAlreadyExistsException(userTopic.UserId, userTopic.TopicId);
+            }
+
+            _subscribedTopics.Add(userTopic);
+
+            AddEvent(new UserTopicAdded(this, userTopic));
+        }
+    }
+}
