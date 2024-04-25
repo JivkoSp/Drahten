1f176e644f2389b39993ab9eafb74809c9268c43
JivkoSp <atjspasov@gmail.com>
2024-04-25 20:23:02 +0300

    Added new query handler: GetArticleLikesHandler.cs in TopicArticleService.Infrastructure/Queries/Handlers/

---
+++ TopicArticleService.Infrastructure/Queries/Handlers/GetArticleLikesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Application.Queries;
+using TopicArticleService.Application.Queries.Handlers;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+
+namespace TopicArticleService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetArticleLikesHandler : IQueryHandler<GetArticleLikesQuery, List<ArticleLikeDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetArticleLikesHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ArticleLikeDto>> HandleAsync(GetArticleLikesQuery query)
+        {
+            var articleLikeReadModels = await _readDbContext.ArticleLikes
+                .Where(x => x.ArticleId == query.ArticleId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return _mapper.Map<List<ArticleLikeDto>>(articleLikeReadModels);
+        }
+    }
+}
