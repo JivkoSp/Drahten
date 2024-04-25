0bb9a4859ef1495749645c2afbac85906b4829da
JivkoSp <atjspasov@gmail.com>
2024-04-25 20:21:13 +0300

    Added two new query types: GetArticleDislikesQuery.cs and GetArticleLikesQuery.cs in TopicArticleService.Application/Queries/.

---
+++ TopicArticleService.Application/Queries/GetArticleDislikesQuery.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos;
+
+namespace TopicArticleService.Application.Queries
+{
+    public class GetArticleDislikesQuery : IQuery<List<ArticleDislikeDto>>
+    {
+        public string ArticleId { get; set; }
+    }
+}
---
+++ TopicArticleService.Application/Queries/GetArticleLikesQuery.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos;
+
+namespace TopicArticleService.Application.Queries
+{
+    public class GetArticleLikesQuery : IQuery<List<ArticleLikeDto>>
+    {
+        public string ArticleId { get; set; }
+    }
+}
