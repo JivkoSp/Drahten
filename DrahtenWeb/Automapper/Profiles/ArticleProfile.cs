530cdff6651021bddcdfea28ae324d9cd94f4762
JivkoSp <atjspasov@gmail.com>
2024-04-25 22:28:25 +0300

    Added Automapper/ directory in DrahtenWeb/. The directory contains Profiles/ sub directory that contains ArticleProfile.cs automapper profile type.

---
+++ DrahtenWeb/Automapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.Automapper.Profiles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDto, WriteArticleDto>()
+                .ForMember(dest => dest.ArticleId, options => options.MapFrom(source => Guid.Parse(source.ArticleId)));
+        }
+    }
+}
