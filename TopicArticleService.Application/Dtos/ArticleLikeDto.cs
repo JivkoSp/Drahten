586cc0f60dd317b21cf87e2a0557a065bc1eb185
JivkoSp <atjspasov@gmail.com>
2024-04-25 20:11:14 +0300

    Added ArticleId property of type Guid in TopicArticleService.Application/Dtos/ArticleLikeDto.cs.

--- TopicArticleService.Application/Dtos/ArticleLikeDto.cs
+++ TopicArticleService.Application/Dtos/ArticleLikeDto.cs
@@ -3,6 +3,7 @@ namespace TopicArticleService.Application.Dtos
 {
     public class ArticleLikeDto
     {
+        public Guid ArticleId { get; set; }
         public DateTimeOffset DateTime { get; set; }
         public Guid UserId { get; set; }
     }
