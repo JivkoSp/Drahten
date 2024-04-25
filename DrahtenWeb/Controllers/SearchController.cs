5e57c30739a033ddd1fcd97ce039842a7ea0837b
JivkoSp <atjspasov@gmail.com>
2024-04-25 22:35:09 +0300

    Modified the CybersecurityNewsEuropeQuery action method in DrahtenWeb/Controllers/SearchController.cs. The modifications are: Removed the communication with the old UserService.

--- DrahtenWeb/Controllers/SearchController.cs
+++ DrahtenWeb/Controllers/SearchController.cs
@@ -1,9 +1,7 @@
 ﻿using DrahtenWeb.Dtos;
-using DrahtenWeb.Services;
 using DrahtenWeb.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Security.Claims;
@@ -31,7 +29,7 @@ namespace DrahtenWeb.Controllers
 
                 var accessToken = await HttpContext.GetTokenAsync("access_token");
 
-                var response = await _searchService.GetAllDocumentsNewsCybersecurityEurope<ResponseDto>(accessToken ?? "");
+                var response = await _searchService.GetAllDocumentsNewsCybersecurityEurope<ResponseDto>(accessToken);
 
                 if (response != null && response.IsSuccess)
                 {
@@ -47,13 +45,13 @@ namespace DrahtenWeb.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> CybersecurityNewsEuropeQuery(int topicId, string query)
+        public async Task<IActionResult> CybersecurityNewsEuropeQuery(string query)
         {
             try
             {
                 var response = new ResponseDto();
                 var documents = new List<NLPQueryAnswerDto>();
-                var searchedTopicsData = new List<ReadSearchedTopicDataHistoryDto>();
+                //var searchedTopicsData = new List<ReadSearchedTopicDataHistoryDto>();
 
                 //Get the user id.
                 //Here the NameIdentifier claim type represents the user id.
@@ -61,36 +59,11 @@ namespace DrahtenWeb.Controllers
 
                 var accessToken = await HttpContext.GetTokenAsync("access_token");
 
-                response = await _searchService.GetAllMathingDocumentsNewsCybersecurityEurope<ResponseDto>(query, accessToken ?? "");
+                response = await _searchService.GetAllMathingDocumentsNewsCybersecurityEurope<ResponseDto>(query, accessToken);
 
                 if(response != null && response.IsSuccess)
                 {
                     documents = JsonConvert.DeserializeObject<List<NLPQueryAnswerDto>>(Convert.ToString(response.Result));
-                }
-
-                //Get all records for user id from SearchedTopicDataPrivateHist table.
-                //Check if the current query exist in the records
-                //If the current query does not exist register it.
-
-                response = await _userService.GetSearchedTopicsDataPrivateHistory<ResponseDto>(userId ?? "", accessToken ?? "");
-
-                searchedTopicsData = JsonConvert.DeserializeObject<List<ReadSearchedTopicDataHistoryDto>>(Convert.ToString(response.Result));
-
-                var searchedTopicDataList = searchedTopicsData?.Where(x => x.Topic?.TopicId == topicId).ToList();
-
-                var searchedTopicQuery = searchedTopicDataList?.FirstOrDefault(x => x.SearchedData == query);
-
-                if(searchedTopicQuery == null)
-                {
-                    var writeSearchedTopicDataHistoryDto = new WriteSearchedTopicDataHistoryDto
-                    {
-                        SearchedData = query,
-                        SearchTime = DateTime.Now,
-                        TopicId = topicId,
-                        HistoryId = userId ?? ""
-                    };
-
-                    await _userService.RegisterSearchedTopicsDataPrivateHistory<ResponseDto>(writeSearchedTopicDataHistoryDto, accessToken ?? "");
                 }
 
                 return new JsonResult(documents);
