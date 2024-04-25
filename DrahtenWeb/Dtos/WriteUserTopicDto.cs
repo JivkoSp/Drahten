7e1d2279816b3f341bf564ffca00f05619c648de
JivkoSp <atjspasov@gmail.com>
2024-04-25 19:53:54 +0300

    Deleted dto type: DrahtenWeb/Dtos/WriteUserTopicDto.cs.

--- DrahtenWeb/Dtos/WriteUserTopicDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DrahtenWeb.Dtos
-{
-    public class WriteUserTopicDto
-    {
-        public string UserId { get; set; } = string.Empty;
-        public int TopicId { get; set; }
-        public DateTime SubscriptionTime { get; set; }
-    }
-}
