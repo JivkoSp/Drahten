ea1071b368e04a7ee3c3d98135abca9fee1afe49
JivkoSp <atjspasov@gmail.com>
2024-04-25 22:23:44 +0300

    Modified the DrahtenWeb/Services/IServices/IUserService.cs. The modifications are: The interface is now defining methods that will talk with the UserService (rather than the old UserService).

--- DrahtenWeb/Services/IServices/IUserService.cs
+++ DrahtenWeb/Services/IServices/IUserService.cs
@@ -1,36 +1,21 @@
-﻿using DrahtenWeb.Dtos;
+﻿using DrahtenWeb.Dtos.UserService;
 
 namespace DrahtenWeb.Services.IServices
 {
     public interface IUserService : IBaseService
     {
-        Task<TEntity> GetUserById<TEntity>(string userId, string accessToken);
-        Task<TEntity> RegisterUser<TEntity>(WriteUserDto user, string accessToken);
-        Task<TEntity> RegisterUserTopic<TEntity>(WriteUserTopicDto user, string accessToken);
-        Task<TEntity> GetTopics<TEntity>(string accessToken);
-        Task<TEntity> GetRootTopicWithChildren<TEntity>(int topicId, string accessToken);
-        Task<TEntity> GetUserTopics<TEntity>(string userId, string accessToken);
-        Task<TEntity> RegisterArticle<TEntity>(WriteArticleDto article, string accessToken);
-        Task<TEntity> RegisterUserArticle<TEntity>(WriteUserArticleDto userArticle, string accessToken);
-        Task<TEntity> GetArticle<TEntity>(string articleId, string accessToken);
-        Task<TEntity> GetArticleLikes<TEntity>(string articleId, string accessToken);
-        Task<TEntity> RegisterArticleLike<TEntity>(WriteArticleLikeDto articleLike, string accessToken);
-        Task<TEntity> GetArticleComments<TEntity>(string articleId, string accessToken);
-        Task<TEntity> RegisterArticleComment<TEntity>(WriteArticleCommentDto articleComment, string accessToken);
-        Task<TEntity> RegisterArticleCommentThumbsUp<TEntity>(WriteArticleCommentThumbsUpDto articleCommentThumbsUp, string accessToken);
-        Task<TEntity> RegisterArticleCommentThumbsDown<TEntity>(WriteArticleCommentThumbsDownDto articleCommentThumbsDown, string accessToken);
-        Task<TEntity> GetArticleCommentThumbsUp<TEntity>(string articleId, int commentId, string accessToken);
-        Task<TEntity> GetArticleCommentThumbsDown<TEntity>(string articleId, int commentId, string accessToken);
-        Task<TEntity> DeleteArticleCommentThumbsUp<TEntity>(string articleId, int articleCommentId, string userId, string accessToken);
-        Task<TEntity> DeleteArticleCommentThumbsDown<TEntity>(string articleId, int articleCommentId, string userId, string accessToken);
-        Task<TEntity> GetUsersRelatedToArticle<TEntity>(string articleId, string accessToken);
-        Task<TEntity> GetUserPrivateHistory<TEntity>(string userId, string accessToken);
-        Task<TEntity> CreateUserPrivateHistory<TEntity>(WritePrivateHistoryDto writePrivateHistoryDto, string accessToken);
-        Task<TEntity> GetUserPublicHistory<TEntity>(string userId, string accessToken);
-        Task<TEntity> CreateUserPublicHistory<TEntity>(WritePublicHistoryDto writePublicHistoryDto, string accessToken);
-        Task<TEntity> GetViewedArticlesPrivateHistory<TEntity>(string userId, string accessToken);
-        Task<TEntity> RegisterViewedArticlePrivateHistory<TEntity>(WriteViewedArticleHistoryDto writeViewedArticleHistoryDto, string accessToken);
-        Task<TEntity> GetSearchedTopicsDataPrivateHistory<TEntity>(string userId, string accessToken);
-        Task<TEntity> RegisterSearchedTopicsDataPrivateHistory<TEntity>(WriteSearchedTopicDataHistoryDto writeSearchedTopicDataHistoryDto, string accessToken);
+        Task<TEntity> GetUserByIdAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetIssuedBansByUserAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetReceivedBansByUserAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetIssuedContactRequestsByUserAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetReceivedContactRequestsByUserAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> RegisterUserAsync<TEntity>(UserDto userDto, string accessToken);
+        Task<TEntity> RegisterBannedUserAsync<TEntity>(BanUserDto banUserDto, string accessToken);
+        Task<TEntity> RegisterContactRequestAsync<TEntity>(ContactRequestDto contactRequestDto, string accessToken);
+        Task<TEntity> RegisterUserActivityAsync<TEntity>(UserActivityDto userActivityDto, string accessToken);
+        Task<TEntity> UpdateContactRequestMessageAsync<TEntity>(ContactRequestDto contactRequestDto, string accessToken);
+        Task<TEntity> RemoveIssuedContactRequestAsync<TEntity>(Guid issuerUserId, Guid receiverUserId, string accessToken);
+        Task<TEntity> RemoveReceivedContactRequestAsync<TEntity>(Guid receiverUserId, Guid issuerUserId, string accessToken);
+        Task<TEntity> RemoveBannedUserAsync<TEntity>(Guid issuerUserId, Guid receiverUserId, string accessToken);
     }
 }
