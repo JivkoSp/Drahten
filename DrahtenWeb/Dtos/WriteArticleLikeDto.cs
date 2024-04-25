d3bb4a7fadcd35a0fe7120d6cea14181acef85c6
JivkoSp <atjspasov@gmail.com>
2024-04-25 19:44:44 +0300

    Deleted dto type: DrahtenWeb/Dtos/WriteArticleLikeDto.cs.

--- DrahtenWeb/Dtos/WriteArticleLikeDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DrahtenWeb.Dtos
-{
-    public class WriteArticleLikeDto
-    {
-        public string ArticleId { get; set; } = string.Empty;
-        public string UserId {  get; set; } = string.Empty; 
-    }
-}
