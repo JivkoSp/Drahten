b3fab8abfb52cc5fba812b8752f34445085b9d74
JivkoSp <atjspasov@gmail.com>
2024-04-25 20:22:07 +0300

    Added new query handler: GetArticleDislikesHandler.cs in TopicArticleService.Infrastructure/Queries/Handlers/.

---
+++ TopicArticleService.Infrastructure/Queries/Handlers/GetArticleDislikesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Application.Queries;
+using TopicArticleService.Application.Queries.Handlers;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+
+namespace TopicArticleService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetArticleDislikesHandler : IQueryHandler<GetArticleDislikesQuery, List<ArticleDislikeDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetArticleDislikesHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ArticleDislikeDto>> HandleAsync(GetArticleDislikesQuery query)
+        {
+            var articleDislikeReadModels = await _readDbContext.ArticleDislikes
+               .Where(x => x.ArticleId == query.ArticleId)
+               .AsNoTracking()
+               .SingleOrDefaultAsync();
+
+            return _mapper.Map<List<ArticleDislikeDto>>(articleDislikeReadModels);
+        }
+    }
+}
