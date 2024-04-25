a1da4ce5e21dd3871e3ff1c781b0b07d77b3b5b2
JivkoSp <atjspasov@gmail.com>
2024-04-25 19:56:23 +0300

    Deleted dto type: DrahtenWeb/Dtos/WriteViewedArticleHistoryDto.cs

--- DrahtenWeb/Dtos/WriteViewedArticleHistoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DrahtenWeb.Dtos
-{
-    public class WriteViewedArticleHistoryDto
-    {
-        public string ArticleId { get; set; } = string.Empty;
-        public DateTime ViewTime { get; set; }
-        public string HistoryId { get; set; } = string.Empty;
-    }
-}
