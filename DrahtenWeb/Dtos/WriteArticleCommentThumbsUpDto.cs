f78d250347a4000ba764a128489ec9e51828f92b
JivkoSp <atjspasov@gmail.com>
2024-04-25 19:33:17 +0300

    DrahtenWeb/Dtos/WriteArticleCommentThumbsUpDto.cs.

--- DrahtenWeb/Dtos/WriteArticleCommentThumbsUpDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DrahtenWeb.Dtos
-{
-    public class WriteArticleCommentThumbsUpDto
-    {
-        public int ArticleCommentId { get; set; }
-        public string ArticleId { get; set; } = string.Empty;
-        public string UserId { get; set; } = string.Empty;
-    }
-}
