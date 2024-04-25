7b25afb8ff73aab3768dfdb1a86831e13061e202
JivkoSp <atjspasov@gmail.com>
2024-04-25 19:52:38 +0300

    Deleted dto type: DrahtenWeb/Dtos/ReadArticleCommentThumbsUpDto.cs.

--- DrahtenWeb/Dtos/ReadArticleCommentThumbsUpDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DrahtenWeb.Dtos
-{
-    public class ReadArticleCommentThumbsUpDto
-    {
-        public int ArticleCommentId { get; set; }
-        public string ArticleId { get; set; } = string.Empty;
-        public string UserId { get; set; } = string.Empty;
-    }
-}
