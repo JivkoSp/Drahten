e9028320cb2c3939efef7a0b11f4377188a08a1f
JivkoSp <atjspasov@gmail.com>
2024-04-25 20:19:50 +0300

    Modified the TopicArticleService.Application/Dtos/UserDto.cs. The modifications are: Changed the UserId property type from string to Guid.

--- TopicArticleService.Application/Dtos/UserDto.cs
+++ TopicArticleService.Application/Dtos/UserDto.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TopicArticleService.Application.Dtos
 {
     public class UserDto
     {
-        public string UserId { get; set; }
+        public Guid UserId { get; set; }
     }
 }
