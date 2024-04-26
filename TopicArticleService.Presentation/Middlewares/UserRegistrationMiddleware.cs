861ce361a6d8e519dc88532019dd1cd94429ac8f
JivkoSp <atjspasov@gmail.com>
2024-04-26 21:23:24 +0300

    Added NEW custom middleware: TopicArticleService.Presentation/Middlewares/UserRegistrationMiddleware.cs. The middleware ensures that when a loged in user is accessing the TopicArticleService, the user will be synchronized with that service.

---
+++ TopicArticleService.Presentation/Middlewares/UserRegistrationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using TopicArticleService.Application.Commands;
+using TopicArticleService.Infrastructure.UserRegistration;
+
+namespace TopicArticleService.Presentation.Middlewares
+{
+    public sealed class UserRegistrationMiddleware : IMiddleware
+    {
+        private readonly IUserSynchronizer _userSynchronizer;
+
+        public UserRegistrationMiddleware(IUserSynchronizer userSynchronizer)
+        {
+            _userSynchronizer = userSynchronizer;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var registerUserCommand = new RegisterUserCommand(userId);
+
+            //Register the user in the database if it does not already exist.
+            await _userSynchronizer.SynchronizeUserAsync(registerUserCommand);
+
+            await next(context);
+        }
+    }
+}
