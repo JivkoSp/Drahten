98a2c5e765a53621bda25325faa95375034402ae
JivkoSp <atjspasov@gmail.com>
2024-04-26 21:21:01 +0300

    Added UserRegistration/ directory in TopicArticleService.Infrastructure/. The directory contains IUserSynchronizer.cs and UserSynchronizer.cs.

---
+++ TopicArticleService.Infrastructure/UserRegistration/IUserSynchronizer.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Commands;
+
+namespace TopicArticleService.Infrastructure.UserRegistration
+{
+    public interface IUserSynchronizer
+    {
+        Task SynchronizeUserAsync(RegisterUserCommand registerUserCommand);
+    }
+}
---
+++ TopicArticleService.Infrastructure/UserRegistration/UserSynchronizer.cs
@@ -0,0 +1,28 @@
+﻿using TopicArticleService.Application.Commands;
+using TopicArticleService.Application.Commands.Dispatcher;
+using TopicArticleService.Application.Services.ReadServices;
+
+namespace TopicArticleService.Infrastructure.UserRegistration
+{
+    public sealed class UserSynchronizer : IUserSynchronizer
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IUserReadService _userReadService;
+
+        public UserSynchronizer(ICommandDispatcher commandDispatcher, IUserReadService userReadService)
+        {
+            _commandDispatcher = commandDispatcher;
+            _userReadService = userReadService;
+        }
+
+        public async Task SynchronizeUserAsync(RegisterUserCommand registerUserCommand)
+        {
+            var alreadyExists = await _userReadService.ExistsByIdAsync(registerUserCommand.UserId);
+
+            if (alreadyExists == false)
+            {
+                await _commandDispatcher.DispatchAsync(registerUserCommand);
+            }
+        }
+    }
+}
