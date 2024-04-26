f0903b0c5aa57ad9b6f9762322ce657814a57a7d
JivkoSp <atjspasov@gmail.com>
2024-04-26 10:20:45 +0300

    Added Extensions/ directory in DrahtenWeb. The directory contains ResponseDtoExtensions.cs.

---
+++ DrahtenWeb/Extensions/ResponseDtoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DrahtenWeb.Dtos;
+using Newtonsoft.Json;
+
+namespace DrahtenWeb.Extensions
+{
+    public static class ResponseDtoExtensions
+    {
+        public static T Map<T>(this ResponseDto responseDto) where T : new ()
+        {
+            var result = new T();
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                result = JsonConvert.DeserializeObject<T>(Convert.ToString(responseDto.Result));
+            }
+
+            return result;
+        }
+    }
+}
