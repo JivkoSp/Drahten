a9e0d8d2a7238f2a54a2dba5ced7e2b4fd942d97
JivkoSp <atjspasov@gmail.com>
2024-05-02 13:26:26 +0300

    Added ToSnakeCase string extension method in TopicArticleService.Infrastructure/Extensions/StringExtensions.cs.

---
+++ TopicArticleService.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace TopicArticleService.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToSnakeCase(this string value) 
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(char.ToLower(value[0]));
+
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                {
+                    builder.Append('_');
+                    builder.Append(char.ToLower(value[i]));
+                    continue;
+                }
+
+                builder.Append(value[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
