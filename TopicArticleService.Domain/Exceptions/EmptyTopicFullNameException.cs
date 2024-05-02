5898f93e3adc68c573227d572c925939fbc7a1f3
JivkoSp <atjspasov@gmail.com>
2024-05-02 12:20:17 +0300

    Added NEW custom domain exception: TopicArticleService.Domain/Exceptions/EmptyTopicFullNameException.cs.

---
+++ TopicArticleService.Domain/Exceptions/EmptyTopicFullNameException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TopicArticleService.Domain.Exceptions
+{
+    public sealed class EmptyTopicFullNameException : DomainException
+    {
+        public EmptyTopicFullNameException() : base(message: "Topic full name cannot be empty!")
+        {
+        }
+    }
+}
