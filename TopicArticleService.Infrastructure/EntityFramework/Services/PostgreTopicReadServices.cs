f3f733ab1cc442bf052dd41e92644bf1bcfe3529
JivkoSp <atjspasov@gmail.com>
2024-05-02 12:42:35 +0300

    Added implementation for the newly added GetTopicByNameAsync method in TopicArticleService.Infrastructure/EntityFramework/Services/PostgreTopicReadServices.cs.

--- TopicArticleService.Infrastructure/EntityFramework/Services/PostgreTopicReadServices.cs
+++ TopicArticleService.Infrastructure/EntityFramework/Services/PostgreTopicReadServices.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using TopicArticleService.Application.Dtos;
 using TopicArticleService.Application.Services.ReadServices;
 using TopicArticleService.Infrastructure.EntityFramework.Contexts;
 
@@ -7,13 +9,23 @@ namespace TopicArticleService.Infrastructure.EntityFramework.Services
     internal sealed class PostgreTopicReadServices : ITopicReadService
     {
         private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
 
-        public PostgreTopicReadServices(ReadDbContext readDbContext)
+        public PostgreTopicReadServices(ReadDbContext readDbContext, IMapper mapper)
         {
             _readDbContext = readDbContext;
+            _mapper = mapper;
         }
 
         public Task<bool> ExistsByIdAsync(Guid id)
             => _readDbContext.Topics.AnyAsync(x => x.TopicId == id);
+
+        public async Task<TopicDto> GetTopicByNameAsync(string topicName)
+        {
+            var topicReadModel = await _readDbContext.Topics
+            .FirstOrDefaultAsync(x => x.TopicFullName == topicName);
+
+            return _mapper.Map<TopicDto>(topicReadModel);
+        }
     }
 }
