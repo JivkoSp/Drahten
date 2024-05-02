07ae3bf7c8edcc66103242a4908998d6456334b6
JivkoSp <atjspasov@gmail.com>
2024-05-02 12:22:18 +0300

    Added NEW domain value object: TopicArticleService.Domain/ValueObjects/TopicFullName.cs.

---
+++ TopicArticleService.Domain/ValueObjects/TopicFullName.cs
@@ -0,0 +1,27 @@
+﻿using TopicArticleService.Domain.Exceptions;
+
+namespace TopicArticleService.Domain.ValueObjects
+{
+    public record TopicFullName
+    {
+        public string Value { get; }
+
+        public TopicFullName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptyTopicFullNameException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to String.
+        public static implicit operator string(TopicFullName name)
+            => name.Value;
+
+        //Conversion from String to ValueObject.
+        public static implicit operator TopicFullName(string name)
+            => new TopicFullName(name);
+    }
+}
