c512d2d68e15bc8698b42e71e664625d548bf5f3
JivkoSp <atjspasov@gmail.com>
2024-05-02 12:40:51 +0300

    Added GetTopicByNameAsync method in TopicArticleService.Application/Services/ReadServices/ITopicReadService.cs.

--- TopicArticleService.Application/Services/ReadServices/ITopicReadService.cs
+++ TopicArticleService.Application/Services/ReadServices/ITopicReadService.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using TopicArticleService.Application.Dtos;
+
 namespace TopicArticleService.Application.Services.ReadServices
 {
     public interface ITopicReadService
     {
         Task<bool> ExistsByIdAsync(Guid id);
+        Task<TopicDto> GetTopicByNameAsync(string topicName);
     }
 }
