08cc5cc5f582d2be40c7252399d9d16e5bc4a865
JivkoSp <atjspasov@gmail.com>
2024-05-03 22:48:42 +0300

    Added Articles, ArticleComments and UsersRelatedToArticle properties in DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs.

--- DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs
+++ DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs
@@ -4,7 +4,10 @@ namespace DrahtenWeb.ViewModels
 {
     public class UserSearchOptionsViewModel
     {
+        public List<ArticleDto> Articles { get; set; } = new List<ArticleDto>();
         public List<TopicDto> Topics { get; set; } = new List<TopicDto>();
+        public Dictionary<string, List<ReadArticleCommentDto>> ArticleComments { get; set; } = new Dictionary<string, List<ReadArticleCommentDto>>();
+        public Dictionary<string, List<ReadUserArticleDto>> UsersRelatedToArticle { get; set; } = new Dictionary<string, List<ReadUserArticleDto>>();
         public List<UserTopicDto> UserTopics { get; set; } = new List<UserTopicDto>();
     }
 }
