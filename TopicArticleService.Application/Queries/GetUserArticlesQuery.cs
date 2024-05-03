4c2bd2f8b7020a17c3bf196acbac84f9acb32ed0
JivkoSp <atjspasov@gmail.com>
2024-05-03 21:50:35 +0300

    Added NEW query type in TopicArticleService.Application/Queries/GetUserArticlesQuery.cs.

---
+++ TopicArticleService.Application/Queries/GetUserArticlesQuery.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos;
+
+namespace TopicArticleService.Application.Queries
+{
+    public class GetUserArticlesQuery : IQuery<List<ArticleDto>>
+    {
+        public string UserId { get; set; }
+    }
+}
