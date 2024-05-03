4f08efe834d1ed6ede89e7f9044d66fa984c4065
JivkoSp <atjspasov@gmail.com>
2024-05-03 22:43:27 +0300

    Added SplitSnakeCase string extension method in DrahtenWeb/Extensions/StringExtensions.cs.

---
+++ DrahtenWeb/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace DrahtenWeb.Extensions
+{
+    public static class StringExtensions
+    {
+        //Splits snake_case string to separate words. The first word begins with upper letter.
+        //For example: Given the word: cyber_attacks the result would be: Cyber attacks.
+        public static string SplitSnakeCase(this string value) 
+        {
+            if (string.IsNullOrEmpty(value))
+                return value;
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(char.ToUpper(value[0]));
+
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (value[i] == '_')
+                {
+                    builder.Append(' ');
+                    continue;
+                }
+
+                builder.Append(value[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
