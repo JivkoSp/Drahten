542dff68308c350c2c3852ab2318180b2af29086
JivkoSp <atjspasov@gmail.com>
2024-05-03 21:56:58 +0300

    Modified the conversion of the ArticleId in TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleConfiguration.cs. The conversion now formats the Guid without hyphens (or dashes).

--- TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleConfiguration.cs
+++ TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleConfiguration.cs
@@ -32,8 +32,20 @@ namespace TopicArticleService.Infrastructure.EntityFramework.ModelConfiguration.
 
             //Property config - Start
 
+            #region ConversionToUUID
+            // *** IMPORTANT! ***
+            // Convert Guid to string without hyphens.
+            // Explanation: UUIDs are commonly represented as a 32-character hexadecimal string (such as "37955c6317e6869e90fe0029564078c8")
+            // or as a 36-character string with hyphens separating groups of characters (such as "37955c63-17e6-869e-90fe-0029564078c8").
+            // The Id is NOT stored as Guid in the database, because PostgreSQL will automatically format it with hyphens for readability
+            // and standardization. This will cause problems when accessing documents with their Id's from the SearchService database store
+            // (a Elasticserach instance). The Elasticsearch instance will expect the Id's to be in a 32-character hexadecimal format and 
+            // will not be able to find the document's if it receaves a 36-character hexadecimal format for the Id.
+            // ---------------------------------------
+            // !!! The "N" in the ToString method is used to format the Guid without hyphens (or dashes).
+            #endregion
             builder.Property(p => p.Id)
-                .HasConversion(id => id.Value.ToString(), id => new ArticleID(Guid.Parse(id)))
+                .HasConversion(id => id.Value.ToString("N"), id => new ArticleID(Guid.ParseExact(id, "N"))) 
                 .HasColumnName("ArticleId");
 
             builder.Property(typeof(ArticlePrevTitle), "_prevTitle")
