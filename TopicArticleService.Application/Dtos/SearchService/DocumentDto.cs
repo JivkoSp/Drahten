f80782f0e5850031817a53efcd90e6e38229f50a
JivkoSp <atjspasov@gmail.com>
2024-05-11 14:35:26 +0300

    Added NEW dto type: TopicArticleService.Application/Dtos/SearchService/DocumentDto.cs.

---
+++ TopicArticleService.Application/Dtos/SearchService/DocumentDto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TopicArticleService.Application.Dtos.SearchService
+{
+    public class DocumentDto
+    {
+        public string DocumentId { get; set; }
+        public string PrevTitle { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PublishingDate { get; set; }
+        public string Author { get; set; }
+        public string Link { get; set; }
+        public string TopicName { get; set; }
+    }
+}
