eb7588e9c58a660cceabf99cf66dc482689d4f90
JivkoSp <atjspasov@gmail.com>
2024-05-11 12:13:54 +0300

    Modified the message publishing to RabbitMq in Drahten_Services_SerarchService/app/asyncDataServices/messageBusClient.py. The message publishing logic is now defined in MessageBusPublisher class. Added additional method PublishDocumentForSimilarityCheck in that class.

--- Drahten_Services_SerarchService/app/asyncDataServices/messageBusClient.py
+++ Drahten_Services_SerarchService/app/asyncDataServices/messageBusClient.py
@@ -1,29 +1,37 @@
 import pika
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq_servicebus'))
+class MessageBusPublisher:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq_servicebus'))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='search_service', exchange_type='direct')
+        print("\n\n*** SearchService INITIALIZED THE MESSAGE PUBLISHER ***\n\n")
 
-channel = connection.channel()
+    def PublishDocumentForSimilarityCheck(self, document, spiderName):
+        document = document.to_dict()
+        self.channel.basic_publish(
+            exchange='search_service',
+            routing_key='search_service.similaritycheck',
+            body=json.dumps({'DocumentId' : document['id'],
+                             'PrevTitle': document['meta']['article_prev_title'],
+                             'Title' : document['meta']['article_title'],
+                             'Content' : document['meta']['article_data'],
+                             'PublishingDate' : document['meta']['article_published_date'],
+                             'Author' : document['meta']['article_author'],
+                             'Link' : document['meta']['article_link'],
+                             'TopicName' : spiderName,
+                             'Event': 'DocumentSimilarityCheck'})
+        )
 
-channel.exchange_declare(
-    exchange='search_service',
-    exchange_type='direct'
-)
+        print("\n\n*** PUBLISHED MESSAGE TO RABBITMQ ***\n\n")
 
-def publishSearchEvent():
-    search_info = {
-    'Data': 'some search data...',
-    'Event': 'Search_Information_Published'
-    }
-    channel.basic_publish(
-    exchange='search_service',
-    routing_key='search_service.event',
-    body=json.dumps({
-                    'Data': search_info['Data'],
-                    'Event': search_info['Event']
-                    })
-    )
-
-    print("\n\n#### SEND MESSAGE TO RABBITMQ ####\n\n")
-
-
+    def PublishSearchEvent(self):
+        search_info = {
+            'Data': 'some search data...',
+            'Event': 'Search_Information_Published'
+        }
+        self.channel.basic_publish(
+            exchange='search_service',
+            routing_key='search_service.event',
+            body=json.dumps({'Data': search_info['Data'], 'Event': search_info['Event']}))
