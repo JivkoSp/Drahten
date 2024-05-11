d1116f86b908894bcdc7b6474947aef7f5ea956e
JivkoSp <atjspasov@gmail.com>
2024-05-11 14:21:22 +0300

    Added implementation for the DocumentSimilarityCheckAsync method in TopicArticleService.Infrastructure/SyncDataServices/Grpc/SearchServiceDataClient.cs.

--- TopicArticleService.Infrastructure/SyncDataServices/Grpc/SearchServiceDataClient.cs
+++ TopicArticleService.Infrastructure/SyncDataServices/Grpc/SearchServiceDataClient.cs
@@ -1,15 +1,19 @@
-﻿using Grpc.Net.Client;
+﻿using AutoMapper;
+using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
+using TopicArticleService.Application.Dtos.SearchService;
 
 namespace TopicArticleService.Infrastructure.SyncDataServices.Grpc
 {
     internal sealed class SearchServiceDataClient : ISearchServiceDataClient
     {
         private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
 
-        public SearchServiceDataClient(IConfiguration configuration)
+        public SearchServiceDataClient(IConfiguration configuration, IMapper mapper)
         {
             _configuration = configuration;
+            _mapper = mapper;
         }
 
         //Returns all documents that are available in the SearchService.
@@ -28,6 +32,26 @@ namespace TopicArticleService.Infrastructure.SyncDataServices.Grpc
                 var currentResponse = reply.ResponseStream.Current;
 
                 yield return (currentResponse.DocumentTopic, currentResponse.Document);
+            }
+        }
+
+        public async IAsyncEnumerable<double> DocumentSimilarityCheckAsync(DocumentDto documentDto)
+        {
+            Console.WriteLine($"\n\n--> Calling the SearchService GRPC Server at address: {_configuration["GrpcSearchService"]}.\n\n");
+
+            using var channel = GrpcChannel.ForAddress(_configuration["GrpcSearchService"]);
+
+            var client = new DocumentSimilarityCheck.DocumentSimilarityCheckClient(channel);
+
+            var document = _mapper.Map<Document>(documentDto);
+
+            var reply = client.CheckDocumentSimilarity(document);
+
+            while(await reply.ResponseStream.MoveNext(CancellationToken.None))
+            {
+                var currentResponse = reply.ResponseStream.Current;
+
+                yield return currentResponse.SimilarityScore;
             }
         }
     }
