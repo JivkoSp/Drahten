8961a448a3a7ad3fe8dabdb39f33cc2a4b577155
JivkoSp <atjspasov@gmail.com>
2024-05-11 14:20:07 +0300

    Added DocumentSimilarityCheckAsync method in TopicArticleService.Infrastructure/SyncDataServices/Grpc/ISearchServiceDataClient.cs.

--- TopicArticleService.Infrastructure/SyncDataServices/Grpc/ISearchServiceDataClient.cs
+++ TopicArticleService.Infrastructure/SyncDataServices/Grpc/ISearchServiceDataClient.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using TopicArticleService.Application.Dtos.SearchService;
+
 namespace TopicArticleService.Infrastructure.SyncDataServices.Grpc
 {
     internal interface ISearchServiceDataClient
     {
         IAsyncEnumerable<(string, Document)> GetDocumentsAsync();
+        IAsyncEnumerable<double> DocumentSimilarityCheckAsync(DocumentDto documentDto);
     }
 }
