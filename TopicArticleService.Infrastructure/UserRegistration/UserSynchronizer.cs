74c0b6a1521033e509d7deb58f3beec9dbbadde0
JivkoSp <atjspasov@gmail.com>
2024-05-15 17:05:26 +0300

    Added ILogger<UserSynchronizer> to TopicArticleService.Infrastructure/UserRegistration/UserSynchronizer.cs.

--- TopicArticleService.Infrastructure/UserRegistration/UserSynchronizer.cs
+++ TopicArticleService.Infrastructure/UserRegistration/UserSynchronizer.cs
@@ -1,4 +1,5 @@
-﻿using TopicArticleService.Application.Commands;
+﻿using Microsoft.Extensions.Logging;
+using TopicArticleService.Application.Commands;
 using TopicArticleService.Application.Commands.Dispatcher;
 using TopicArticleService.Application.Services.ReadServices;
 
@@ -8,11 +9,13 @@ namespace TopicArticleService.Infrastructure.UserRegistration
     {
         private readonly ICommandDispatcher _commandDispatcher;
         private readonly IUserReadService _userReadService;
+        private readonly ILogger<UserSynchronizer> _logger;
 
-        public UserSynchronizer(ICommandDispatcher commandDispatcher, IUserReadService userReadService)
+        public UserSynchronizer(ICommandDispatcher commandDispatcher, IUserReadService userReadService, ILogger<UserSynchronizer> logger)
         {
             _commandDispatcher = commandDispatcher;
             _userReadService = userReadService;
+            _logger = logger;
         }
 
         public async Task SynchronizeUserAsync(RegisterUserCommand registerUserCommand)
@@ -21,7 +24,11 @@ namespace TopicArticleService.Infrastructure.UserRegistration
 
             if (alreadyExists == false)
             {
+                _logger.LogInformation($"--> User with ID: {registerUserCommand.UserId} is ATTEMPTING to synchronize with TopicArticleService.");
+
                 await _commandDispatcher.DispatchAsync(registerUserCommand);
+
+                _logger.LogInformation($"--> User with ID: {registerUserCommand.UserId} is SUCCESSFULLY synchronized with TopicArticleService.");
             }
         }
     }
