1e0cce4516614aabb947afc03ef10ba3b178f86b
JivkoSp <atjspasov@gmail.com>
2024-05-15 16:58:37 +0300

    Added TopicArticleService.Infrastructure/AsyncDataServices/IMessageBusPublisher.cs interface with PublishNewDocument method.

---
+++ TopicArticleService.Infrastructure/AsyncDataServices/IMessageBusPublisher.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos.SearchService;
+
+namespace TopicArticleService.Infrastructure.AsyncDataServices
+{
+    public interface IMessageBusPublisher
+    {
+        void PublishNewDocument(DocumentDto documentDto);
+    }
+}
