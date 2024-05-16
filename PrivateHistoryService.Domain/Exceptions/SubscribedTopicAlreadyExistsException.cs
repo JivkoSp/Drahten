199f35ffc62cb6f06c4a6b2bb67242412c7cdfc7
JivkoSp <atjspasov@gmail.com>
2024-05-16 21:43:35 +0300

    Added NEW custom domain exception: PrivateHistoryService.Domain/Exceptions/SubscribedTopicAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/SubscribedTopicAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class SubscribedTopicAlreadyExistsException : DomainException
+    {
+        internal SubscribedTopicAlreadyExistsException(Guid topicId, Guid userId) 
+            : base(message: $"There is already topic subscription for topic #{topicId} by user #{userId}!")
+        {
+        }
+    }
+}
