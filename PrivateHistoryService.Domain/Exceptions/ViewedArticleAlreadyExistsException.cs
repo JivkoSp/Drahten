17317d62e4ec9ebd4cfed921006c4f9b51d8f068
JivkoSp <atjspasov@gmail.com>
2024-05-16 20:40:55 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/ViewedArticleAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/ViewedArticleAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class ViewedArticleAlreadyExistsException : DomainException
+    {
+        public ViewedArticleAlreadyExistsException(Guid articleId, Guid userId, DateTimeOffset dateTime) 
+            : base(message: $"Article #{articleId} is already viewed by user: #{userId} on {dateTime}.")
+        {
+        }
+    }
+}
