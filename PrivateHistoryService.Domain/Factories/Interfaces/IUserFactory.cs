78bdd04819dcb0291b86a75b44d52f679330e218
JivkoSp <atjspasov@gmail.com>
2024-05-17 21:58:14 +0300

    Added Factories/ directory in PrivateHistoryService.Domain/. This directory contains UserFactory.cs class and Interfaces/ sub-directory. The UserFactory.cs is a factory for User domain entities. The Interfaces/ sub-directory contains IUserFactory.cs interfaces that defines the methods that the UserFactory.cs implements.

---
+++ PrivateHistoryService.Domain/Factories/Interfaces/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Factories.Interfaces
+{
+    public interface IUserFactory
+    {
+        User Create(UserID userId);
+    }
+}
---
+++ PrivateHistoryService.Domain/Factories/UserFactory.cs
@@ -0,0 +1,12 @@
+﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.Factories.Interfaces;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Factories
+{
+    public sealed class UserFactory : IUserFactory
+    {
+        public User Create(UserID userId)
+            => new User(userId);
+    }
+}
