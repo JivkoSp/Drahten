fa34d96fba93aafcd1655db602dd509aecc7743b
JivkoSp <atjspasov@gmail.com>
2024-05-17 14:49:39 +0300

    Added NEW custom domain event type: PrivateHistoryService.Domain/Events/DislikedArticleCommentAdded.cs.

---
+++ PrivateHistoryService.Domain/Events/DislikedArticleCommentAdded.cs
@@ -0,0 +1,7 @@
+﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Events
+{
+    public record DislikedArticleCommentAdded(User User, DislikedArticleComment DislikedArticleComment) : IDomainEvent;
+}
