0f784e74f99fc05317bff937ed9470ace48fd483
JivkoSp <atjspasov@gmail.com>
2024-05-17 11:01:31 +0300

    Added two NEW domain exception types in PrivateHistoryService.Domain/Exceptions/. The domain exceptions are: SearchedArticleDataAlreadyExistsException.cs and SearchedTopicDataAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/SearchedArticleDataAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class SearchedArticleDataAlreadyExistsException : DomainException
+    {
+        internal SearchedArticleDataAlreadyExistsException(Guid userId, DateTimeOffset dateTime) 
+            : base(message: $"This article data is already searched by user #{userId} on {dateTime}!")
+        {
+        }
+    }
+}
