d6348be999e6e5b7678be2b63c2bb8252197f51d
JivkoSp <atjspasov@gmail.com>
2024-05-17 19:13:04 +0300

    Added two NEW custom domain event types in PrivateHistoryService.Domain/Events/. The events are: CommentedArticleRemoved.cs and ViewedUserRemoved.cs.

---
+++ PrivateHistoryService.Domain/Events/CommentedArticleRemoved.cs
@@ -0,0 +1,7 @@
+﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Events
+{
+    public record CommentedArticleRemoved(User User, CommentedArticle CommentedArticle) : IDomainEvent;
+}
---
+++ PrivateHistoryService.Domain/Events/ViewedUserRemoved.cs
@@ -0,0 +1,7 @@
+﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Events
+{
+    public record ViewedUserRemoved(User User, ViewedUser ViewedUser) : IDomainEvent;
+}
