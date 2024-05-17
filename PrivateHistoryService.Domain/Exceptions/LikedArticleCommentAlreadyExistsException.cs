7b18691ac883e02cd1b44dfdd5c49139bee6331d
JivkoSp <atjspasov@gmail.com>
2024-05-17 14:02:06 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/LikedArticleCommentAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/LikedArticleCommentAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class LikedArticleCommentAlreadyExistsException : DomainException
+    {
+        internal LikedArticleCommentAlreadyExistsException(Guid articleCommentID, Guid userId) 
+            : base(message: $"There is already like for article comment #{articleCommentID} from user #{userId}!")
+        {
+        }
+    }
+}
