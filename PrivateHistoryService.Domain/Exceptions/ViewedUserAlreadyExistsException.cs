5d2e4fce50e8c0f05122e265ef821d30b4a7c92a
JivkoSp <atjspasov@gmail.com>
2024-05-17 15:01:15 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/ViewedUserAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/ViewedUserAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class ViewedUserAlreadyExistsException : DomainException
+    {
+        internal ViewedUserAlreadyExistsException(Guid viewedUserID, Guid viewerUserID) 
+            : base(message: $"There is already viewed user #{viewedUserID} by user #{viewerUserID}!")
+        {
+        }
+    }
+}
