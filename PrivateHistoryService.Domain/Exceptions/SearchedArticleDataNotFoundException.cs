3fce7e4b623ba6c6b62e050ab5835c684e1090aa
JivkoSp <atjspasov@gmail.com>
2024-05-17 16:37:32 +0300

    Added NEW custom domain event type: PrivateHistoryService.Domain/Exceptions/SearchedArticleDataNotFoundException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/SearchedArticleDataNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class SearchedArticleDataNotFoundException : DomainException
+    {
+        internal SearchedArticleDataNotFoundException(Guid articleId, Guid userId, DateTimeOffset dateTime) 
+            : base(message: $"There is no searched data for article #{articleId} from user #{userId} on {dateTime}!")
+        {
+        }
+    }
+}
