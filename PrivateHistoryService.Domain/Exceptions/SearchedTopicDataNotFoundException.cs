a85f144969be2bbc1adaaddff13e666c886adc99
JivkoSp <atjspasov@gmail.com>
2024-05-17 16:51:20 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/SearchedTopicDataNotFoundException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/SearchedTopicDataNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class SearchedTopicDataNotFoundException : DomainException
+    {
+        internal SearchedTopicDataNotFoundException(Guid topicId, Guid userId, DateTimeOffset dateTime) 
+            : base(message: $"There is no searched data for topic #{topicId} from user #{userId} on {dateTime}!")
+        {
+        }
+    }
+}
