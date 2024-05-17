8c811dc3da168407100b9204ec2a295a9a7c503e
JivkoSp <atjspasov@gmail.com>
2024-05-17 13:37:14 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/DislikedArticleAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/DislikedArticleAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class DislikedArticleAlreadyExistsException : DomainException
+    {
+        internal DislikedArticleAlreadyExistsException(Guid articleId, Guid userId) 
+            : base(message: $"There is already dislike for article #{articleId} from user #{userId}!")
+        {
+        }
+    }
+}
