85e2fa9027f90ab8717aec63927a7939fa5f04e9
JivkoSp <atjspasov@gmail.com>
2024-05-17 19:11:51 +0300

    Added two NEW custom domain exception types in PrivateHistoryService.Domain/Exceptions/. The domain exceptions are: CommentedArticleNotFoundException.cs and ViewedUserNotFoundException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/ViewedUserNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class ViewedUserNotFoundException : DomainException
+    {
+        internal ViewedUserNotFoundException(Guid viewedUserID, Guid viewerUserID, DateTimeOffset dateTime) 
+            : base(message: $"There is no viewed user #{viewedUserID} by user #{viewerUserID} on {dateTime}!")
+        {
+        }
+    }
+}
