e305a324c2132a8374085b43751b5095c7e8c7a5
JivkoSp <atjspasov@gmail.com>
2024-05-17 16:25:36 +0300

    Added NEW custom domain exception type: PrivateHistoryService.Domain/Exceptions/TopicSubscriptionNotFoundException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/TopicSubscriptionNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class TopicSubscriptionNotFoundException : DomainException
+    {
+        internal TopicSubscriptionNotFoundException(Guid topicId, Guid userId) 
+            : base(message: $"There is no subscription for topic #{topicId} from user #{userId}!")
+        {
+        }
+    }
+}
