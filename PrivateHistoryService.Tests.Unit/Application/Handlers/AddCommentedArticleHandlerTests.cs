8f06182a64252654a093cfa5a0749275c5cd6f6b
JivkoSp <atjspasov@gmail.com>
2024-05-19 23:55:32 +0300

    Added Application/ directory in PrivateHistoryService.Tests.Unit/. This directory contains Handlers/ sub-directory that has AddCommentedArticleHandlerTests.cs class. The AddCommentedArticleHandlerTests.cs class will contain unit tests for the AddCommentedArticleHandler command handler.

---
+++ PrivateHistoryService.Tests.Unit/Application/Handlers/AddCommentedArticleHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using PrivateHistoryService.Application.Commands;
+using PrivateHistoryService.Application.Commands.Handlers;
+using PrivateHistoryService.Domain.Factories;
+using PrivateHistoryService.Domain.Factories.Interfaces;
+using PrivateHistoryService.Domain.Repositories;
+
+namespace PrivateHistoryService.Tests.Unit.Application.Handlers
+{
+    public sealed class AddCommentedArticleHandlerTests
+    {
+        #region GLOBAL ARRANGE
+
+        private readonly IUserFactory _userConcreteFactory;
+        private readonly IUserRepository _userRepository;
+        private readonly ICommandHandler<AddCommentedArticleCommand> _handler;
+
+        private AddCommentedArticleCommand GetAddCommentedArticleCommand()
+        {
+            var command = new AddCommentedArticleCommand(ArticleId: Guid.NewGuid(), UserId: Guid.NewGuid(), 
+                ArticleComment: "", DateTime: DateTimeOffset.Now);
+
+            return command;
+        }
+
+        public AddCommentedArticleHandlerTests()
+        {
+            _userConcreteFactory = new UserFactory();
+            _userRepository = Substitute.For<IUserRepository>();
+            _handler = new AddCommentedArticleHandler(_userRepository);
+        }
+
+        #endregion
+    }
+}
