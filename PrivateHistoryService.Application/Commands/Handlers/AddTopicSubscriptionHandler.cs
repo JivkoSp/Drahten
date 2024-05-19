853093213ffd7ce1614f0852ca48e9eef65c01e8
JivkoSp <atjspasov@gmail.com>
2024-05-19 18:13:55 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/AddTopicSubscriptionHandler.cs. This command handler is responsible for handling the AddTopicSubscriptionCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddTopicSubscriptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddTopicSubscriptionHandler : ICommandHandler<AddTopicSubscriptionCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddTopicSubscriptionHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddTopicSubscriptionCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var topicSubscription = new TopicSubscription(command.TopicId, command.UserId, command.DateTime);
+
+            user.AddTopicSubscription(topicSubscription);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
