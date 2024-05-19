f03808c66ce839c19223d0d4d5b618bb00c07638
JivkoSp <atjspasov@gmail.com>
2024-05-19 19:06:02 +0300

    Added GetUserByIdAsync method in PrivateHistoryService.Domain/Repositories/IUserRepository.cs.

--- PrivateHistoryService.Domain/Repositories/IUserRepository.cs
+++ PrivateHistoryService.Domain/Repositories/IUserRepository.cs
@@ -1,9 +1,11 @@
 ﻿using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
 
 namespace PrivateHistoryService.Domain.Repositories
 {
     public interface IUserRepository
     {
+        Task<User> GetUserByIdAsync(UserID userId);
         Task AddUserAsync(User user);
         Task UpdateUserAsync(User user);
     }
