6d702591c85a60f8b82aae972b6372b05373955a
JivkoSp <atjspasov@gmail.com>
2024-05-19 19:04:31 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/RemoveCommentedArticleHandler.cs. This command hanlder is responsible for handling the RemoveCommentedArticleCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/RemoveCommentedArticleHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class RemoveCommentedArticleHandler : ICommandHandler<RemoveCommentedArticleCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RemoveCommentedArticleHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(RemoveCommentedArticleCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var commentedArticle = new CommentedArticle(command.ArticleId, command.UserId, command.ArticleComment, command.DateTime);
+
+            user.RemoveCommentedArticle(commentedArticle);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
