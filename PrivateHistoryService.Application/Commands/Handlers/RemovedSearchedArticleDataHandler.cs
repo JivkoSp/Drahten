f2200ea4d47df8216ce09609c39f6a7101e3aee6
JivkoSp <atjspasov@gmail.com>
2024-05-19 20:21:56 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/RemovedSearchedArticleDataHandler.cs. This command handler is responsible for handling the RemovedSearchedArticleDataCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/RemovedSearchedArticleDataHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class RemovedSearchedArticleDataHandler : ICommandHandler<RemovedSearchedArticleDataCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RemovedSearchedArticleDataHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(RemovedSearchedArticleDataCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var searchedArticleData = new SearchedArticleData(command.ArticleId, command.UserId, command.SearchedData, command.DateTime);
+
+            user.RemovedSearchedArticleData(searchedArticleData);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
