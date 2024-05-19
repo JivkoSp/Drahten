cf882d5b311830578e7d257a839e51004323af57
JivkoSp <atjspasov@gmail.com>
2024-05-19 16:41:31 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/AddDislikedArticleHandler.cs. This command handler is responsible for handling the AddDislikedArticleCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddDislikedArticleHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddDislikedArticleHandler : ICommandHandler<AddDislikedArticleCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddDislikedArticleHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddDislikedArticleCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var dislikedArticle = new DislikedArticle(command.ArticleId, command.UserId, command.DateTime);
+
+            user.AddDislikedArticle(dislikedArticle);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
