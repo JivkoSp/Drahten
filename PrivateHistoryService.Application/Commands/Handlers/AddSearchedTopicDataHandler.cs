822995cbf8a2a6f161dfb9f3ecb5718f0ac7a2ac
JivkoSp <atjspasov@gmail.com>
2024-05-19 17:58:08 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/AddSearchedTopicDataHandler.cs. This command handler is responsible for handling the AddSearchedTopicDataCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddSearchedTopicDataHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddSearchedTopicDataHandler : ICommandHandler<AddSearchedTopicDataCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddSearchedTopicDataHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddSearchedTopicDataCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var searchedTopicData = new SearchedTopicData(command.TopicId, command.UserId, command.SearchedData, command.DateTime);
+
+            user.AddSearchedTopicData(searchedTopicData);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
