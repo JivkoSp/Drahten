dcf9bc196ae69e73fe43ddb83b97d8734aa8d85b
JivkoSp <atjspasov@gmail.com>
2024-05-19 21:17:44 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/RemoveViewedUserHandler.cs. This command handler is responsible for handling the RemoveViewedUserCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/RemoveViewedUserHandler.cs
@@ -0,0 +1,39 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class RemoveViewedUserHandler : ICommandHandler<RemoveViewedUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RemoveViewedUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(RemoveViewedUserCommand command)
+        {
+            var viewer = await _userRepository.GetUserByIdAsync(command.ViewerUserId);
+
+            if (viewer == null)
+            {
+                throw new UserNotFoundException(command.ViewerUserId);
+            }
+
+            var viewedUser = await _userRepository.GetUserByIdAsync(command.ViewedUserId);
+
+            if (viewedUser == null)
+            {
+                throw new UserNotFoundException(command.ViewedUserId);
+            }
+
+            var viewedUserValueObject = new ViewedUser(command.ViewerUserId, command.ViewedUserId, command.DateTime);
+
+            viewer.RemoveViewedUser(viewedUserValueObject);
+
+            await _userRepository.UpdateUserAsync(viewer);
+        }
+    }
+}
