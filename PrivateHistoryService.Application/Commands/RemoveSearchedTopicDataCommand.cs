6e654534c210f2fdbbbee1e94ea4bd70ba2f99c3
JivkoSp <atjspasov@gmail.com>
2024-05-19 13:29:53 +0300

    Added NEW command type: PrivateHistoryService.Application/Commands/RemoveSearchedTopicDataCommand.cs.

---
+++ PrivateHistoryService.Application/Commands/RemoveSearchedTopicDataCommand.cs
@@ -0,0 +1,5 @@
+﻿
+namespace PrivateHistoryService.Application.Commands
+{
+    public record RemoveSearchedTopicDataCommand(Guid TopicId, Guid UserId, string SearchedData, DateTimeOffset DateTime) : ICommand;
+}
