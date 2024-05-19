3826bb1b88569b57c266f238affb47b01674cd77
JivkoSp <atjspasov@gmail.com>
2024-05-19 20:59:38 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/RemoveTopicSubscriptionHandler.cs. This command handler is responsible for handling the RemoveTopicSubscriptionCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/RemoveTopicSubscriptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class RemoveTopicSubscriptionHandler : ICommandHandler<RemoveTopicSubscriptionCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RemoveTopicSubscriptionHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(RemoveTopicSubscriptionCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            user.RemoveTopicSubscription(command.TopicId, command.UserId);
+            
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
