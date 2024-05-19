0daca2d23ee5f1019b4cee834a30aef289d657b1
JivkoSp <atjspasov@gmail.com>
2024-05-19 21:30:44 +0300

    Added implementation for the ICommandDispatcher in PrivateHistoryService.Application/Commands/Dispatcher/InMemoryCommandDispatcher.cs.

---
+++ PrivateHistoryService.Application/Commands/Dispatcher/InMemoryCommandDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PrivateHistoryService.Application.Commands.Handlers;
+
+namespace PrivateHistoryService.Application.Commands.Dispatcher
+{
+    internal sealed class InMemoryCommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public InMemoryCommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, ICommand
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+            await handler.HandleAsync(command);
+        }
+    }
+}
