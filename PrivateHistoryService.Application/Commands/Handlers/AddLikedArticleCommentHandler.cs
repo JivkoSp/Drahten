eccd998a14ab5d9b9cac5d14438e6149f9871e87
JivkoSp <atjspasov@gmail.com>
2024-05-19 17:00:16 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/AddLikedArticleCommentHandler.cs. This command handler is responsible for handling the AddLikedArticleCommentCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddLikedArticleCommentHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddLikedArticleCommentHandler : ICommandHandler<AddLikedArticleCommentCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddLikedArticleCommentHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddLikedArticleCommentCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var likedArticleComment = new LikedArticleComment(command.ArticleId, command.UserId, command.ArticleCommentId, command.DateTime);
+
+            user.AddLikedArticleComment(likedArticleComment);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
