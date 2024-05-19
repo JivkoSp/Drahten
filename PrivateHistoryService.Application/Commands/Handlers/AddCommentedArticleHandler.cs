07ff61cbd6ff0f600e8aafddd938a0f1b2ca3d4e
JivkoSp <atjspasov@gmail.com>
2024-05-19 16:34:26 +0300

    Added command handler for the PrivateHistoryService.Application/Commands/AddCommentedArticleCommand command type: PrivateHistoryService.Application/Commands/Handlers/AddCommentedArticleHandler.cs

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddCommentedArticleHandler.cs
@@ -0,0 +1,32 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddCommentedArticleHandler : ICommandHandler<AddCommentedArticleCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddCommentedArticleHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddCommentedArticleCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var commentedArticle = new CommentedArticle(command.ArticleId, command.UserId, command.ArticleComment, command.DateTime);
+
+            user.AddCommentedArticle(commentedArticle);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
