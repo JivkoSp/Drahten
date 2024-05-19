ea9be45a67fbb9a30ed5a6bfcb6bc7d697b0fa2d
JivkoSp <atjspasov@gmail.com>
2024-05-19 18:45:15 +0300

    Added NEW command handler: PrivateHistoryService.Application/Commands/Handlers/AddViewedUserHandler.cs. This command handler is responsible for handling the AddViewedUserCommand command type.

---
+++ PrivateHistoryService.Application/Commands/Handlers/AddViewedUserHandler.cs
@@ -0,0 +1,39 @@
+﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Domain.Repositories;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddViewedUserHandler : ICommandHandler<AddViewedUserCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddViewedUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddViewedUserCommand command)
+        {
+            var viewer = await _userRepository.GetUserByIdAsync(command.ViewerUserId);
+
+            if (viewer == null)
+            {
+                throw new UserNotFoundException(command.ViewerUserId);
+            }
+
+            var viewedUser = await _userRepository.GetUserByIdAsync(command.ViewedUserId);
+
+            if (viewedUser == null)
+            {
+                throw new UserNotFoundException(command.ViewedUserId);
+            }
+
+            var viewedUserValueObject = new ViewedUser(command.ViewerUserId, command.ViewedUserId, command.DateTime);
+
+            viewer.AddViewedUser(viewedUserValueObject);
+
+            await _userRepository.UpdateUserAsync(viewer);
+        }
+    }
+}
