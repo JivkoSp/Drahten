8de6f359d9ac853f63913f8bd11cf26d13eff0ee
JivkoSp <atjspasov@gmail.com>
2024-05-20 22:39:20 +0300

    Added NEW query type: PrivateHistoryService.Application/Queries/GetSearchedTopicsDataQuery.cs

---
+++ PrivateHistoryService.Application/Queries/GetSearchedTopicsDataQuery.cs
@@ -0,0 +1,6 @@
+﻿using PrivateHistoryService.Application.Dtos;
+
+namespace PrivateHistoryService.Application.Queries
+{
+    public record GetSearchedTopicsDataQuery(Guid UserId) : IQuery<List<SearchedTopicDataDto>>;
+}
