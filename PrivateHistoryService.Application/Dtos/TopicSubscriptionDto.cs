4fbf45a984f1a6b063288072d1a5b78bc3320da7
JivkoSp <atjspasov@gmail.com>
2024-05-20 22:23:58 +0300

    Added NEW dto type: PrivateHistoryService.Application/Dtos/TopicSubscriptionDto.cs.

---
+++ PrivateHistoryService.Application/Dtos/TopicSubscriptionDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Application.Dtos
+{
+    public class TopicSubscriptionDto
+    {
+        public Guid TopicId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
