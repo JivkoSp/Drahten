2c1f0a597a3986c47cee806cd92142924f48c5b9
JivkoSp <atjspasov@gmail.com>
2024-05-20 22:38:41 +0300

    Added NEW dto type: PrivateHistoryService.Application/Dtos/SearchedTopicDataDto.cs

---
+++ PrivateHistoryService.Application/Dtos/SearchedTopicDataDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Application.Dtos
+{
+    public class SearchedTopicDataDto
+    {
+        public Guid TopicId { get; set; }
+        public Guid UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
