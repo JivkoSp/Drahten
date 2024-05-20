981e49260d27843b18398c68c30cdf8796dcf67a
JivkoSp <atjspasov@gmail.com>
2024-05-20 21:56:59 +0300

    Added NEW query type: PrivateHistoryService.Application/Queries/GetViewedArticlesQuery.cs

---
+++ PrivateHistoryService.Application/Queries/GetViewedArticlesQuery.cs
@@ -0,0 +1,6 @@
+﻿using PrivateHistoryService.Application.Dtos;
+
+namespace PrivateHistoryService.Application.Queries
+{
+    public record GetViewedArticlesQuery(Guid UserId) : IQuery<List<ViewedArticleDto>>;
+}
