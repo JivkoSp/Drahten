65bc7f04c2438a15475c5465f4e610f260992784
JivkoSp <atjspasov@gmail.com>
2024-05-21 18:09:28 +0300

    Added database configurations for the UserReadModel database model type in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<UserReadModel>
+    {
+        public void Configure(EntityTypeBuilder<UserReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("User");
+
+            //Primary key
+            builder.HasKey(key => key.UserId);
+
+            builder.Property(p => p.UserId)
+                .ValueGeneratedNever();
+        }
+    }
+}
