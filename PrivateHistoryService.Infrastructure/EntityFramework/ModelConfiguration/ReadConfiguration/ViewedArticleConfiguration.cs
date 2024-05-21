942609d07d208a4c1f79fd812a318f81c86e08b4
JivkoSp <atjspasov@gmail.com>
2024-05-21 18:13:41 +0300

    Added database configurations for the ViewedArticleReadModel database model type in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ViewedArticleConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ViewedArticleConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class ViewedArticleConfiguration : IEntityTypeConfiguration<ViewedArticleReadModel>
+    {
+        public void Configure(EntityTypeBuilder<ViewedArticleReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("ViewedArticle");
+
+            //Primary key
+            builder.HasKey(key => key.ViewedArticleId);
+
+            //Property config - Start
+
+            builder.Property(p => p.ArticleId)
+                .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.ViewedArticles)
+                .HasForeignKey(p => p.UserId)
+                .HasConstraintName("FK_User_ViewedArticles")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
