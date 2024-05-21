03606b428dc07c23600c76a72fbb550b0fd78936
JivkoSp <atjspasov@gmail.com>
2024-05-21 11:06:02 +0300

    Added Dispatcher/ directory in PrivateHistoryService.Application/Queries/. This directory contains IQueryDispatcher.cs interface that has DispatchAsync method. This interface will be implemented by the query dispatcher.

---
+++ PrivateHistoryService.Application/Queries/Dispatcher/IQueryDispatcher.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PrivateHistoryService.Application.Queries.Dispatcher
+{
+    public interface IQueryDispatcher
+    {
+        Task<TResult> DispatchAsync<TResult>(IQuery<TResult> query);
+    }
+}
