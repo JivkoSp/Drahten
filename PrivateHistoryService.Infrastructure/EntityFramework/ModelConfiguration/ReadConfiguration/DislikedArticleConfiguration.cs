eaa66dd01675592e0939258d6b7cba49e0b3430a
JivkoSp <atjspasov@gmail.com>
2024-05-21 17:40:22 +0300

    Added database configurations for the DislikedArticleReadModel database model type in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/DislikedArticleConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/DislikedArticleConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class DislikedArticleConfiguration : IEntityTypeConfiguration<DislikedArticleReadModel>
+    {
+        public void Configure(EntityTypeBuilder<DislikedArticleReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("DislikedArticle");
+
+            //Composite primary key
+            builder.HasKey(key => new { key.ArticleId, key.UserId });
+
+            //Property config - Start
+
+            builder.Property(p => p.ArticleId)
+                .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.DislikedArticles)
+                .HasForeignKey(p => p.UserId)
+                .HasConstraintName("FK_User_DislikedArticles")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
