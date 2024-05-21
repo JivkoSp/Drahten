dc287f132781feca1155385935962d7346bae1cb
JivkoSp <atjspasov@gmail.com>
2024-05-21 15:56:37 +0300

    Added NEW database model type: PrivateHistoryService.Infrastructure/EntityFramework/Models/TopicSubscriptionReadModel.cs

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/TopicSubscriptionReadModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class TopicSubscriptionReadModel
+    {
+        //Composite primary key { TopicId, UserId }
+        public Guid TopicId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        //Relationships
+        public virtual UserReadModel User { get; set; }
+    }
+}
