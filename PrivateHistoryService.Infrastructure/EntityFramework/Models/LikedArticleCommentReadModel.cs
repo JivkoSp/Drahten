65d6044e21c279519d11b54bc96c34c7525782c7
JivkoSp <atjspasov@gmail.com>
2024-05-21 16:26:34 +0300

    Added NEW database model type: PrivateHistoryService.Infrastructure/EntityFramework/Models/LikedArticleCommentReadModel.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/LikedArticleCommentReadModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class LikedArticleCommentReadModel
+    {
+        //Composite primary key { ArticleCommentId, UserId }
+        public Guid ArticleCommentId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        //Relationships
+        public virtual UserReadModel User { get; set; }
+    }
+}
