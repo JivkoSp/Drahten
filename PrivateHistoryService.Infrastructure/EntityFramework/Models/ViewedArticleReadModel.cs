0633629bd9002054c12f9e42c622450793df3141
JivkoSp <atjspasov@gmail.com>
2024-05-21 15:37:09 +0300

    Added NEW database model type: PrivateHistoryService.Infrastructure/EntityFramework/Models/ViewedArticleReadModel.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/ViewedArticleReadModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class ViewedArticleReadModel
+    {
+        //Primary key
+        public Guid ViewedArticleId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        //Relationships
+        public virtual UserReadModel User { get; set; }
+    }
+}
