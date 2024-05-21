9426df13e6499bddaa05a17a93b3ad716a4cd74a
JivkoSp <atjspasov@gmail.com>
2024-05-21 20:47:58 +0300

    Modified the PrivateHistoryService.Infrastructure/EntityFramework/Models/ViewedUserReadModel.cs. The modifications are: Renamed the Viewed property to ViewedUser; Removed the property Viewer; Renamed the property ViewerId to ViewerUserId; Renamed the property ViewedId to ViewedUserId; Renamed the property ViewedUserId to ViewedUserReadModelId.

--- PrivateHistoryService.Infrastructure/EntityFramework/Models/ViewedUserReadModel.cs
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/ViewedUserReadModel.cs
@@ -4,13 +4,12 @@ namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
     internal class ViewedUserReadModel
     {
         //Primary key
-        public Guid ViewedUserId { get; set; }
-        public string ViewerId { get; set; }
-        public string ViewedId { get; set; }
+        public Guid ViewedUserReadModelId { get; set; }
+        public string ViewerUserId { get; set; }
+        public string ViewedUserId { get; set; }
         public DateTimeOffset DateTime { get; set; }
 
         //Relationships
-        public virtual UserReadModel Viewer { get; set; }
-        public virtual UserReadModel Viewed { get; set; }
+        public virtual UserReadModel ViewedUser { get; set; }
     }
 }
