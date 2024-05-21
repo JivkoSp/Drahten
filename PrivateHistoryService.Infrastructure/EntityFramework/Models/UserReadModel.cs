ee342d96311f0d0e0e5f030375a6d17f6d9ce56a
JivkoSp <atjspasov@gmail.com>
2024-05-21 15:28:05 +0300

    Added EntityFramework/ directory PrivateHistoryService.Infrastructure/. This directory contains Models/ sub-directory which has UserReadModel.cs. The UserReadModel.cs will represent the User domain entity in the database.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/UserReadModel.cs
@@ -0,0 +1,14 @@
+﻿
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class UserReadModel
+    {
+        //Primary key
+        public string UserId { get; set; }
+        public int Version { get; set; }
+
+        //Relationships
+    }
+}
