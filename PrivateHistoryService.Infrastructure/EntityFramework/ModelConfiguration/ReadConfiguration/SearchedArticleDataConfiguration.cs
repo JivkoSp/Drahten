72e5f55538ef80f300a8f11b794be03a5a4005aa
JivkoSp <atjspasov@gmail.com>
2024-05-21 17:54:47 +0300

    Added database configurations for the SearchedArticleDataReadModel database model type in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/SearchedArticleDataConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/SearchedArticleDataConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class SearchedArticleDataConfiguration : IEntityTypeConfiguration<SearchedArticleDataReadModel>
+    {
+        public void Configure(EntityTypeBuilder<SearchedArticleDataReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("SearchedArticleData");
+
+            //Primary key
+            builder.HasKey(key => key.SearchedArticleDataId);
+
+            //Property config - Start
+
+            builder.Property(p => p.ArticleId)
+                .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .IsRequired();
+
+            builder.Property(p => p.SearchedData)
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.SearchedArticles)
+                .HasForeignKey(p => p.UserId)
+                .HasConstraintName("FK_User_SearchedArticles")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
