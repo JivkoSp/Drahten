7171548d6c666928a1397ce4e5f11f31262bb3e1
JivkoSp <atjspasov@gmail.com>
2024-05-21 20:49:29 +0300

    Added database configurations for the ViewedUserReadModel database model type in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ViewedUserConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/ViewedUserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class ViewedUserConfiguration : IEntityTypeConfiguration<ViewedUserReadModel>
+    {
+        public void Configure(EntityTypeBuilder<ViewedUserReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("ViewedUser");
+
+            //Primary key
+            builder.HasKey(key => key.ViewedUserReadModelId);
+
+            //Property config - Start
+
+            builder.Property(p => p.ViewerUserId)
+                .IsRequired();
+
+            builder.Property(p => p.ViewedUserId)
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.ViewedUser)
+                .WithMany(p => p.ViewedUsers)
+                .HasForeignKey(p => p.ViewedUserId)
+                .HasConstraintName("FK_User_ViewedUsers")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
