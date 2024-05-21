5ba4c1cd8737d2bd91fc8adff202c2ec8cc4621c
JivkoSp <atjspasov@gmail.com>
2024-05-21 11:01:38 +0300

    Added Handlers/ directory in PrivateHistoryService.Application/Queries/. This directory contains IQueryHandler.cs interface that defines HandleAsync method that must be implemented by all query handlers.

---
+++ PrivateHistoryService.Application/Queries/Handlers/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PrivateHistoryService.Application.Queries.Handlers
+{
+    public interface IQueryHandler<in TQuery, TResult> where TQuery : class, IQuery<TResult>
+    {
+        Task<TResult> HandleAsync(TQuery query);
+    }
+}
