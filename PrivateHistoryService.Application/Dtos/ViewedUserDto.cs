73bc6fa7c69c1a98c42f1a252e35741d8abc4703
JivkoSp <atjspasov@gmail.com>
2024-05-21 10:56:36 +0300

    Added NEW dto type: PrivateHistoryService.Application/Dtos/ViewedUserDto.cs.

---
+++ PrivateHistoryService.Application/Dtos/ViewedUserDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Application.Dtos
+{
+    public class ViewedUserDto
+    {
+        public Guid ViewerUserId { get; set; }
+        public Guid ViewedUserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
