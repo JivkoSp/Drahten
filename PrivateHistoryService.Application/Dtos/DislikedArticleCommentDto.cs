8f7b4c0731cb3cd5429d3d6aaad9955215bcc9c5
JivkoSp <atjspasov@gmail.com>
2024-05-21 10:48:50 +0300

    Added NEW dto type: PrivateHistoryService.Application/Dtos/DislikedArticleCommentDto.cs.

---
+++ PrivateHistoryService.Application/Dtos/DislikedArticleCommentDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Application.Dtos
+{
+    public class DislikedArticleCommentDto
+    {
+        public Guid ArticleId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ArticleCommentId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
