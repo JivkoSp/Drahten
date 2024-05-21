4a1b85cb705b2ce8e427399c2f9112d8b1c09866
JivkoSp <atjspasov@gmail.com>
2024-05-21 16:42:45 +0300

    Added relationships to other database model types in PrivateHistoryService.Infrastructure/EntityFramework/Models/UserReadModel.cs.

--- PrivateHistoryService.Infrastructure/EntityFramework/Models/UserReadModel.cs
+++ PrivateHistoryService.Infrastructure/EntityFramework/Models/UserReadModel.cs
@@ -1,6 +1,4 @@
 ﻿
-using PrivateHistoryService.Domain.ValueObjects;
-
 namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
 {
     internal class UserReadModel
@@ -10,5 +8,15 @@ namespace PrivateHistoryService.Infrastructure.EntityFramework.Models
         public int Version { get; set; }
 
         //Relationships
+        public virtual ICollection<ViewedArticleReadModel> ViewedArticles { get; set; }
+        public virtual ICollection<TopicSubscriptionReadModel> TopicSubscriptions { get; set; }
+        public virtual ICollection<SearchedArticleDataReadModel> SearchedArticles { get; set; }
+        public virtual ICollection<SearchedTopicDataReadModel> SearchedTopics { get; set; }
+        public virtual ICollection<CommentedArticleReadModel> CommentedArticles { get; set; }
+        public virtual ICollection<LikedArticleReadModel> LikedArticles { get; set; }
+        public virtual ICollection<DislikedArticleReadModel> DislikedArticles { get; set; }
+        public virtual ICollection<LikedArticleCommentReadModel> LikedArticleComments { get; set; }
+        public virtual ICollection<DislikedArticleCommentReadModel> DislikedArticleComments { get; set; }
+        public virtual ICollection<ViewedUserReadModel> ViewedUsers { get; set; }
     }
 }
