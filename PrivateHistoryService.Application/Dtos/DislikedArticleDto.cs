9fa6ee7f89698d9a10883c249556d8ccc8b809ac
JivkoSp <atjspasov@gmail.com>
2024-05-21 10:32:44 +0300

    Added NEW dto type: PrivateHistoryService.Application/Dtos/DislikedArticleDto.cs

---
+++ PrivateHistoryService.Application/Dtos/DislikedArticleDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Application.Dtos
+{
+    public class DislikedArticleDto
+    {
+        public Guid ArticleId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
