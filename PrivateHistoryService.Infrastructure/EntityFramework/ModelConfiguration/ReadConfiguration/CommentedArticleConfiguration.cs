9fb50fe24d08a448f852106d1b7c4ad03e4667c4
JivkoSp <atjspasov@gmail.com>
2024-05-21 17:14:48 +0300

    Added ModelConfiguration/ directory in PrivateHistoryService.Infrastructure/EntityFramework/. This directory contains ReadConfiguration/ sub-directory which has CommentedArticleConfiguration.cs class. This class implements the IEntityTypeConfiguration<T> interface and contains database configurations for the CommentedArticleReadModel database model type.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/CommentedArticleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class CommentedArticleConfiguration : IEntityTypeConfiguration<CommentedArticleReadModel>
+    {
+        public void Configure(EntityTypeBuilder<CommentedArticleReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("CommentedArticle");
+
+            //Primary key
+            builder.HasKey(key => key.CommentedArticleId);
+
+            //Property config - Start
+
+            builder.Property(p => p.ArticleComment)
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.CommentedArticles)
+                .HasForeignKey(p => p.UserId)
+                .HasConstraintName("FK_User_CommentedArticles")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
