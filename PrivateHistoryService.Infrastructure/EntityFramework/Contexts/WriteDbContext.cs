492f2d4713a593e9ac7f2cc81b09e5b9534d0681
JivkoSp <atjspasov@gmail.com>
2024-05-22 12:23:02 +0300

    Added DbContext for applying the database configurations for the DOMAIN entity and value objects in PrivateHistoryService.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Contexts/WriteDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class WriteDbContext : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public WriteDbContext(DbContextOptions<WriteDbContext> options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLoggerFactory(_loggerFactory); // Enable logging
+                optionsBuilder.EnableSensitiveDataLogging(); // Include parameter values in logs
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("private-history-service");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the domain entities and value objects
+            modelBuilder.ApplyConfiguration(new CommentedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new DislikedArticleCommentConfiguration());
+            modelBuilder.ApplyConfiguration(new DislikedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new LikedArticleCommentConfiguration());
+            modelBuilder.ApplyConfiguration(new LikedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new SearchedArticleDataConfiguration());
+            modelBuilder.ApplyConfiguration(new SearchedTopicDataConfiguration());
+            modelBuilder.ApplyConfiguration(new TopicSubscriptionConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ViewedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new ViewedUserConfiguration());
+        }
+    }
+}
