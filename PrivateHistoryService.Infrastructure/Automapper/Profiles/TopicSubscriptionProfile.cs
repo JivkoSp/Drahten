d2f147d61ffd2a12428dac6e7841084570113931
JivkoSp <atjspasov@gmail.com>
2024-05-22 19:04:24 +0300

    Added NEW automapper profile type: PrivateHistoryService.Infrastructure/Automapper/Profiles/TopicSubscriptionProfile.cs.

---
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/TopicSubscriptionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class TopicSubscriptionProfile : Profile
+    {
+        internal TopicSubscriptionProfile()
+        {
+            CreateMap<TopicSubscriptionReadModel, TopicSubscriptionDto>();
+        }
+    }
+}
