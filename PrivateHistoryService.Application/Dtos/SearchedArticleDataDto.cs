89040202bd8bcd8af9c06357d4ddf25adef59823
JivkoSp <atjspasov@gmail.com>
2024-05-22 18:37:12 +0300

    Changed the ArticleId and UserId properties in PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs from Guid to string. Reason: The returned read model from the database already has this properties in string format, there is no point of converting the data from string to Guid for the dto.

--- PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs
+++ PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs
@@ -3,8 +3,8 @@ namespace PrivateHistoryService.Application.Dtos
 {
     public class SearchedArticleDataDto
     {
-        public Guid ArticleId { get; set; }
-        public Guid UserId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
         public string SearchedData { get; set; }
         public DateTimeOffset DateTime { get; set; }
     }
