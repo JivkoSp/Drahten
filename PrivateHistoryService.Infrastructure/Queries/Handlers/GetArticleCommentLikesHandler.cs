9d80e4b536882d1c2fd26749649d9bc547879307
JivkoSp <atjspasov@gmail.com>
2024-05-22 15:57:41 +0300

    Added query handler for the GetArticleCommentLikesQuery query type: PrivateHistoryService.Infrastructure/Queries/Handlers/GetArticleCommentLikesHandler.cs.

---
+++ PrivateHistoryService.Infrastructure/Queries/Handlers/GetArticleCommentLikesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Queries;
+using PrivateHistoryService.Application.Queries.Handlers;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetArticleCommentLikesHandler : IQueryHandler<GetArticleCommentLikesQuery, List<LikedArticleCommentDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetArticleCommentLikesHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LikedArticleCommentDto>> HandleAsync(GetArticleCommentLikesQuery query)
+        {
+            var likedArticleCommentReadModels = await _readDbContext.LikedArticleComments
+               .Where(x => x.UserId == query.UserId.ToString())
+               .AsNoTracking()
+               .ToListAsync();
+
+            return _mapper.Map<List<LikedArticleCommentDto>>(likedArticleCommentReadModels);
+        }
+    }
+}
