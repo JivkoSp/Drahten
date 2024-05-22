6cfb0c6defd95becad90a31546fe375ff802654f
JivkoSp <atjspasov@gmail.com>
2024-05-22 12:06:23 +0300

    Added Contexts/ directory in PrivateHistoryService.Infrastructure/EntityFramework/. This directory contains ReadDbContext.cs. This class is DbContext and apply the database configurations for the database model types (The read models).

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class ReadDbContext : DbContext
+    {
+        public ReadDbContext(DbContextOptions<ReadDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("private-history-service");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new CommentedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new DislikedArticleCommentConfiguration());
+            modelBuilder.ApplyConfiguration(new DislikedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new LikedArticleCommentConfiguration());
+            modelBuilder.ApplyConfiguration(new LikedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new SearchedArticleDataConfiguration());
+            modelBuilder.ApplyConfiguration(new SearchedTopicDataConfiguration());
+            modelBuilder.ApplyConfiguration(new TopicSubscriptionConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ViewedArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new ViewedUserConfiguration());
+        }
+    }
+}
