04e043ba173b2421d73205fc5a279e4626a48dcd
JivkoSp <atjspasov@gmail.com>
2024-05-22 14:05:35 +0300

    Added Initialization/ directory in PrivateHistoryService.Infrastructure/EntityFramework/. This directory contains DbInitializer.cs. The DbInitializer is IHostedService and its purpose is to apply the entityframework migrations to the database.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Initialization/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PrivateHistoryService.Infrastructure.Exceptions;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Initialization
+{
+    internal sealed class DbInitializer : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbInitializer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var dbContextTypes = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(x => typeof(DbContext).IsAssignableFrom(x) && !x.IsInterface && x != typeof(DbContext));
+
+            var scope = _serviceProvider.CreateScope();
+
+            foreach (var dbContextType in dbContextTypes)
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService(dbContextType) as DbContext;
+
+                if (dbContext == null)
+                {
+                    throw new NullDbContextException();
+                }
+
+                Console.WriteLine($"\n\n*** Applying migrations for {dbContext} ... ***\n\n");
+
+                await dbContext.Database.MigrateAsync(cancellationToken);
+
+                Console.WriteLine($"\n\n*** Done! All migrations for {dbContext} are applied. ***\n\n");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
