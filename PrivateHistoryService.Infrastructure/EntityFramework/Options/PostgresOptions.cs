86412b05189b0a28db65db7bf02827f4c71c5ec9
JivkoSp <atjspasov@gmail.com>
2024-05-22 14:08:51 +0300

    Added Options/ directory in PrivateHistoryService.Infrastructure/EntityFramework/. This directory contains PostgresOptions.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Options/PostgresOptions.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Options
+{
+    internal class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
