5261fe1b5fdb0c492d998ba295d6e0f1ec50a55a
JivkoSp <atjspasov@gmail.com>
2024-05-22 18:38:48 +0300

    Added NEW automapper profile type: PrivateHistoryService.Infrastructure/Automapper/Profiles/SearchedArticleDataProfile.cs.

---
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/SearchedArticleDataProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class SearchedArticleDataProfile : Profile
+    {
+        internal SearchedArticleDataProfile()
+        {
+            CreateMap<SearchedArticleDataReadModel, SearchedArticleDataDto>();
+        }
+    }
+}
