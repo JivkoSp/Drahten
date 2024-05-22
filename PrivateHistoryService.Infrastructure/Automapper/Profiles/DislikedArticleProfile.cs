37faed583faafd40fa3bd88e7d8fbf8af04f1a73
JivkoSp <atjspasov@gmail.com>
2024-05-22 16:36:35 +0300

    Added NEW automapper profile type: PrivateHistoryService.Infrastructure/Automapper/Profiles/DislikedArticleProfile.cs.

---
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/DislikedArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class DislikedArticleProfile : Profile
+    {
+        internal DislikedArticleProfile()
+        {
+            CreateMap<DislikedArticleReadModel, DislikedArticleDto>();
+        }
+    }
+}
