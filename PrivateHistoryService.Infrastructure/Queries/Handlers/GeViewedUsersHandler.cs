8e16f4691631a7213835e6ca58d28847a5584c54
JivkoSp <atjspasov@gmail.com>
2024-05-22 20:49:21 +0300

    Added query handler for the query type GeViewedUsersQuery: PrivateHistoryService.Infrastructure/Queries/Handlers/GeViewedUsersHandler.cs.

---
+++ PrivateHistoryService.Infrastructure/Queries/Handlers/GeViewedUsersHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Queries;
+using PrivateHistoryService.Application.Queries.Handlers;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GeViewedUsersHandler : IQueryHandler<GeViewedUsersQuery, List<ViewedUserDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GeViewedUsersHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ViewedUserDto>> HandleAsync(GeViewedUsersQuery query)
+        {
+            var viewedUserReadModels = await _readDbContext.ViewedUsers
+              .Where(x => x.ViewerUserId == query.ViewerUserId.ToString())
+              .AsNoTracking()
+              .ToListAsync();
+
+            return _mapper.Map<List<ViewedUserDto>>(viewedUserReadModels);
+        }
+    }
+}
