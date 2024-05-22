eb184bce681405ff15f822d943bba36b8ebc08e9
JivkoSp <atjspasov@gmail.com>
2024-05-22 18:40:58 +0300

    Added query handler for the query type GetSearchedArticlesDataQuery: PrivateHistoryService.Infrastructure/Queries/Handlers/GetSearchedArticlesDataHandler.cs.

---
+++ PrivateHistoryService.Infrastructure/Queries/Handlers/GetSearchedArticlesDataHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Queries;
+using PrivateHistoryService.Application.Queries.Handlers;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetSearchedArticlesDataHandler : IQueryHandler<GetSearchedArticlesDataQuery, List<SearchedArticleDataDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetSearchedArticlesDataHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<SearchedArticleDataDto>> HandleAsync(GetSearchedArticlesDataQuery query)
+        {
+            var searchedArticleReadModels = await _readDbContext.SearchedArticles
+              .Where(x => x.UserId == query.UserId.ToString())
+              .AsNoTracking()
+              .ToListAsync();
+
+            return _mapper.Map<List<SearchedArticleDataDto>>(searchedArticleReadModels);
+        }
+    }
+}
