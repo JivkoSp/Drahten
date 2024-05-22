f5b4910a439e51f19c92116ddab582b23dc153f3
JivkoSp <atjspasov@gmail.com>
2024-05-22 19:03:25 +0300

    Changed the UserId property in PrivateHistoryService.Application/Dtos/TopicSubscriptionDto.cs from Guid to string. Reason: The returned read model from the database already has this property in string format, there is no point of converting the data from string to Guid for the dto.

--- PrivateHistoryService.Application/Dtos/TopicSubscriptionDto.cs
+++ PrivateHistoryService.Application/Dtos/TopicSubscriptionDto.cs
@@ -4,7 +4,7 @@ namespace PrivateHistoryService.Application.Dtos
     public class TopicSubscriptionDto
     {
         public Guid TopicId { get; set; }
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public DateTimeOffset DateTime { get; set; }
     }
 }
