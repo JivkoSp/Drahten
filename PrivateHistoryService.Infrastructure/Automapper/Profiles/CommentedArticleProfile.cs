c21d9d4731ba2be4c3d4730eb07f13a0601668a0
JivkoSp <atjspasov@gmail.com>
2024-05-22 17:27:56 +0300

    Added NEW automapper profile type: PrivateHistoryService.Infrastructure/Automapper/Profiles/CommentedArticleProfile.cs.

---
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/CommentedArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class CommentedArticleProfile : Profile
+    {
+        internal CommentedArticleProfile()
+        {
+            CreateMap<CommentedArticleReadModel, CommentedArticleDto>();
+        }
+    }
+}
