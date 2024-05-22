44293ad51a05fe5df40ee6f8c12fbc2b0b307015
JivkoSp <atjspasov@gmail.com>
2024-05-22 11:30:54 +0300

    Added database configurations for the User DOMAIN entity in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Domain.Entities;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            //Table name
+            builder.ToTable("User");
+
+            //Primary key
+            builder.HasKey(key => key.Id);
+
+            //Property config
+            builder.Property(p => p.Id)
+               .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+               .HasColumnName("UserId")
+               .ValueGeneratedNever()
+               .IsRequired();
+        }
+    }
+}
