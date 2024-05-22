0a8de322f94bfa9d281c777f7d8de8a6f0cf586c
JivkoSp <atjspasov@gmail.com>
2024-05-22 17:26:11 +0300

    Changed the ArticleId and UserId properties in PrivateHistoryService.Application/Dtos/CommentedArticleDto.cs from Guid to string. Reason: The returned read model from the database already has this properties in string format, there is no point of converting the data from string to Guid for the dto.

--- PrivateHistoryService.Application/Dtos/CommentedArticleDto.cs
+++ PrivateHistoryService.Application/Dtos/CommentedArticleDto.cs
@@ -3,8 +3,8 @@ namespace PrivateHistoryService.Application.Dtos
 {
     public class CommentedArticleDto
     {
-        public Guid ArticleId { get; set; }
-        public Guid UserId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
         public string ArticleComment { get; set; }
         public DateTimeOffset DateTime { get; set; }
     }
