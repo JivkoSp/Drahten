4935b2475d272cd415ce7f334b34f0cb839f78b1
JivkoSp <atjspasov@gmail.com>
2024-05-22 16:39:16 +0300

    Added query handler for the query type GetArticleDislikesQuery: PrivateHistoryService.Infrastructure/Queries/Handlers/GetArticleDislikesHandler.cs.

---
+++ PrivateHistoryService.Infrastructure/Queries/Handlers/GetArticleDislikesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Queries;
+using PrivateHistoryService.Application.Queries.Handlers;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetArticleDislikesHandler : IQueryHandler<GetArticleDislikesQuery, List<DislikedArticleDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetArticleDislikesHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<DislikedArticleDto>> HandleAsync(GetArticleDislikesQuery query)
+        {
+            var dislikedArticleReadModels = await _readDbContext.DislikedArticles
+               .Where(x => x.UserId == query.UserId.ToString())
+               .AsNoTracking()
+               .ToListAsync();
+
+            return _mapper.Map<List<DislikedArticleDto>>(dislikedArticleReadModels);
+        }
+    }
+}
