641d7c9944bcf45b1dac09afd9127780c5b94960
JivkoSp <atjspasov@gmail.com>
2024-05-22 11:22:56 +0300

    Added database configurations for the TopicSubscription DOMAIN value object in PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/TopicSubscriptionConfiguration.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/TopicSubscriptionConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class TopicSubscriptionConfiguration : IEntityTypeConfiguration<TopicSubscription>
+    {
+        public void Configure(EntityTypeBuilder<TopicSubscription> builder)
+        {
+            //Table name
+            builder.ToTable("TopicSubscription");
+
+            //Composite primary key
+            builder.HasKey(key => new { key.TopicID, key.UserID });
+
+            //Property config - Start
+
+            builder.Property(p => p.TopicID)
+                .HasConversion(id => id.Value, id => new TopicID(id))
+                .HasColumnName("TopicId")
+                .IsRequired();
+
+            builder.Property(p => p.UserID)
+               .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+               .HasColumnName("UserId")
+               .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+             .IsRequired();
+
+            //Property config - End
+        }
+    }
+}
