8340f030739f24fa8932c3a3e17b2ff0d66c222d
JivkoSp <atjspasov@gmail.com>
2024-05-22 20:46:19 +0300

    Added NEW automapper profile type: PrivateHistoryService.Infrastructure/Automapper/Profiles/ViewedUserProfile.cs.

---
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/ViewedUserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class ViewedUserProfile : Profile
+    {
+        internal ViewedUserProfile()
+        {
+            CreateMap<ViewedUserReadModel, ViewedUserDto>();
+        }
+    }
+}
