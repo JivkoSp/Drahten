a230f3a25f1ae4b97b882ff4cf3428ea331b54ce
JivkoSp <atjspasov@gmail.com>
2024-05-22 19:06:31 +0300

    Added query handler for the query type GetTopicSubscriptionsQuery: PrivateHistoryService.Infrastructure/Queries/Handlers/GetTopicSubscriptionsHandler.cs.

---
+++ PrivateHistoryService.Infrastructure/Queries/Handlers/GetTopicSubscriptionsHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Queries;
+using PrivateHistoryService.Application.Queries.Handlers;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.Queries.Handlers
+{
+    internal sealed class GetTopicSubscriptionsHandler : IQueryHandler<GetTopicSubscriptionsQuery, List<TopicSubscriptionDto>>
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public GetTopicSubscriptionsHandler(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TopicSubscriptionDto>> HandleAsync(GetTopicSubscriptionsQuery query)
+        {
+            var topicSubscriptionReadModels = await _readDbContext.TopicSubscriptions
+              .Where(x => x.UserId == query.UserId.ToString())
+              .AsNoTracking()
+              .ToListAsync();
+
+            return _mapper.Map<List<TopicSubscriptionDto>>(topicSubscriptionReadModels);
+        }
+    }
+}
