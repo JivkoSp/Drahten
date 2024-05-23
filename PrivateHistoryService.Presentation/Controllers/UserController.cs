a278c0430e360b3ee1c44daa5c29c5acd34ee712
JivkoSp <atjspasov@gmail.com>
2024-05-23 13:57:20 +0300

    Added GetLikedArticles action method in PrivateHistoryService.Presentation/Controllers/UserController.cs.

--- PrivateHistoryService.Presentation/Controllers/UserController.cs
+++ PrivateHistoryService.Presentation/Controllers/UserController.cs
@@ -39,5 +39,27 @@ namespace PrivateHistoryService.Presentation.Controllers
 
             return Ok(_responseDto);
         }
+
+        //TODO: Change the name of the query. The query name should be GetLikedArticlesQuery, instead of GetArticleLikesQuery.
+        //Reason: The current name is NOT appropriate because it suggests that the query will return likes for article,
+        //but the query returns articles that are liked by user with the specified UserId.
+        [HttpGet("{UserId:guid}/liked-articles/")]
+        [ProducesResponseType(typeof(ResponseDto), 200)]
+        [ProducesResponseType(typeof(ResponseDto), 404)]
+        public async Task<ActionResult> GetLikedArticles([FromRoute] GetArticleLikesQuery getArticleLikesQuery)
+        {
+            var result = await _queryDispatcher.DispatchAsync(getArticleLikesQuery);
+
+            _responseDto.Result = result;
+
+            if (result == null)
+            {
+                return NotFound(_responseDto);
+            }
+
+            _responseDto.IsSuccess = true;
+
+            return Ok(_responseDto);
+        }
     }
 }
