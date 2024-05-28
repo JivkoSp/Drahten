1c4a375296cd33d07d10c4e31762eaa38a17b138
JivkoSp <atjspasov@gmail.com>
2024-05-28 16:20:48 +0300

    Added Middlewares/ directory in PrivateHistoryService.Presentation/. This directory contains ErrorHandlerMiddleware.cs custom middleware that is responsible for handling all exceptions from the PrivateHistoryService.

---
+++ PrivateHistoryService.Presentation/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using PrivateHistoryService.Infrastructure.Exceptions.Interfaces;
+using System.Net;
+using System.Text.Json;
+
+namespace PrivateHistoryService.Presentation.Middlewares
+{
+    public sealed class ErrorHandlerMiddleware : IMiddleware
+    {
+        private readonly IExceptionToResponseMapper _exceptionToResponseMapper;
+        private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+        public ErrorHandlerMiddleware(IExceptionToResponseMapper exceptionToResponseMapper, ILogger<ErrorHandlerMiddleware> logger)
+        {
+            _exceptionToResponseMapper = exceptionToResponseMapper;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                await HandleErrorAsync(context, ex);
+            }
+        }
+
+        private async Task HandleErrorAsync(HttpContext context, Exception ex)
+        {
+            var exceptionResponse = _exceptionToResponseMapper.Map(ex);
+
+            context.Response.StatusCode = (int)(exceptionResponse?.StatusCode ?? HttpStatusCode.BadRequest);
+
+            if (exceptionResponse?.Response == null)
+            {
+                await context.Response.WriteAsync(string.Empty);
+
+                return;
+            }
+
+            context.Response.ContentType = "application/json";
+
+            await JsonSerializer.SerializeAsync(context.Response.Body, exceptionResponse.Response);
+        }
+    }
+}
