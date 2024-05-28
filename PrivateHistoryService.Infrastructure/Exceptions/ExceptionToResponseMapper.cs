76f327e87abe9bb1881c418b646b7fb37b3a0112
JivkoSp <atjspasov@gmail.com>
2024-05-28 16:19:09 +0300

    Added implementation of the IExceptionToResponseMapper interface: PrivateHistoryService.Infrastructure/Exceptions/ExceptionToResponseMapper.cs

---
+++ PrivateHistoryService.Infrastructure/Exceptions/ExceptionToResponseMapper.cs
@@ -0,0 +1,62 @@
+﻿using PrivateHistoryService.Domain.Exceptions;
+using PrivateHistoryService.Infrastructure.Exceptions.Interfaces;
+using System.Collections.Concurrent;
+using System.Net;
+
+namespace PrivateHistoryService.Infrastructure.Exceptions
+{
+    internal sealed class ExceptionToResponseMapper : IExceptionToResponseMapper
+    {
+        private static readonly ConcurrentDictionary<Type, string> _errorCodes = new ConcurrentDictionary<Type, string>();
+
+        public ExceptionResponse Map(Exception exception)
+            => exception switch
+            {
+                DomainException ex => new ExceptionResponse(response: new
+                {
+                    ErrorCode = GetExceptionCode(ex),
+                    Reason = ex.Message
+                }, statusCode: HttpStatusCode.BadRequest),
+
+                PrivateHistoryService.Application.Exceptions.ApplicationException ex => new ExceptionResponse(
+                    response: new { ErrorCode = GetExceptionCode(ex), Reason = ex.Message },
+                    statusCode: HttpStatusCode.BadRequest),
+
+                InfrastructureException ex => new ExceptionResponse(response: new
+                {
+                    ErrorCode = GetExceptionCode(ex),
+                    Reason = ex.Message
+                }, statusCode: HttpStatusCode.BadRequest),
+
+                _ => new ExceptionResponse(response: new { ErrorCode = "Error", Reason = "There was an server error!" },
+                        statusCode: HttpStatusCode.InternalServerError)
+            };
+
+        private static string GetExceptionCode(Exception ex)
+        {
+            var exceptionType = ex.GetType();
+
+            if (_errorCodes.TryGetValue(exceptionType, out var code))
+            {
+                return code;
+            }
+
+            var exceptionCode = ex switch
+            {
+                DomainException domainException when !string.IsNullOrWhiteSpace(domainException.ErrorCode) => domainException.ErrorCode,
+
+                PrivateHistoryService.Application.Exceptions.ApplicationException applicationException
+                    when !string.IsNullOrWhiteSpace(applicationException.ErrorCode) => applicationException.ErrorCode,
+
+                InfrastructureException infrastructureException when !string.IsNullOrWhiteSpace(infrastructureException.ErrorCode)
+                    => infrastructureException.ErrorCode,
+
+                _ => exceptionType.Name.Replace("Exception", string.Empty)
+            };
+
+            _errorCodes.TryAdd(exceptionType, exceptionCode);
+
+            return exceptionCode;
+        }
+    }
+}
