c6852fbaf597fdf7e60216ab7253f830d8274d4c
JivkoSp <atjspasov@gmail.com>
2024-05-28 17:23:55 +0300

    Added NEW Application layer custom exception type: PrivateHistoryService.Application/Exceptions/UserAlreadyExistsException.cs.

---
+++ PrivateHistoryService.Application/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Application.Exceptions
+{
+    public sealed class UserAlreadyExistsException : ApplicationException
+    {
+        internal UserAlreadyExistsException(Guid userId) : base(message: $"User # {userId} already exists!")
+        {
+        }
+    }
+}
