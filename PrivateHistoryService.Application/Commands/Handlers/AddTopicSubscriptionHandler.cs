21665ee641a18bf0b0a36808a617a1d842d5b3f2
JivkoSp <atjspasov@gmail.com>
2024-05-28 17:01:22 +0300

    Added conversion of the DateTime property from the AddTopicSubscriptionCommand in PrivateHistoryService.Application/Commands/Handlers/AddTopicSubscriptionHandler.cs. The conversion of the DateTime is to universal time.

--- PrivateHistoryService.Application/Commands/Handlers/AddTopicSubscriptionHandler.cs
+++ PrivateHistoryService.Application/Commands/Handlers/AddTopicSubscriptionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Application.Extensions;
 using PrivateHistoryService.Domain.Repositories;
 using PrivateHistoryService.Domain.ValueObjects;
 
@@ -22,7 +23,7 @@ namespace PrivateHistoryService.Application.Commands.Handlers
                 throw new UserNotFoundException(command.UserId);
             }
 
-            var topicSubscription = new TopicSubscription(command.TopicId, command.UserId, command.DateTime);
+            var topicSubscription = new TopicSubscription(command.TopicId, command.UserId, command.DateTime.ToUtc());
 
             user.AddTopicSubscription(topicSubscription);
 
