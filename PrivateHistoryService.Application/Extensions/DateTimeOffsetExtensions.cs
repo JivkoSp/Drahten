607961bf30efb7698b7edde928126060e80d86f6
JivkoSp <atjspasov@gmail.com>
2024-05-28 17:22:52 +0300

    Added custom DateTimeOffset extension method in PrivateHistoryService.Application/Extensions/DateTimeOffsetExtensions.cs.

---
+++ PrivateHistoryService.Application/Extensions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PrivateHistoryService.Application.Extensions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static DateTimeOffset ToUtc(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.ToUniversalTime();
+        }
+    }
+}
