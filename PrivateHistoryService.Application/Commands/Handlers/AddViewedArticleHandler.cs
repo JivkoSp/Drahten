eddf30dec0cd7925675e8d1d8dd7d66c2bb27491
JivkoSp <atjspasov@gmail.com>
2024-05-28 17:03:04 +0300

    Added conversion of the DateTime property from the AddViewedArticleCommand in PrivateHistoryService.Application/Commands/Handlers/AddViewedArticleHandler.cs. The conversion of the DateTime is to universal time.

--- PrivateHistoryService.Application/Commands/Handlers/AddViewedArticleHandler.cs
+++ PrivateHistoryService.Application/Commands/Handlers/AddViewedArticleHandler.cs
@@ -1,4 +1,5 @@
 ﻿using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Application.Extensions;
 using PrivateHistoryService.Domain.Repositories;
 using PrivateHistoryService.Domain.ValueObjects;
 
@@ -22,7 +23,7 @@ namespace PrivateHistoryService.Application.Commands.Handlers
                 throw new UserNotFoundException(command.UserId);
             }
 
-            var viewedArticle = new ViewedArticle(command.ArticleId, command.UserId, command.DateTime);
+            var viewedArticle = new ViewedArticle(command.ArticleId, command.UserId, command.DateTime.ToUtc());
 
             user.AddViewedArticle(viewedArticle);
 
