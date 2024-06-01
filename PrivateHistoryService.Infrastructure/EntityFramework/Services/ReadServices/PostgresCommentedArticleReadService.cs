99aa6f89cc39444945b2d181efd4633a535e2253
JivkoSp <atjspasov@gmail.com>
2024-06-01 11:33:00 +0300

    Added NEW read service: PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresCommentedArticleReadService.cs. This service implements the ICommentedArticleReadService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresCommentedArticleReadService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Services.ReadServices;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresCommentedArticleReadService : ICommentedArticleReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public PostgresCommentedArticleReadService(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<CommentedArticleDto> GetCommentedArticleByIdAsync(Guid commentedArticleId)
+        {
+            var commentedArticleReadModel = await _readDbContext.CommentedArticles
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.CommentedArticleId == commentedArticleId);
+
+            return _mapper.Map<CommentedArticleDto>(commentedArticleReadModel);
+        }
+    }
+}
