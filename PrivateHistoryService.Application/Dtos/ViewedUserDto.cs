46dac7eb6761242161931b14d9e53eb41e6f63cf
JivkoSp <atjspasov@gmail.com>
2024-06-02 21:55:55 +0300

    Added ViewedUserReadModelId property of type Guid in PrivateHistoryService.Application/Dtos/ViewedUserDto.cs.

--- PrivateHistoryService.Application/Dtos/ViewedUserDto.cs
+++ PrivateHistoryService.Application/Dtos/ViewedUserDto.cs
@@ -3,6 +3,7 @@ namespace PrivateHistoryService.Application.Dtos
 {
     public class ViewedUserDto
     {
+        public Guid ViewedUserReadModelId { get; set; }
         public string ViewerUserId { get; set; }
         public string ViewedUserId { get; set; }
         public DateTimeOffset DateTime { get; set; }
