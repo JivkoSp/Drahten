ead0937f1af3f077e45e3abdf161be785a0117b0
JivkoSp <atjspasov@gmail.com>
2024-06-02 19:03:22 +0300

    Added NEW application layer custom exception type: PrivateHistoryService.Application/Exceptions/SearchedTopicNotFoundException.cs.

---
+++ PrivateHistoryService.Application/Exceptions/SearchedTopicNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace PrivateHistoryService.Application.Exceptions
+{
+    public sealed class SearchedTopicNotFoundException : ApplicationException
+    {
+        internal SearchedTopicNotFoundException(Guid searchedTopicDataId) 
+            : base(message: $"Searched topic #{searchedTopicDataId} was NOT found!")
+        {
+        }
+    }
+}
