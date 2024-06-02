08af4491f1b800681a04868f83c13691731152b2
JivkoSp <atjspasov@gmail.com>
2024-06-02 19:09:15 +0300

    Added SearchedTopicDataId property of type Guid in PrivateHistoryService.Application/Dtos/SearchedTopicDataDto.cs.

--- PrivateHistoryService.Application/Dtos/SearchedTopicDataDto.cs
+++ PrivateHistoryService.Application/Dtos/SearchedTopicDataDto.cs
@@ -3,6 +3,7 @@ namespace PrivateHistoryService.Application.Dtos
 {
     public class SearchedTopicDataDto
     {
+        public Guid SearchedTopicDataId { get; set; }
         public Guid TopicId { get; set; }
         public string UserId { get; set; }
         public string SearchedData { get; set; }
