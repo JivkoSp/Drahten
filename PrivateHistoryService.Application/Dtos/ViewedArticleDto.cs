23d0349cc28e9cc627a9f017caacc5c6feeffcba
JivkoSp <atjspasov@gmail.com>
2024-06-02 14:50:06 +0300

    Added ViewedArticleId property of type Guid in PrivateHistoryService.Application/Dtos/ViewedArticleDto.cs.

--- PrivateHistoryService.Application/Dtos/ViewedArticleDto.cs
+++ PrivateHistoryService.Application/Dtos/ViewedArticleDto.cs
@@ -3,6 +3,7 @@ namespace PrivateHistoryService.Application.Dtos
 {
     public class ViewedArticleDto
     {
+        public Guid ViewedArticleId { get; set; }
         public string ArticleId { get; set; }
         public string UserId { get; set; }
         public DateTimeOffset DateTime { get; set; }
