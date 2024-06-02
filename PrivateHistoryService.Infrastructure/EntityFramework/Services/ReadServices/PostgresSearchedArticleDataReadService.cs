ba2938377ce04ec481b167087e540c0eb0ed25cf
JivkoSp <atjspasov@gmail.com>
2024-06-02 15:27:32 +0300

    Added NEW read service: PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresSearchedArticleDataReadService.cs. This service implements the ISearchedArticleDataReadService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresSearchedArticleDataReadService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Services.ReadServices;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresSearchedArticleDataReadService : ISearchedArticleDataReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public PostgresSearchedArticleDataReadService(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<SearchedArticleDataDto> GetSearchedArticleDataByIdAsync(Guid searchedArticleDataId)
+        {
+            var searchedArticleDataReadModel = await _readDbContext.SearchedArticles
+                    .AsNoTracking()
+                    .SingleOrDefaultAsync(x => x.SearchedArticleDataId == searchedArticleDataId);
+
+            return _mapper.Map<SearchedArticleDataDto>(searchedArticleDataReadModel);
+        }
+    }
+}
