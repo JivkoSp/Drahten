5bc6a4134c5c2b512943ccb344ceb976d6c6e602
JivkoSp <atjspasov@gmail.com>
2024-06-02 21:58:15 +0300

    Added NEW read service: PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresViewedUserReadService.cs. This service implements the IViewedUserReadService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresViewedUserReadService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Services.ReadServices;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresViewedUserReadService : IViewedUserReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public PostgresViewedUserReadService(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ViewedUserDto> GetViewedUserByIdAsync(Guid viewedUserId)
+        {
+            var viewedUserReadModel = await _readDbContext.ViewedUsers
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.ViewedUserReadModelId == viewedUserId);
+
+            return _mapper.Map<ViewedUserDto>(viewedUserReadModel);
+        }
+    }
+}
