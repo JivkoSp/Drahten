b837a99267b79943de5f572ba39447cdac7e6bfe
JivkoSp <atjspasov@gmail.com>
2024-06-02 19:06:01 +0300

    Added ISearchedTopicDataReadService dependency in PrivateHistoryService.Application/Commands/Handlers/RemoveSearchedTopicDataHandler.cs. Added retrieval of SearchedTopicDataDto in the HandleAsync method in the same command handler.

--- PrivateHistoryService.Application/Commands/Handlers/RemoveSearchedTopicDataHandler.cs
+++ PrivateHistoryService.Application/Commands/Handlers/RemoveSearchedTopicDataHandler.cs
@@ -1,5 +1,6 @@
 ﻿using PrivateHistoryService.Application.Exceptions;
 using PrivateHistoryService.Application.Extensions;
+using PrivateHistoryService.Application.Services.ReadServices;
 using PrivateHistoryService.Domain.Repositories;
 using PrivateHistoryService.Domain.ValueObjects;
 
@@ -8,10 +9,12 @@ namespace PrivateHistoryService.Application.Commands.Handlers
     internal sealed class RemoveSearchedTopicDataHandler : ICommandHandler<RemoveSearchedTopicDataCommand>
     {
         private readonly IUserRepository _userRepository;
+        private readonly ISearchedTopicDataReadService _searchedTopicDataReadService;
 
-        public RemoveSearchedTopicDataHandler(IUserRepository userRepository)
+        public RemoveSearchedTopicDataHandler(IUserRepository userRepository, ISearchedTopicDataReadService searchedTopicDataReadService)
         {
             _userRepository = userRepository;
+            _searchedTopicDataReadService = searchedTopicDataReadService;
         }
 
         public async Task HandleAsync(RemoveSearchedTopicDataCommand command)
@@ -23,7 +26,15 @@ namespace PrivateHistoryService.Application.Commands.Handlers
                 throw new UserNotFoundException(command.UserId);
             }
 
-            var searchedTopicData = new SearchedTopicData(command.TopicId, command.UserId, command.SearchedData, command.DateTime.ToUtc());
+            var searchedTopicDataDto = await _searchedTopicDataReadService.GetSearchedTopicDataByIdAsync(command.SearchedTopicDataId);
+
+            if (searchedTopicDataDto == null)
+            {
+                throw new SearchedTopicNotFoundException(command.SearchedTopicDataId);
+            }
+
+            var searchedTopicData = new SearchedTopicData(searchedTopicDataDto.TopicId, Guid.Parse(searchedTopicDataDto.UserId), 
+                searchedTopicDataDto.SearchedData, searchedTopicDataDto.DateTime.ToUtc());
 
             user.RemoveSearchedTopicData(searchedTopicData);
 
