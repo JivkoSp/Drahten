8b83bd32728ee35ba82c0adcd27c5c539e5c7967
JivkoSp <atjspasov@gmail.com>
2024-06-02 19:12:55 +0300

    Modified the PrivateHistoryService.Tests.Unit/Application/Handlers/RemoveSearchedTopicDataHandlerTests.cs. Added NEW unit test: Throws_SearchedTopicNotFoundException_When_SearchedTopicData_With_Given_SearchedTopicDataId_Is_Not_Returned_From_Repository; Added GetSearchedTopicDataDto method; Added ISearchedTopicDataReadService mock

--- PrivateHistoryService.Tests.Unit/Application/Handlers/RemoveSearchedTopicDataHandlerTests.cs
+++ PrivateHistoryService.Tests.Unit/Application/Handlers/RemoveSearchedTopicDataHandlerTests.cs
@@ -1,7 +1,9 @@
 ﻿using NSubstitute;
 using PrivateHistoryService.Application.Commands;
 using PrivateHistoryService.Application.Commands.Handlers;
+using PrivateHistoryService.Application.Dtos;
 using PrivateHistoryService.Application.Exceptions;
+using PrivateHistoryService.Application.Services.ReadServices;
 using PrivateHistoryService.Domain.Entities;
 using PrivateHistoryService.Domain.Factories;
 using PrivateHistoryService.Domain.Factories.Interfaces;
@@ -18,18 +20,12 @@ namespace PrivateHistoryService.Tests.Unit.Application.Handlers
 
         private readonly IUserFactory _userConcreteFactory;
         private readonly IUserRepository _userRepository;
+        private readonly ISearchedTopicDataReadService _searchedTopicDataReadService;
         private readonly ICommandHandler<RemoveSearchedTopicDataCommand> _handler;
 
-        private RemoveSearchedTopicDataCommand GetRemoveSearchedTopicDataCommand(Guid? topicId = null, Guid? userId = null,
-            string searchedData = null, DateTimeOffset? dateTime = null)
+        private RemoveSearchedTopicDataCommand GetRemoveSearchedTopicDataCommand()
         {
-            var TopicId = topicId ?? Guid.NewGuid();
-            var UserId = userId ?? Guid.NewGuid();
-            var SearchedData = searchedData ?? "...";
-            var DateTime = dateTime ?? DateTimeOffset.Now;
-
-            var command = new RemoveSearchedTopicDataCommand(TopicId: TopicId, UserId: UserId,
-                SearchedData: SearchedData, DateTime: DateTime);
+            var command = new RemoveSearchedTopicDataCommand(UserId: Guid.NewGuid(), SearchedTopicDataId: Guid.NewGuid());
 
             return command;
         }
@@ -42,11 +38,26 @@ namespace PrivateHistoryService.Tests.Unit.Application.Handlers
             return searchedTopicData;
         }
 
+        private SearchedTopicDataDto GetSearchedTopicDataDto(SearchedTopicData searchedTopicData)
+        {
+            var searchedTopicDataDto = new SearchedTopicDataDto
+            {
+                SearchedTopicDataId = Guid.NewGuid(),
+                TopicId = searchedTopicData.TopicID.Value,
+                UserId = searchedTopicData.UserID.Value.ToString(),
+                SearchedData = searchedTopicData.SearchedData,
+                DateTime = searchedTopicData.DateTime
+            };
+
+            return searchedTopicDataDto;
+        }
+
         public RemoveSearchedTopicDataHandlerTests()
         {
             _userConcreteFactory = new UserFactory();
             _userRepository = Substitute.For<IUserRepository>();
-            _handler = new RemoveSearchedTopicDataHandler(_userRepository);
+            _searchedTopicDataReadService = Substitute.For<ISearchedTopicDataReadService>();
+            _handler = new RemoveSearchedTopicDataHandler(_userRepository, _searchedTopicDataReadService);
         }
 
         #endregion
@@ -73,23 +84,51 @@ namespace PrivateHistoryService.Tests.Unit.Application.Handlers
             exception.ShouldBeOfType<UserNotFoundException>();
         }
 
-        //Should remove SearchedTopicData value object if the UserId from the RemoveSearchedTopicDataCommand is valid Id for an existing User.
-        //The SearchedTopicData value object to be removed must have the same values as those in the RemoveSearchedTopicDataCommand.
-        //Ensure the repository is called to update the User.
+        //Should throw SearchedTopicNotFoundException when the following condition is met:
+        //There is no SearchedTopicData returned from the repository that corresponds to the SearchedTopicDataId from the RemoveSearchedTopicDataCommand.
         [Fact]
-        public async Task Given_Valid_UserId_Removes_SearchedTopicData_Instance_From_User_And_Calls_Repository_On_Success()
+        public async Task Throws_SearchedTopicNotFoundException_When_SearchedTopicData_With_Given_SearchedTopicDataId_Is_Not_Returned_From_Repository()
+        {
+            //ARRANGE
+            var user = _userConcreteFactory.Create(Guid.NewGuid());
+
+            var removeSearchedTopicDataCommand = GetRemoveSearchedTopicDataCommand();
+
+            _userRepository.GetUserByIdAsync(removeSearchedTopicDataCommand.UserId).Returns(user);
+
+            _searchedTopicDataReadService.GetSearchedTopicDataByIdAsync(removeSearchedTopicDataCommand.SearchedTopicDataId)
+                .Returns(default(SearchedTopicDataDto));
+
+            //ACT
+            var exception = await Record.ExceptionAsync(async () => await Act(removeSearchedTopicDataCommand));
+
+            //ASSERT
+            exception.ShouldNotBeNull();
+
+            exception.ShouldBeOfType<SearchedTopicNotFoundException>();
+        }
+
+        //Should remove SearchedTopicData value object if the UserId and SearchedTopicDataId from the RemoveSearchedTopicDataCommand
+        //are valid Ids for an existing User domain entity and SearchedTopicData value object.
+        //The repository must be called to update the User.
+        [Fact]
+        public async Task Given_Valid_UserId_And_SearchedTopicDataId_Removes_SearchedTopicData_Instance_From_User_And_Calls_Repository_On_Success()
         {
             //ARRANGE
             var user = _userConcreteFactory.Create(Guid.NewGuid());
 
             var searchedTopicData = GetSearchedTopicData();
 
-            var removeSearchedTopicDataCommand = GetRemoveSearchedTopicDataCommand(
-                searchedTopicData.TopicID, searchedTopicData.UserID, searchedTopicData.SearchedData, searchedTopicData.DateTime);
+            var searchedTopicDataDto = GetSearchedTopicDataDto(searchedTopicData);
+
+            var removeSearchedTopicDataCommand = GetRemoveSearchedTopicDataCommand();
 
             user.AddSearchedTopicData(searchedTopicData);
 
             _userRepository.GetUserByIdAsync(removeSearchedTopicDataCommand.UserId).Returns(user);
+
+            _searchedTopicDataReadService.GetSearchedTopicDataByIdAsync(removeSearchedTopicDataCommand.SearchedTopicDataId)
+                .Returns(searchedTopicDataDto);
 
             //ACT
             var exception = await Record.ExceptionAsync(async () => await Act(removeSearchedTopicDataCommand));
