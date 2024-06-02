44210859be70779bb91a0d046ac5c3cd80908f51
JivkoSp <atjspasov@gmail.com>
2024-06-02 19:02:21 +0300

    Added NEW read service interface: PrivateHistoryService.Application/Services/ReadServices/ISearchedTopicDataReadService.cs. The interface defines GetSearchedTopicDataByIdAsync method.

---
+++ PrivateHistoryService.Application/Services/ReadServices/ISearchedTopicDataReadService.cs
@@ -0,0 +1,9 @@
+﻿using PrivateHistoryService.Application.Dtos;
+
+namespace PrivateHistoryService.Application.Services.ReadServices
+{
+    public interface ISearchedTopicDataReadService
+    {
+        Task<SearchedTopicDataDto> GetSearchedTopicDataByIdAsync(Guid searchedTopicDataId);
+    }
+}
