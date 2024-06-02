b22877e2948f2a1138477c6db1e5de4d32355543
JivkoSp <atjspasov@gmail.com>
2024-06-02 20:52:08 +0300

    Added NEW read service interface: PrivateHistoryService.Application/Services/ReadServices/IViewedUserReadService.cs. The interface defines GetViewedUserByIdAsync method.

---
+++ PrivateHistoryService.Application/Services/ReadServices/IViewedUserReadService.cs
@@ -0,0 +1,9 @@
+﻿using PrivateHistoryService.Application.Dtos;
+
+namespace PrivateHistoryService.Application.Services.ReadServices
+{
+    public interface IViewedUserReadService
+    {
+        Task<ViewedUserDto> GetViewedUserByIdAsync(Guid viewedUserId);
+    }
+}
