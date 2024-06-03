63774504cea30b91871eaf00a039bd84d8bce57a
JivkoSp <atjspasov@gmail.com>
2024-06-03 23:01:01 +0300

    Added NEW controller: DrahtenWeb/Controllers/HistoryController.cs.

---
+++ DrahtenWeb/Controllers/HistoryController.cs
@@ -0,0 +1,20 @@
+﻿using DrahtenWeb.Dtos;
+using DrahtenWeb.Models;
+using DrahtenWeb.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DrahtenWeb.Controllers
+{
+    [Authorize]
+    public class HistoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult ViewHistory()
+        {
+            return View();
+        }
+    }
+}
