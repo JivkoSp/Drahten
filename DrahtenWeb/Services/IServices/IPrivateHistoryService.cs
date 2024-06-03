52dd1548e9c37e084cdfe16adbe52bdd8ab55a54
JivkoSp <atjspasov@gmail.com>
2024-06-03 12:36:42 +0300

    Added NEW service interface: DrahtenWeb/Services/IServices/IPrivateHistoryService.cs.

---
+++ DrahtenWeb/Services/IServices/IPrivateHistoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace DrahtenWeb.Services.IServices
+{
+    public interface IPrivateHistoryService : IBaseService
+    {
+        Task<TEntity> GetCommentedArticlesAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetLikedArticlesAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetDislikedArticlesAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetLikedArticleCommentsAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetDislikedArticleCommentsAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetSearchedArticlesAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetSearchedTopicsAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetTopicSubscriptionsAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetViewedArticlesAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> GetViewedUsersAsync<TEntity>(Guid userId, string accessToken);
+        Task<TEntity> RemoveCommentedArticleAsync<TEntity>(Guid userId, Guid commentedArticleId, string accessToken);
+        Task<TEntity> RemoveSearchedArticleDataAsync<TEntity>(Guid userId, Guid searchedArticleDataId, string accessToken);
+        Task<TEntity> RemoveSearchedTopicDataAsync<TEntity>(Guid userId, Guid searchedTopicDataId, string accessToken);
+        Task<TEntity> RemoveTopicSubscriptionAsync<TEntity>(Guid userId, Guid topicId, string accessToken);
+        Task<TEntity> RemoveViewedArticleAsync<TEntity>(Guid userId, Guid viewedArticleId, string accessToken);
+        Task<TEntity> RemoveViewedUserAsync<TEntity>(Guid userId, Guid viewedUserId, string accessToken);
+    }
+}
