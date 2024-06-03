c6473b30d3f2b71dcc6a17d95c082c75eb4379d4
JivkoSp <atjspasov@gmail.com>
2024-06-03 23:09:03 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/HistoryArticleViewModel.cs

---
+++ DrahtenWeb/ViewModels/HistoryArticleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrahtenWeb.Dtos.PrivateHistoryService;
+using DrahtenWeb.Models;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class HistoryArticleViewModel
+    {
+        public List<ViewedArticleDto> Articles { get; set; }
+        public Pagination Pagination { get; set; }
+    }
+}
