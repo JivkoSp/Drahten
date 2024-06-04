6e0885c69d005f206da502b0a761396329ab036f
JivkoSp <atjspasov@gmail.com>
2024-06-04 17:06:20 +0300

    Added NEW dto type: DrahtenWeb/Dtos/PrivateHistoryService/ViewedUserDto.cs.

---
+++ DrahtenWeb/Dtos/PrivateHistoryService/ViewedUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.Dtos.PrivateHistoryService
+{
+    public class ViewedUserDto
+    {
+        public Guid ViewedUserReadModelId { get; set; }
+        public string ViewerUserId { get; set; }
+        public string ViewedUserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
