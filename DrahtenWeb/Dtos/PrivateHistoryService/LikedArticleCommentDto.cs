14f665648c19d7b291168d4fd886934ea9fd471b
JivkoSp <atjspasov@gmail.com>
2024-06-05 12:15:50 +0300

    Added NEW dto type: DrahtenWeb/Dtos/PrivateHistoryService/LikedArticleCommentDto.cs.

---
+++ DrahtenWeb/Dtos/PrivateHistoryService/LikedArticleCommentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.Dtos.PrivateHistoryService
+{
+    public class LikedArticleCommentDto
+    {
+        public Guid ArticleCommentId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
