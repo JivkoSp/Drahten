4532578364ff725f10c21e275c7df91f6dae41cf
JivkoSp <atjspasov@gmail.com>
2024-06-05 13:40:35 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/HistoryDislikedArticleCommentViewModel.cs.

---
+++ DrahtenWeb/ViewModels/HistoryDislikedArticleCommentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrahtenWeb.Dtos.PrivateHistoryService;
+using DrahtenWeb.Models;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class HistoryDislikedArticleCommentViewModel
+    {
+        public List<DislikedArticleCommentDto> DislikedArticleComments { get; set; }
+        public Pagination Pagination { get; set; }
+    }
+}
