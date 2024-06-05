99c5586f962bccff6ae2ac57580afa138466d33d
JivkoSp <atjspasov@gmail.com>
2024-06-05 17:17:22 +0300

    Added UserRegistration/ directory in PrivateHistoryService.Infrastructure/. This directory contains IUserSynchronizer.cs interface that defines SynchronizeUserAsync method.

---
+++ PrivateHistoryService.Infrastructure/UserRegistration/IUserSynchronizer.cs
@@ -0,0 +1,9 @@
+﻿using PrivateHistoryService.Application.Commands;
+
+namespace PrivateHistoryService.Infrastructure.UserRegistration
+{
+    public interface IUserSynchronizer
+    {
+        Task SynchronizeUserAsync(AddUserCommand addUserCommand);
+    }
+}
