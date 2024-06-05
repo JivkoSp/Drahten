9e3a1ae987b06c1141ff223c5eab5bc057a01170
JivkoSp <atjspasov@gmail.com>
2024-06-05 10:12:53 +0300

    Added NEW view model: DrahtenWeb/ViewModels/HistoryCommentedArticleViewModel.cs

---
+++ DrahtenWeb/ViewModels/HistoryCommentedArticleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrahtenWeb.Dtos.PrivateHistoryService;
+using DrahtenWeb.Models;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class HistoryCommentedArticleViewModel
+    {
+        public List<CommentedArticleDto> CommentedArticles { get; set; }
+        public Pagination Pagination { get; set; }
+    }
+}
