c01ed669fa25b2fbc257e3f7271bb6c816b0c205
JivkoSp <atjspasov@gmail.com>
2024-06-05 14:22:28 +0300

    Added NEW dto type: DrahtenWeb/Dtos/PrivateHistoryService/SearchedTopicDataDto.cs.

---
+++ DrahtenWeb/Dtos/PrivateHistoryService/SearchedTopicDataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DrahtenWeb.Dtos.PrivateHistoryService
+{
+    public class SearchedTopicDataDto
+    {
+        public Guid SearchedTopicDataId { get; set; }
+        public Guid TopicId { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
