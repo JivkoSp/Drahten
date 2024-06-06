e5e7a62cf6824ae2b745738b2510f22efe9d348a
JivkoSp <atjspasov@gmail.com>
2024-06-06 19:56:59 +0300

    Added WriteServices/ directory in PrivateHistoryService.Infrastructure/EntityFramework/Services/. This directory contains PostgresViewedArticleWriteService.cs. This class implements the IViewedArticleWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresViewedArticleWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresViewedArticleWriteService : IViewedArticleWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresViewedArticleWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddViewedArticleAsync(ViewedArticle viewedArticle)
+        {
+            await _writeDbContext.ViewedArticles.AddAsync(viewedArticle);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
