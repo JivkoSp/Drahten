f66bff3f54a51540ca66ff11b3010cf5c030e976
JivkoSp <atjspasov@gmail.com>
2024-06-06 15:54:56 +0300

    Added TopicArticleService.Infrastructure/AsyncDataServices/MessageDescriptor.cs. This class encapsulates the information for message that is published to a RabbitMq message broker.

---
+++ TopicArticleService.Infrastructure/AsyncDataServices/MessageDescriptor.cs
@@ -0,0 +1,33 @@
+﻿using TopicArticleService.Infrastructure.Exceptions;
+
+namespace TopicArticleService.Infrastructure.AsyncDataServices
+{
+    internal sealed class MessageDescriptor
+    {
+        public string Message { get; }
+        public string Exchange { get; }
+        public string RoutingKey { get; }
+
+        internal MessageDescriptor(string message, string exchange, string routingKey)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new EmptyMessageDescriptorMessageException();
+            }
+
+            if (string.IsNullOrWhiteSpace(exchange))
+            {
+                throw new EmptyMessageDescriptorExchangeException();
+            }
+
+            if (string.IsNullOrWhiteSpace(routingKey))
+            {
+                throw new EmptyMessageDescriptorRoutingKeyException();
+            }
+
+            Message = message;
+            Exchange = exchange;
+            RoutingKey = routingKey;
+        }
+    }
+}
