94c7b82c445e52233eff5db7e83643a0f71a5a3c
JivkoSp <atjspasov@gmail.com>
2024-06-06 14:39:48 +0300

    Added AsyncDataServices/ directory in PrivateHistoryService.Infrastructure/. This directory contains MessageBusSubscriber.cs.

---
+++ PrivateHistoryService.Infrastructure/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using PrivateHistoryService.Infrastructure.EventProcessing;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace PrivateHistoryService.Infrastructure.AsyncDataServices
+{
+    //This is long running background service.
+    public sealed class MessageBusSubscriber : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IEventProcessor _eventProcessor;
+        private IConnection _connection;
+        private IModel _channel;
+        private string _queueName;
+
+        public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+        {
+            _configuration = configuration;
+            _eventProcessor = eventProcessor;
+            InitializeRabbitMq();
+        }
+
+        private void InitializeRabbitMq()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+
+            _connection = factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(exchange: "topic_article_service", type: ExchangeType.Direct);
+
+            _queueName = _channel.QueueDeclare().QueueName;
+
+            _channel.QueueBind(queue: _queueName,
+                       exchange: "topic_article_service",
+                       routingKey: "topic_article_service.viewed-article");
+
+            Console.WriteLine("\n--> PrivateHistoryService listening on the message bus!\n");
+
+            _connection.ConnectionShutdown += RabbitMqConnectionShutDown;
+        }
+
+        private void RabbitMqConnectionShutDown(object sender, ShutdownEventArgs args)
+        {
+            Console.WriteLine("--> PrivateHistoryService connection shutdown!");
+        }
+
+        //Listening for events from the message bus (RabbitMQ).
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += async (ModuleHandle, deliverEventArgs) => {
+
+                Console.WriteLine("--> PrivateHistoryService event received!");
+
+                var body = deliverEventArgs.Body;
+
+                var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+                await _eventProcessor.ProcessEventAsync(notificationMessage);
+            };
+
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+
+            base.Dispose();
+        }
+    }
+}
