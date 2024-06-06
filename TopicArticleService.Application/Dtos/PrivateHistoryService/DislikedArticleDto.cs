4de9417626ed08e8d8a079b8b3bd2744309b0b32
JivkoSp <atjspasov@gmail.com>
2024-06-06 20:32:39 +0300

    Added NEW dto type: TopicArticleService.Application/Dtos/PrivateHistoryService/DislikedArticleDto.cs.

---
+++ TopicArticleService.Application/Dtos/PrivateHistoryService/DislikedArticleDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TopicArticleService.Application.Dtos.PrivateHistoryService
+{
+    public class DislikedArticleDto
+    {
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string Event { get; set; }
+    }
+}
