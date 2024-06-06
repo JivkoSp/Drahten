17dc221c7c0a6d97a943d2d98706e7bd500fb3ab
JivkoSp <atjspasov@gmail.com>
2024-06-06 20:46:08 +0300

    Added NEW dto type: TopicArticleService.Application/Dtos/PrivateHistoryService/LikedArticleCommentDto.cs.

---
+++ TopicArticleService.Application/Dtos/PrivateHistoryService/LikedArticleCommentDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TopicArticleService.Application.Dtos.PrivateHistoryService
+{
+    public class LikedArticleCommentDto
+    {
+        public Guid ArticleCommentId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public string Event { get; set; }
+    }
+}
