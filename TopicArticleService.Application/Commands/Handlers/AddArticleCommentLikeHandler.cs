4425440f51ee6b0418c4c9e83f09657de632b5c6
JivkoSp <atjspasov@gmail.com>
2024-06-07 09:16:07 +0300

    Added publishing of LikedArticleCommentDto to the message broker in the HandleAsync method from TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs.

--- TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs
+++ TopicArticleService.Application/Commands/Handlers/AddArticleCommentLikeHandler.cs
@@ -1,5 +1,8 @@
-﻿using TopicArticleService.Application.Exceptions;
+﻿using TopicArticleService.Application.AsyncDataServices;
+using TopicArticleService.Application.Dtos.PrivateHistoryService;
+using TopicArticleService.Application.Exceptions;
 using TopicArticleService.Application.Extensions;
+using TopicArticleService.Application.Services.ReadServices;
 using TopicArticleService.Domain.Factories;
 using TopicArticleService.Domain.Repositories;
 
@@ -8,13 +11,17 @@ namespace TopicArticleService.Application.Commands.Handlers
     internal sealed class AddArticleCommentLikeHandler : ICommandHandler<AddArticleCommentLikeCommand>
     {
         private readonly IArticleCommentRepository _articleCommentRepository;
+        private readonly IArticleCommentReadService _articleCommentReadService;
         private readonly IArticleCommentLikeFactory _articleCommentLikeFactory;
+        private readonly IMessageBusPublisher _messageBusPublisher;
 
-        public AddArticleCommentLikeHandler(IArticleCommentRepository articleCommentRepository,
-                IArticleCommentLikeFactory articleCommentLikeFactory)
+        public AddArticleCommentLikeHandler(IArticleCommentRepository articleCommentRepository, IArticleCommentReadService articleCommentReadService,
+                IArticleCommentLikeFactory articleCommentLikeFactory, IMessageBusPublisher messageBusPublisher)
         {
             _articleCommentRepository = articleCommentRepository;
+            _articleCommentReadService = articleCommentReadService;
             _articleCommentLikeFactory = articleCommentLikeFactory;
+            _messageBusPublisher = messageBusPublisher;
         }
 
         public async Task HandleAsync(AddArticleCommentLikeCommand command)
@@ -25,6 +32,20 @@ namespace TopicArticleService.Application.Commands.Handlers
             {
                 throw new ArticleCommentNotFoundException(command.ArticleCommentId);
             }
+
+            var articleCommentDto = await _articleCommentReadService.GetArticleCommentByIdAsync(command.ArticleCommentId);
+
+            var likedArticleCommentDto = new LikedArticleCommentDto
+            {
+                ArticleCommentId = command.ArticleCommentId,
+                ArticleId = articleCommentDto.ArticleDto.ArticleId,
+                UserId = command.UserId.ToString(),
+                DateTime = command.DateTime,
+                Event = "LikedArticleComment"
+            };
+
+            //Post message to the message broker about adding like for article-comment with ID: ArticleCommentId by user with ID: UserId.
+            _messageBusPublisher.PublishLikedArticleComment(likedArticleCommentDto);
 
             var articleCommentLike = _articleCommentLikeFactory.Create(command.ArticleCommentId, command.UserId, 
                 command.DateTime.ToUtc());
