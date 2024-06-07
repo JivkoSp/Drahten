8a3191d425b35ca2f74e45773e6bc367f5f18543
JivkoSp <atjspasov@gmail.com>
2024-06-07 20:25:50 +0300

    Added atomapper mapping configuration for the LikedArticleCommentDto and LikedArticleComment in PrivateHistoryService.Infrastructure/Automapper/Profiles/LikedArticleCommentProfile.cs.

--- PrivateHistoryService.Infrastructure/Automapper/Profiles/LikedArticleCommentProfile.cs
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/LikedArticleCommentProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Extensions;
+using PrivateHistoryService.Domain.ValueObjects;
 using PrivateHistoryService.Infrastructure.EntityFramework.Models;
 
 namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
@@ -9,6 +11,10 @@ namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
         public LikedArticleCommentProfile()
         {
             CreateMap<LikedArticleCommentReadModel, LikedArticleCommentDto>();
+
+            CreateMap<LikedArticleCommentDto, LikedArticleComment>()
+               .ConstructUsing(source =>
+                   new LikedArticleComment(Guid.Parse(source.ArticleId), Guid.Parse(source.UserId), source.ArticleCommentId, source.DateTime.ToUtc()));
         }
     }
 }
