a82c4ad8f1659a5e73100de477c625dc0f7093f7
JivkoSp <atjspasov@gmail.com>
2024-06-07 22:17:00 +0300

    Added automapper mapping configuration for the DislikedArticleCommentDto and DislikedArticleComment in PrivateHistoryService.Infrastructure/Automapper/Profiles/DislikedArticleCommentProfile.cs.

--- PrivateHistoryService.Infrastructure/Automapper/Profiles/DislikedArticleCommentProfile.cs
+++ PrivateHistoryService.Infrastructure/Automapper/Profiles/DislikedArticleCommentProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using PrivateHistoryService.Application.Dtos;
+using PrivateHistoryService.Application.Extensions;
+using PrivateHistoryService.Domain.ValueObjects;
 using PrivateHistoryService.Infrastructure.EntityFramework.Models;
 
 namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
@@ -9,6 +11,10 @@ namespace PrivateHistoryService.Infrastructure.Automapper.Profiles
         public DislikedArticleCommentProfile()
         {
             CreateMap<DislikedArticleCommentReadModel, DislikedArticleCommentDto>();
+
+            CreateMap<DislikedArticleCommentDto, DislikedArticleComment>()
+              .ConstructUsing(source =>
+                  new DislikedArticleComment(Guid.Parse(source.ArticleId), Guid.Parse(source.UserId), source.ArticleCommentId, source.DateTime.ToUtc()));
         }
     }
 }
