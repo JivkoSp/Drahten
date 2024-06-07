614f9f1997fecd11b8cd970a737fcd94e7c37a5b
JivkoSp <atjspasov@gmail.com>
2024-06-07 19:03:47 +0300

    Added NEW write service: PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresLikedArticleCommentWriteService.cs. This service implements the ILikedArticleCommentWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresLikedArticleCommentWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresLikedArticleCommentWriteService : ILikedArticleCommentWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresLikedArticleCommentWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddLikedArticleCommentAsync(LikedArticleComment likedArticleComment)
+        {
+            await _writeDbContext.LikedArticleComments.AddAsync(likedArticleComment);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
