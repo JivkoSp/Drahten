5169e48cfe05ed302968a2df4f258e0fa2b4ee50
JivkoSp <atjspasov@gmail.com>
2024-06-07 22:05:04 +0300

    Added NEW write service interface: PrivateHistoryService.Application/Services/WriteServices/IDislikedArticleCommentWriteService.cs.

---
+++ PrivateHistoryService.Application/Services/WriteServices/IDislikedArticleCommentWriteService.cs
@@ -0,0 +1,10 @@
+﻿
+using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Services.WriteServices
+{
+    public interface IDislikedArticleCommentWriteService
+    {
+        Task AddDislikedArticleCommentAsync(DislikedArticleComment dislikedArticleComment);
+    }
+}
