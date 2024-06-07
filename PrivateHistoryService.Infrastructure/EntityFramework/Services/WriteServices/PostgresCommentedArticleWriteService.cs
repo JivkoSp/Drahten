8819da1548095dd14c935535986c684172b9a64f
JivkoSp <atjspasov@gmail.com>
2024-06-07 18:37:38 +0300

    Added NEW write service: PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresCommentedArticleWriteService.cs. This service implements the ICommentedArticleWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresCommentedArticleWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresCommentedArticleWriteService : ICommentedArticleWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresCommentedArticleWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddCommentedArticleAsync(CommentedArticle commentedArticle)
+        {
+            await _writeDbContext.CommentedArticles.AddAsync(commentedArticle);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
