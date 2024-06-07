ca2182dcdd61188b5974c84c3d976e14e0b1d501
JivkoSp <atjspasov@gmail.com>
2024-06-07 22:37:14 +0300

    Added NEW write service interface: PrivateHistoryService.Application/Services/WriteServices/ITopicSubscriptionWriteService.cs.

---
+++ PrivateHistoryService.Application/Services/WriteServices/ITopicSubscriptionWriteService.cs
@@ -0,0 +1,9 @@
+﻿using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Services.WriteServices
+{
+    public interface ITopicSubscriptionWriteService
+    {
+        Task AddTopicSubscriptionAsync(TopicSubscription topicSubscription);
+    }
+}
