ea0bb59d9445ec614efe015741ca26bc7ee6f187
JivkoSp <atjspasov@gmail.com>
2024-06-07 22:39:57 +0300

    Adde NEW write service: PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresTopicSubscriptionWriteService.cs. This service implements the ITopicSubscriptionWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresTopicSubscriptionWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresTopicSubscriptionWriteService : ITopicSubscriptionWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresTopicSubscriptionWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddTopicSubscriptionAsync(TopicSubscription topicSubscription)
+        {
+            await _writeDbContext.TopicSubscriptions.AddAsync(topicSubscription);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
