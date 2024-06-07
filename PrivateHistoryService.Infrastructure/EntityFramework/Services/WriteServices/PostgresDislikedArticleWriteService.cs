9bffbd91ae50a530f4f0e88ffa93f01e36dd2bdf
JivkoSp <atjspasov@gmail.com>
2024-06-07 17:42:12 +0300

    Added NEW write service: PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresDislikedArticleWriteService.cs. This service implements the IDislikedArticleWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresDislikedArticleWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresDislikedArticleWriteService : IDislikedArticleWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresDislikedArticleWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddLikedArticleAsync(DislikedArticle dislikedArticle)
+        {
+            await _writeDbContext.DislikedArticles.AddAsync(dislikedArticle);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
