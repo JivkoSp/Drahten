bdd19b9d2c3b0ea86c99235f64ac3ba50fb39314
JivkoSp <atjspasov@gmail.com>
2024-06-08 21:48:43 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/DislikedArticleCommentViewModel.cs.

---
+++ DrahtenWeb/ViewModels/DislikedArticleCommentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class DislikedArticleCommentViewModel
+    {
+        public Guid ArticleCommentId { get; set; }
+        public ArticleDto Article { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
