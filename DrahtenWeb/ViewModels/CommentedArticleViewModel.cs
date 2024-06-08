6bfe90d082d5d0fc8eeafc79c139a8a282cb22f3
JivkoSp <atjspasov@gmail.com>
2024-06-08 20:19:27 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/CommentedArticleViewModel.cs.

---
+++ DrahtenWeb/ViewModels/CommentedArticleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class CommentedArticleViewModel
+    {
+        public Guid CommentedArticleId { get; set; }
+        public ArticleDto Article { get; set; }
+        public string UserId { get; set; }
+        public string ArticleComment { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
