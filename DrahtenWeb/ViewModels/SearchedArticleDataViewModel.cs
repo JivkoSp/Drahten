28a1f922436b065b601e72413b18853a886ac82e
JivkoSp <atjspasov@gmail.com>
2024-06-08 19:13:29 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/SearchedArticleDataViewModel.cs.

---
+++ DrahtenWeb/ViewModels/SearchedArticleDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class SearchedArticleDataViewModel
+    {
+        public Guid SearchedArticleDataId { get; set; }
+        public ArticleDto Article { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
