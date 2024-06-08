2666ef1524e2ad87c6599e4838a880dc4d35ade4
JivkoSp <atjspasov@gmail.com>
2024-06-08 20:00:48 +0300

    Modified the DrahtenWeb/ViewModels/HistoryDislikedArticleViewModel.cs. The modifications are: Changed the DislikedArticles property type from List<DislikedArticleDto> to List<DislikedArticleViewModel>.

--- DrahtenWeb/ViewModels/HistoryDislikedArticleViewModel.cs
+++ DrahtenWeb/ViewModels/HistoryDislikedArticleViewModel.cs
@@ -1,11 +1,10 @@
-﻿using DrahtenWeb.Dtos.PrivateHistoryService;
-using DrahtenWeb.Models;
+﻿using DrahtenWeb.Models;
 
 namespace DrahtenWeb.ViewModels
 {
     public class HistoryDislikedArticleViewModel
     {
-        public List<DislikedArticleDto> DislikedArticles { get; set; }
+        public List<DislikedArticleViewModel> DislikedArticles { get; set; } = new List<DislikedArticleViewModel>();
         public Pagination Pagination { get; set; }
     }
 }
