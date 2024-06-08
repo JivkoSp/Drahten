9d3cd03432132c4fb7ce89a803153bc6eb5759e5
JivkoSp <atjspasov@gmail.com>
2024-06-08 18:31:36 +0300

    Added NEW view model type: DrahtenWeb/ViewModels/ViewedUserViewModel.cs.

---
+++ DrahtenWeb/ViewModels/ViewedUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrahtenWeb.Dtos.UserService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class ViewedUserViewModel
+    {
+        public Guid ViewedUserReadModelId { get; set; }
+        public UserDto ViewedUser { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
