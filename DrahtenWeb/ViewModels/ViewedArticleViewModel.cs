5ba1d3cd6d1cc900b4867ffe02b8cd166e076c7d
JivkoSp <atjspasov@gmail.com>
2024-06-08 15:50:01 +0300

    Added NEW view model: DrahtenWeb/ViewModels/ViewedArticleViewModel.cs

---
+++ DrahtenWeb/ViewModels/ViewedArticleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class ViewedArticleViewModel
+    {
+        public Guid ViewedArticleId { get; set; }
+        public ArticleDto Article { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
