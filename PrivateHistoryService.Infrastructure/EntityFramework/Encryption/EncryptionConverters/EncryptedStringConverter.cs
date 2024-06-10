cedd7c759cd18c3b77ed51ed6275b3f84e142dfd
JivkoSp <atjspasov@gmail.com>
2024-06-10 22:50:22 +0300

    Added Encryption/ directory in PrivateHistoryService.Infrastructure/EntityFramework/. This directory contains EncryptionConverters/ and EncryptionProvider/ sub-directories. The EncryptionConverters/ sub-directory contains the following custom converters that will be used with EntityFramework: EncryptedArticleCommentConverter.cs, EncryptedDateTimeOffsetConverter.cs and EncryptedStringConverter.cs. The EncryptionProvider/ sub-directory contains IEncryptionProvider.cs that defines Encrypt and Decrypt methods and EncryptionProvider.cs class that implements the IEncryptionProvider.cs interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PrivateHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedStringConverter<T> : ValueConverter<T, string> where T : class
+    {
+        public EncryptedStringConverter(IEncryptionProvider encryptionProvider)
+            : base(v => encryptionProvider.Encrypt(v.ToString()),
+                   v => (T)Convert.ChangeType(encryptionProvider.Decrypt(v), typeof(T)))
+        {
+        }
+    }
+}
