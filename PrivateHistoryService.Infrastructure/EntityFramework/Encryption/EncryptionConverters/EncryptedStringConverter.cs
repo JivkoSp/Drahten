8f3d97bbf8f45afa17ec54130c0a9d6cd48a5d36
JivkoSp <atjspasov@gmail.com>
2024-06-11 12:57:21 +0300

    Changed the PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs to be non-generic. The reason is that this convertor will be used only for conversion of string properties, thus it's not needed to be generic.

--- PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
+++ PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
@@ -3,11 +3,11 @@ using PrivateHistoryService.Infrastructure.EntityFramework.Encryption.Encryption
 
 namespace PrivateHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
 {
-    internal sealed class EncryptedStringConverter<T> : ValueConverter<T, string> where T : class
+    internal sealed class EncryptedStringConverter : ValueConverter<string, string>
     {
         public EncryptedStringConverter(IEncryptionProvider encryptionProvider)
             : base(v => encryptionProvider.Encrypt(v.ToString()),
-                   v => (T)Convert.ChangeType(encryptionProvider.Decrypt(v), typeof(T)))
+                   v => encryptionProvider.Decrypt(v))
         {
         }
     }
