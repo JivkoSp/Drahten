7a1ceb68247cc6e63545686ea650be0f0232f43c
JivkoSp <atjspasov@gmail.com>
2024-06-11 13:47:50 +0300

    Added NEW EntityFramework migration in PrivateHistoryService.Infrastructure/EntityFramework/Migrations/. The migration is 20240611103934_Change all DateTime column types to string.cs.

--- PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240611103934_Change all DateTime column types to string.Designer.cs
+++ PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240611103934_Change all DateTime column types to string.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
@@ -11,9 +12,11 @@ using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
 namespace PrivateHistoryService.Infrastructure.EntityFramework.Migrations
 {
     [DbContext(typeof(ReadDbContext))]
-    partial class ReadDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240611103934_Change all DateTime column types to string")]
+    partial class ChangeallDateTimecolumntypestostring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240611103934_Change all DateTime column types to string.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeallDateTimecolumntypestostring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "ViewedUser",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "ViewedArticle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "TopicSubscription",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "SearchedTopicData",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "SearchedArticleData",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "LikedArticleComment",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "LikedArticle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "DislikedArticleComment",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "DislikedArticle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "ViewedUser",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "ViewedArticle",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "TopicSubscription",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "SearchedTopicData",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "SearchedArticleData",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "LikedArticleComment",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "LikedArticle",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "DislikedArticleComment",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "private-history-service",
+                table: "DislikedArticle",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
