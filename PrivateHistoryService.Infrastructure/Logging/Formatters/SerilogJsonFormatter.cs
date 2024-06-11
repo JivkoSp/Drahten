590c72dfd37fd0d1ee1397cf2b5a6295942a28e1
JivkoSp <atjspasov@gmail.com>
2024-06-11 15:57:39 +0300

    Added Logging/ directory in PrivateHistoryService.Infrastructure/. This directory contains Formatters/ sub-directory that has SerilogJsonFormatter.cs. This is custom IBatchFormatter that formats log messages to proper JSON.

---
+++ PrivateHistoryService.Infrastructure/Logging/Formatters/SerilogJsonFormatter.cs
@@ -0,0 +1,23 @@
+﻿using Serilog.Sinks.Http;
+using Newtonsoft.Json.Linq;
+
+namespace PrivateHistoryService.Infrastructure.Logging.Formatters
+{
+    public sealed class SerilogJsonFormatter : IBatchFormatter
+    {
+        public void Format(IEnumerable<string> logEvents, TextWriter output)
+        {
+            foreach (var logEvent in logEvents)
+            {
+                // Parse each log event string into a JSON object.
+                var logEventObject = JObject.Parse(logEvent);
+
+                // Serialize the JSON object back to JSON with indentation for readability.
+                var serializedLogEvent = logEventObject.ToString(Newtonsoft.Json.Formatting.None);
+
+                // Write the serialized log event to the output.
+                output.WriteLine(serializedLogEvent);
+            }
+        }
+    }
+}
