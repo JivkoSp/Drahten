a2e67ae14ed28c9473d7fb21a8b099356c6d7347
JivkoSp <atjspasov@gmail.com>
2024-06-13 21:00:25 +0300

    Added NEW EntityFramework migration in PrivateHistoryService.Infrastructure/EntityFramework/Migrations/. The migration is 20240613152409_Added RetentionUntil property of type DateTimeOffset to UserR

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240613152409_Added RetentionUntil property of type DateTimeOffset to UserReadModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRetentionUntilpropertyoftypeDateTimeOffsettoUserReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "RetentionUntil",
+                schema: "private-history-service",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RetentionUntil",
+                schema: "private-history-service",
+                table: "User");
+        }
+    }
+}
