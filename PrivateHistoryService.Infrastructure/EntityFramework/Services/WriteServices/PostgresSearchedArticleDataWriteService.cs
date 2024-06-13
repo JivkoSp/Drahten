ff79629beb40f0f54f3f71cb100b138018ce91a0
JivkoSp <atjspasov@gmail.com>
2024-06-13 11:08:44 +0300

    Added NEW write service: PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresSearchedArticleDataWriteService.cs. This service implements the ISearchedArticleDataWriteService interface.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Services/WriteServices/PostgresSearchedArticleDataWriteService.cs
@@ -0,0 +1,23 @@
+﻿using PrivateHistoryService.Application.Services.WriteServices;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Services.WriteServices
+{
+    internal sealed class PostgresSearchedArticleDataWriteService : ISearchedArticleDataWriteService
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresSearchedArticleDataWriteService(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public async Task AddSearchedArticleDataAsync(SearchedArticleData searchedArticleData)
+        {
+            await _writeDbContext.SearchedArticles.AddAsync(searchedArticleData);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
