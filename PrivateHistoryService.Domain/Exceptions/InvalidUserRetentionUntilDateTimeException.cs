1b700272b919627570c38a8a2a53612933356736
JivkoSp <atjspasov@gmail.com>
2024-06-13 21:32:46 +0300

    Added two NEW domain custom exception types in PrivateHistoryService.Domain/Exceptions/. The exception types are: InvalidUserRetentionUntilDateTimeException.cs and NullUserRetentionUntilException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/InvalidUserRetentionUntilDateTimeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class InvalidUserRetentionUntilDateTimeException : DomainException
+    {
+        internal InvalidUserRetentionUntilDateTimeException() : base(message: "Invalid datetime for user retention!")
+        {
+        }
+    }
+}
---
+++ PrivateHistoryService.Domain/Exceptions/NullUserRetentionUntilException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class NullUserRetentionUntilException : DomainException
+    {
+        internal NullUserRetentionUntilException(Guid userId) : base(message: $"User #{userId} cannot be set with a null UserRetentionUntil value!")
+        {
+        }
+    }
+}
