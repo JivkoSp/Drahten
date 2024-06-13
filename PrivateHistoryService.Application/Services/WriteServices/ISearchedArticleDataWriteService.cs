ae60b560d61970e63727e927f39129249415ca59
JivkoSp <atjspasov@gmail.com>
2024-06-13 10:56:27 +0300

    Added NEW write service interface: PrivateHistoryService.Application/Services/WriteServices/ISearchedArticleDataWriteService.cs.

---
+++ PrivateHistoryService.Application/Services/WriteServices/ISearchedArticleDataWriteService.cs
@@ -0,0 +1,9 @@
+﻿using PrivateHistoryService.Domain.ValueObjects;
+
+namespace PrivateHistoryService.Application.Services.WriteServices
+{
+    public interface ISearchedArticleDataWriteService
+    {
+        Task AddSearchedArticleDataAsync(SearchedArticleData searchedArticleData);
+    }
+}
