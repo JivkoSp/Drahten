1ba88c8c3157233c47336f905e2f0d4001ddc1b1
JivkoSp <atjspasov@gmail.com>
2024-06-13 23:59:37 +0300

    Added NEW write service interface: PrivateHistoryService.Application/Services/WriteServices/IUserWriteService.cs.

---
+++ PrivateHistoryService.Application/Services/WriteServices/IUserWriteService.cs
@@ -0,0 +1,11 @@
+﻿using PrivateHistoryService.Domain.Entities;
+
+namespace PrivateHistoryService.Application.Services.WriteServices
+{
+    public interface IUserWriteService
+    {
+        Task<List<User>> GetUsersAsync();
+        Task UpdateUsersAsync(List<User> users);
+        Task DeleteUsersAsync(List<User> users);
+    }
+}
