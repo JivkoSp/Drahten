810d7407145e30f23fbe2516e38b3d062d71ee13
JivkoSp <atjspasov@gmail.com>
2024-06-13 10:27:28 +0300

    Added generation for ID's for each received document with the UUIDGenerator from app.uuidGeneration in Drahten_Services_SerarchService/drahten_scraper/drahten_scraper/pipelines.py.

--- Drahten_Services_SerarchService/drahten_scraper/drahten_scraper/pipelines.py
+++ Drahten_Services_SerarchService/drahten_scraper/drahten_scraper/pipelines.py
@@ -6,9 +6,8 @@
 from scrapy.utils.serialize import ScrapyJSONEncoder
 from haystack.schema import Document as HaystackDocument
 import json
-import uuid
+from app.uuidGeneration import UUIDGenerator
 from app.asyncDataServices import messageBusClient
-
 
 class DrahtenScraperPipeline:
     def __init__(self):
@@ -24,22 +23,19 @@ class DrahtenScraperPipeline:
         try:
             spider_name = item.get('spider_name')[0]
 
-            #Encode the scrapy object type (item) as JSON string.
+            # Encode the scrapy object type (item) as JSON string.
             encoded_item = self.jsonEncoder.encode(item)
             
-            #Convert the encoded_item from JSON string to JSON object.
+            # Convert the encoded_item from JSON string to JSON object.
             encoded_item = json.loads(encoded_item)
-
-            # Generate a UUID
-            generated_uuid = uuid.uuid4()
-
-            # Convert UUID to string without dashes
-            uuid_string = str(generated_uuid).replace('-', '')
+            
+            # Generate ID for the haystack document, based on the encoded_item['article_data'][0] (The document content)
+            document_id = UUIDGenerator.generate_uuid_from_content(encoded_item['article_data'][0])
 
             #Create haystack document type.
             #This step is needed, becouse the ElasticsearchDocumentStore stores data as haystack document types.
             #Here the meta is Dict[str, Any] and it's purpose is to hold the data from the document for easy access.
-            new_document = HaystackDocument(id=uuid_string,
+            new_document = HaystackDocument(id=document_id,
                                             content=encoded_item['article_data'][0], 
                                             meta={"article_prev_title": encoded_item['article_prev_title'][0],
                                                   "article_title": encoded_item['article_title'][0],
@@ -50,6 +46,7 @@ class DrahtenScraperPipeline:
 
             # Publish the Haystack document (new_document) object to the message bus (RabbitMq).
             self.messageBusPublisher.PublishDocumentForSimilarityCheck(new_document, spider_name)
+            
         except Exception as ex:
             #TODO: Log the exception to logging service
             print(f"Exception: {ex}")
