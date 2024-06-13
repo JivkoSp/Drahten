5d86496eb2a6fc7e3fbdfff0bf5981366683c222
JivkoSp <atjspasov@gmail.com>
2024-06-13 21:31:08 +0300

    Added NEW value object: PrivateHistoryService.Domain/ValueObjects/UserRetentionUntil.cs.

---
+++ PrivateHistoryService.Domain/ValueObjects/UserRetentionUntil.cs
@@ -0,0 +1,20 @@
+﻿
+using PrivateHistoryService.Domain.Exceptions;
+
+namespace PrivateHistoryService.Domain.ValueObjects
+{
+    public record UserRetentionUntil
+    {
+        internal DateTimeOffset RetentionUntil { get; set; } // Date until which data should be retained
+
+        public UserRetentionUntil(DateTimeOffset dateTime)
+        {
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidUserRetentionUntilDateTimeException();
+            }
+
+            RetentionUntil = dateTime;
+        }
+    }
+}
