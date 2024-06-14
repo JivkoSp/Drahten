1d2739a7a74a8ef13ed096c8c930f62fb3a56f7e
JivkoSp <atjspasov@gmail.com>
2024-06-14 12:21:06 +0300

    Added NEW encryption custom converter: PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedUserRetentionUntilConverter.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedUserRetentionUntilConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PrivateHistoryService.Domain.ValueObjects;
+using PrivateHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedUserRetentionUntilConverter : ValueConverter<UserRetentionUntil, string>
+    {
+        public EncryptedUserRetentionUntilConverter(IEncryptionProvider encryptionProvider, ConverterMappingHints mappingHints = null)
+            : base(
+                  v => ConvertToDateTimeOffset(v, encryptionProvider),
+                  v => ConvertToUserRetentionUntil(v, encryptionProvider),
+                  mappingHints)
+        {
+        }
+
+        private static string ConvertToDateTimeOffset(DateTimeOffset dateTime, IEncryptionProvider encryptionProvider)
+        {
+            // Encrypt the DateTime
+            string encryptedValue = encryptionProvider.Encrypt(dateTime.ToString("o"));
+
+            return encryptedValue;
+        }
+
+        private static UserRetentionUntil ConvertToUserRetentionUntil(string value, IEncryptionProvider encryptionProvider)
+        {
+            // Decrypt the string
+            string decryptedValue = encryptionProvider.Decrypt(value);
+
+            // Convert string to ArticleComment
+            return new UserRetentionUntil(DateTimeOffset.Parse(decryptedValue));
+        }
+    }
+}
