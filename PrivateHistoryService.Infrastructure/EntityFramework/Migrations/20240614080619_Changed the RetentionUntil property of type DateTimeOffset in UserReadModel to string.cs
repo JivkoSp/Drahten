614a956d77d351da05473ed10fddcc84889169e7
JivkoSp <atjspasov@gmail.com>
2024-06-14 12:48:09 +0300

    Added NEW EntityFramework migration in PrivateHistoryService.Infrastructure/EntityFramework/Migrations/. The migration is: 20240614080619_Changed the RetentionUntil property of type DateTimeOffset in UserReadModel to string.cs

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240614080619_Changed the RetentionUntil property of type DateTimeOffset in UserReadModel to string.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedtheRetentionUntilpropertyoftypeDateTimeOffsetinUserReadModeltostring : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RetentionUntil",
+                schema: "private-history-service",
+                table: "User",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "RetentionUntil",
+                schema: "private-history-service",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
