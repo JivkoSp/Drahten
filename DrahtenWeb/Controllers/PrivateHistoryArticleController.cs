3c8cb15db556f2567cc97d9d8443d340402448bd
JivkoSp <atjspasov@gmail.com>
2024-06-15 17:15:33 +0300

    Added NEW controller: DrahtenWeb/Controllers/PrivateHistoryArticleController.cs. The controller has ViewArticle action method.

---
+++ DrahtenWeb/Controllers/PrivateHistoryArticleController.cs
@@ -0,0 +1,29 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+using DrahtenWeb.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DrahtenWeb.Controllers
+{
+    [Authorize]
+    public class PrivateHistoryArticleController : Controller
+    {
+        private readonly IPrivateHistoryService _privateHistoryService;
+        private readonly ITopicArticleService _topicArticleService;
+
+        public PrivateHistoryArticleController(IPrivateHistoryService privateHistoryService, ITopicArticleService topicArticleService)
+        {
+            _privateHistoryService = privateHistoryService;
+            _topicArticleService = topicArticleService;
+        }
+
+        [HttpPost]
+        public IActionResult ViewArticle(string articleJson)
+        {
+            var article = JsonConvert.DeserializeObject<ArticleDto>(articleJson);
+
+            return View(article);
+        }
+    }
+}
