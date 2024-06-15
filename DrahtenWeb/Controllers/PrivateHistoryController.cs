34a15f382f51869aae53b86cd73cf368d32d6c03
JivkoSp <atjspasov@gmail.com>
2024-06-15 12:15:23 +0300

    Added NEW action method: RemoveCommentedArticle in DrahtenWeb/Controllers/PrivateHistoryController.cs.

--- DrahtenWeb/Controllers/PrivateHistoryController.cs
+++ DrahtenWeb/Controllers/PrivateHistoryController.cs
@@ -674,5 +674,17 @@ namespace DrahtenWeb.Controllers
 
             await _privateHistoryService.RemoveSearchedArticleDataAsync<string>(userId, searchedArticleDataId, accessToken);
         }
+
+        [HttpDelete]
+        public async Task RemoveCommentedArticle(Guid commentedArticleId)
+        {
+            //Get the user id.
+            //Here the NameIdentifier claim type represents the user id.
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            await _privateHistoryService.RemoveCommentedArticleAsync<string>(userId, commentedArticleId, accessToken);
+        }
     }
 }
