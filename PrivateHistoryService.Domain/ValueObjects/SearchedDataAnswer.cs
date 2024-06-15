51471134dbd0b5dd4d3ccfc620476bc08a436bc3
JivkoSp <atjspasov@gmail.com>
2024-06-15 22:42:44 +0300

    Added NEW value object: PrivateHistoryService.Domain/ValueObjects/SearchedDataAnswer.cs

---
+++ PrivateHistoryService.Domain/ValueObjects/SearchedDataAnswer.cs
@@ -0,0 +1,27 @@
+﻿using PrivateHistoryService.Domain.Exceptions;
+
+namespace PrivateHistoryService.Domain.ValueObjects
+{
+    public record SearchedDataAnswer
+    {
+        internal string Value { get; }
+
+        public SearchedDataAnswer(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptySearchedDataAnswerException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to String.
+        public static implicit operator string(SearchedDataAnswer searchedDataAnswer)
+            => searchedDataAnswer.Value;
+
+        //Conversion from String to ValueObject.
+        public static implicit operator SearchedDataAnswer(string searchedDataAnswer)
+            => new SearchedDataAnswer(searchedDataAnswer);
+    }
+}
