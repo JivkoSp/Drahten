a0ed8335d050dcd458bfdb0637736f425466328a
JivkoSp <atjspasov@gmail.com>
2024-06-16 13:36:35 +0300

    Added NEW EnityFramework migration in PrivateHistoryService.Infrastructure/EntityFramework/Migrations/. The migration is: 20240616101910_Added SearchedDataAnswerContext property of type string in SearchedArticleDataReadModel.cs.

---
+++ PrivateHistoryService.Infrastructure/EntityFramework/Migrations/20240616101910_Added SearchedDataAnswerContext property of type string in SearchedArticleDataReadModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrivateHistoryService.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSearchedDataAnswerContextpropertyoftypestringinSearchedArticleDataReadModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SearchedDataAnswerContext",
+                schema: "private-history-service",
+                table: "SearchedArticleData",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchedDataAnswerContext",
+                schema: "private-history-service",
+                table: "SearchedArticleData");
+        }
+    }
+}
