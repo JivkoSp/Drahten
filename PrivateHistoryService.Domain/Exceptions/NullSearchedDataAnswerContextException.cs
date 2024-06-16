635abc446251b5796dfa4b341b499d11ca1d7929
JivkoSp <atjspasov@gmail.com>
2024-06-16 12:56:00 +0300

    Added NEW custom domain exception: PrivateHistoryService.Domain/Exceptions/NullSearchedDataAnswerContextException.cs

---
+++ PrivateHistoryService.Domain/Exceptions/NullSearchedDataAnswerContextException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class NullSearchedDataAnswerContextException : DomainException
+    {
+        internal NullSearchedDataAnswerContextException() : base(message: $"The searched data answer context cannot be null!")
+        {
+        }
+    }
+}
