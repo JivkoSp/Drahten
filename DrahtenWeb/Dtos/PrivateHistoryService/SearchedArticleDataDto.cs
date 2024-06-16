e9b49566e53cab82521f711029358388f8de3902
JivkoSp <atjspasov@gmail.com>
2024-06-16 19:18:28 +0300

    Added two new properties in DrahtenWeb/Dtos/PrivateHistoryService/SearchedArticleDataDto.cs. The properties are: SearchedDataAnswer of type string and SearchedDataAnswerContext of type string.

--- DrahtenWeb/Dtos/PrivateHistoryService/SearchedArticleDataDto.cs
+++ DrahtenWeb/Dtos/PrivateHistoryService/SearchedArticleDataDto.cs
@@ -6,6 +6,8 @@
         public string ArticleId { get; set; }
         public string UserId { get; set; }
         public string SearchedData { get; set; }
+        public string SearchedDataAnswer { get; set; }
+        public string SearchedDataAnswerContext { get; set; }
         public DateTimeOffset DateTime { get; set; }
         public DateTimeOffset? RetentionUntil { get; set; }
     }
