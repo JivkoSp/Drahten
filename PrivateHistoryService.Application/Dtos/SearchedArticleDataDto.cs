fa1766f905acbb42e331f636921c71ecd655f77a
JivkoSp <atjspasov@gmail.com>
2024-06-16 10:26:10 +0300

    Added SearchedDataAnswer property of type string in PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs.

--- PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs
+++ PrivateHistoryService.Application/Dtos/SearchedArticleDataDto.cs
@@ -7,6 +7,7 @@ namespace PrivateHistoryService.Application.Dtos
         public string ArticleId { get; set; }
         public string UserId { get; set; }
         public string SearchedData { get; set; }
+        public string SearchedDataAnswer { get; set; }
         public DateTimeOffset DateTime { get; set; }
         public DateTimeOffset? RetentionUntil { get; set; }
     }
