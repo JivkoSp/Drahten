24ccfc561ed764f2f2c61fcd9c0fa32b4664e24b
JivkoSp <atjspasov@gmail.com>
2024-06-16 12:11:49 +0300

    Added NEW custom domain exception: PrivateHistoryService.Domain/Exceptions/EmptySearchedDataAnswerContextException.cs.

---
+++ PrivateHistoryService.Domain/Exceptions/EmptySearchedDataAnswerContextException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PrivateHistoryService.Domain.Exceptions
+{
+    public sealed class EmptySearchedDataAnswerContextException : DomainException
+    {
+        internal EmptySearchedDataAnswerContextException() : base(message: $"The searched data answer context cannot be empty!")
+        {
+        }
+    }
+}
