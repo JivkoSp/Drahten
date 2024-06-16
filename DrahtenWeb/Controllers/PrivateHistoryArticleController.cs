4a6326dbd79f43b1560ad6b9afccb5594a8e4c3a
JivkoSp <atjspasov@gmail.com>
2024-06-16 19:19:42 +0300

    Added NEW action method: SearchedArticleData in DrahtenWeb/Controllers/PrivateHistoryArticleController.cs.

--- DrahtenWeb/Controllers/PrivateHistoryArticleController.cs
+++ DrahtenWeb/Controllers/PrivateHistoryArticleController.cs
@@ -1,8 +1,14 @@
-﻿using DrahtenWeb.Dtos.TopicArticleService;
+﻿using DrahtenWeb.Dtos;
+using DrahtenWeb.Dtos.PrivateHistoryService;
+using DrahtenWeb.Dtos.TopicArticleService;
+using DrahtenWeb.Extensions;
 using DrahtenWeb.Services.IServices;
+using DrahtenWeb.ViewModels;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace DrahtenWeb.Controllers
 {
@@ -24,6 +30,31 @@ namespace DrahtenWeb.Controllers
             var article = JsonConvert.DeserializeObject<ArticleDto>(articleJson);
 
             return View(article);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchedArticleData(string articleId)
+        {
+            try
+            {
+                //Get the user id.
+                //Here the NameIdentifier claim type represents the user id.
+                var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                var response = await _privateHistoryService.GetSearchedArticlesAsync<ResponseDto>(userId, accessToken);
+
+                var allSearchedArticles = response.Map<List<SearchedArticleDataDto>>();
+
+                var searchedArticles = allSearchedArticles.Where(x => x.ArticleId == articleId).ToList();
+
+                return new JsonResult(searchedArticles);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
