9ff8c74c6987158e89577694f4ed6b30db9a172d
JivkoSp <atjspasov@gmail.com>
2024-06-18 18:30:06 +0300

    Refactored the SendAsync method from DrahtenWeb/Services/BaseService.cs. This method is now divided in three methods: CreateHttpRequestMessage, SendHttpRequestAsync and SendAsync. Added Polly retry policy in the SendAsync method.

--- DrahtenWeb/Services/BaseService.cs
+++ DrahtenWeb/Services/BaseService.cs
@@ -3,8 +3,11 @@ using DrahtenWeb.Exceptions;
 using DrahtenWeb.Models;
 using DrahtenWeb.Services.IServices;
 using Newtonsoft.Json;
+using Polly.Retry;
+using Polly;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Net;
 
 namespace DrahtenWeb.Services
 {
@@ -18,51 +21,29 @@ namespace DrahtenWeb.Services
             _httpClientFactory = httpClientFactory;
         }
 
-        /// <summary>
-        /// Method that sends http request.
+        // <summary>
+        /// Creates an <see cref="HttpRequestMessage"/> based on the specified <paramref name="apiRequest"/>.
         /// </summary>
-        /// <typeparam name="T">Represents the type of response that this method will generate.</typeparam>
-        /// <input><param name="apiRequest">
-        /// Object that represents http reguest, includes: Type, Url, Data, AccessToken.
-        /// </param></input> 
-        /// <returns>Task<T></returns>
-        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        /// <param name="apiRequest">The <see cref="ApiRequest"/> object containing the details for the HTTP request.</param>
+        /// <returns>A new instance of <see cref="HttpRequestMessage"/> configured with the data from <paramref name="apiRequest"/>.</returns>
+        private HttpRequestMessage CreateHttpRequestMessage(ApiRequest apiRequest)
         {
-            if (string.IsNullOrEmpty(apiRequest.Url))
+            var httpRequestMessage = new HttpRequestMessage
             {
-                throw new UrlNotFoundException();
-            }
+                RequestUri = new Uri(apiRequest.Url),
+                Headers = { { "Accept", "application/json" } }
+            };
 
-            var httpClient = _httpClientFactory.CreateClient();
-
-            httpClient.DefaultRequestHeaders.Clear();
-
-            //Check if the request has accesstoken.
-            if (!string.IsNullOrEmpty(apiRequest.AccessToken))
-            {
-                //The request HAS accesstoken.
-
-                //Put the accesstoken in the authorization header of the request.
-                httpClient.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue("Bearer", apiRequest.AccessToken);
-            }
-
-            var httpRequestMessage = new HttpRequestMessage();
-
-            httpRequestMessage.Headers.Add("Accept", "application/json");
-
-            httpRequestMessage.RequestUri = new Uri(apiRequest.Url);
-
-            //Check if the request has data.
+            // Check if the request has data.
             if (apiRequest.Data != null)
             {
-                //The request HAS data, set the contents of the http message.
+                // The request HAS data, set the contents of the HTTP message.
                 httpRequestMessage.Content = new StringContent(
                     JsonConvert.SerializeObject(apiRequest.Data),
                     Encoding.UTF8, "application/json");
             }
 
-            //Check the TYPE of the request.
+            // Check the TYPE of the request.
             switch (apiRequest.ApiType)
             {
                 case ApiType.GET:
@@ -79,19 +60,87 @@ namespace DrahtenWeb.Services
                     break;
             }
 
-            //Send the http request and initialize the variable "httpResponse" with object of type HttpResponseMessage,
-            //containing the CONTENT OF THE HTTP RESPONSE MESSAGE, STATUS CODE and other information that relates to the request.
-            var httpResponse = await httpClient.SendAsync(httpRequestMessage);
+            return httpRequestMessage;
+        }
 
-            //Initialize the variable "httpResponseSerializedContent" with the CONTENT OF THE HTTP RESPONSE MESSAGE.
-            //This is json serialized string.
-            var httpResponseSerializedContent = await httpResponse.Content.ReadAsStringAsync();
+        /// <summary>
+        /// Sends an HTTP request based on the specified <paramref name="apiRequest"/> using the provided <paramref name="httpClient"/>.
+        /// </summary>
+        /// <param name="apiRequest">The <see cref="ApiRequest"/> object containing the details for the HTTP request.</param>
+        /// <param name="httpClient">The <see cref="HttpClient"/> instance used to send the request.</param>
+        /// <returns>
+        /// A task representing the asynchronous operation. The task result is the <see cref="HttpResponseMessage"/> from the request.
+        /// </returns>
+        private async Task<HttpResponseMessage> SendHttpRequestAsync(ApiRequest apiRequest, HttpClient httpClient)
+        {
+            var httpRequestMessage = CreateHttpRequestMessage(apiRequest);
+            return await httpClient.SendAsync(httpRequestMessage);
+        }
 
-            //Initialize the variable "httpResponseDeserializedContent" with the deserialized content
-            //of "httpResponseSerializedContent".
-            var httpResponseDeserializedContent = JsonConvert.DeserializeObject<T>(httpResponseSerializedContent);
+        /// <summary>
+        /// Sends an HTTP request based on the specified <paramref name="apiRequest"/> and asynchronously retrieves the response.
+        /// </summary>
+        /// <typeparam name="T">The type of the response object that this method will return.</typeparam>
+        /// <param name="apiRequest">
+        /// An <see cref="ApiRequest"/> object representing the HTTP request, including request type, URL, data, and access token.
+        /// </param>
+        /// <returns>
+        /// A task representing the asynchronous operation. The task result is the response deserialized into an object of type <typeparamref name="T"/>.
+        /// </returns>
+        /// <exception cref="UrlNotFoundException">Thrown when the URL in <paramref name="apiRequest"/> is null or empty.</exception>
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            if (string.IsNullOrEmpty(apiRequest.Url))
+            {
+                throw new UrlNotFoundException();
+            }
 
-            return httpResponseDeserializedContent;
+            var httpClient = _httpClientFactory.CreateClient();
+
+            httpClient.DefaultRequestHeaders.Clear();
+
+            // Check if the request has access token.
+            if (!string.IsNullOrEmpty(apiRequest.AccessToken))
+            {
+                // The request HAS access token.
+                // Put the access token in the authorization header of the request.
+                httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", apiRequest.AccessToken);
+            }
+
+            // Define the retry policy
+            AsyncRetryPolicy<HttpResponseMessage> retryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.InternalServerError || 
+                                                        r.StatusCode == HttpStatusCode.ServiceUnavailable ||
+                                                        r.StatusCode == HttpStatusCode.BadGateway)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (result, timeSpan, retryCount, context) =>
+                    {
+                        // TODO: Log the retry attempt
+                        Console.WriteLine($"Retry {retryCount} for {apiRequest.Url} after {timeSpan.Seconds} seconds delay due to {result.Result.StatusCode}.");
+                    });
+
+            try
+            {
+                // Execute the HTTP request within the retry policy
+                var httpResponse = await retryPolicy.ExecuteAsync(() => SendHttpRequestAsync(apiRequest, httpClient));
+
+                // Initialize the variable "httpResponseSerializedContent" with the CONTENT OF THE HTTP RESPONSE MESSAGE.
+                // This is json serialized string.
+                var httpResponseSerializedContent = await httpResponse.Content.ReadAsStringAsync();
+
+                // Initialize the variable "httpResponseDeserializedContent" with the deserialized content
+                // of "httpResponseSerializedContent".
+                var httpResponseDeserializedContent = JsonConvert.DeserializeObject<T>(httpResponseSerializedContent);
+
+                return httpResponseDeserializedContent;
+            }
+            catch (HttpRequestException ex)
+            {
+                // Log the exception or handle it as needed
+                Console.WriteLine($"HttpRequestException occurred: {ex.Message}");
+                throw;
+            }
         }
     }
 }
