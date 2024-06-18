b2934436c9dacf5ce8c68f08ef5aeba8da82163d
JivkoSp <atjspasov@gmail.com>
2024-06-18 20:28:16 +0300

    Added Options/ directory in Drahten_ApiGateway_Yarp/. This directory contains RateLimitOptions.cs. This class contains default options for ratelimiting.

---
+++ Drahten_ApiGateway_Yarp/Options/RateLimitOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Drahten_ApiGateway_Yarp.Options
+{
+    public class RateLimitOptions
+    {
+        public int RequestLimit { get; set; } = 50;
+        public int ViolationThreshold { get; set; } = 5;
+        public int BlacklistDurationMinutes { get; set; } = 60;
+        public int BlacklistHttpStatusCode { get; set; } = StatusCodes.Status403Forbidden;
+        public int ViolationHttpStatusCode { get; set; } = StatusCodes.Status429TooManyRequests;
+    }
+}
