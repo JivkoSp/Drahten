b367d76316ec7d0c2a332af92a154ec5d6afe109
JivkoSp <atjspasov@gmail.com>
2024-06-19 17:57:58 +0300

    Added Attributes/ directory in DrahtenWeb/. This directory contains custom attribute: CheckTermsAndConditionsAttribute.cs.

---
+++ DrahtenWeb/Attributes/CheckTermsAndConditionsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrahtenWeb.Attributes
+{
+    public class CheckTermsAndConditionsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var consentCookie = context.HttpContext.Request.Cookies["TermsAndConditionsConsent"];
+
+            // Check if the user has not consented to terms and conditions.
+            if (string.IsNullOrEmpty(consentCookie) || !bool.TryParse(consentCookie, out bool hasConsented) || !hasConsented)
+            {
+                // User has not consented or cookie doesn't exist, redirect to terms and conditions page.
+                context.Result = new RedirectToActionResult("Index", "TermsAndConditions", null);
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
