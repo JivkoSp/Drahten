6535ef7afa6e20eec7f28e9c239925b59813967b
JivkoSp <atjspasov@gmail.com>
2024-06-19 17:59:57 +0300

    Added NEW controller: DrahtenWeb/Controllers/TermsAndConditionsController.cs.

---
+++ DrahtenWeb/Controllers/TermsAndConditionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrahtenWeb.Controllers
+{
+    [Authorize]
+    public class TermsAndConditionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        // Action to handle user consent.
+        [HttpPost]
+        public IActionResult Consent()
+        {
+            Response.Cookies.Append("TermsAndConditionsConsent", "true");
+
+            // Redirect to the home page.
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
