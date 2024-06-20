8eb787b116d79b0efd3e14aaa9dd05b4a6750eaa
JivkoSp <atjspasov@gmail.com>
2024-06-20 13:21:38 +0300

    Added Encryption/ directory in TopicArticleService.Infrastructure/EntityFramework/. This directory contains EncryptionProvider/ sub-directory that contains IEncryptionProvider.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionProvider/IEncryptionProvider.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionProvider
+{
+    public interface IEncryptionProvider
+    {
+        string Encrypt(string plaintext);
+        string Decrypt(string combinedData);
+    }
+}
