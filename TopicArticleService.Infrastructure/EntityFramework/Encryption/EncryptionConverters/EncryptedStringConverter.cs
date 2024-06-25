1b360a91bc775de3bfa24e7ac01a375b7b107433
JivkoSp <atjspasov@gmail.com>
2024-06-25 10:42:31 +0300

    Added custom EntityFramework converter: TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedStringConverter : ValueConverter<string, string>
+    {
+        public EncryptedStringConverter(IEncryptionProvider encryptionProvider)
+            : base(v => encryptionProvider.Encrypt(v.ToString()),
+                   v => encryptionProvider.Decrypt(v))
+        {
+        }
+    }
+}
