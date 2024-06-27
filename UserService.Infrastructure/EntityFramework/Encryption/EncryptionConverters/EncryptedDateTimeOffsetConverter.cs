b874f9fae502f2300da04a06bb8ac901436d0fd6
JivkoSp <atjspasov@gmail.com>
2024-06-27 17:23:48 +0300

    Added NEW custom EntityFramework converter: UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs.

---
+++ UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedDateTimeOffsetConverter : ValueConverter<DateTimeOffset, string>
+    {
+        public EncryptedDateTimeOffsetConverter(IEncryptionProvider encryptionProvider)
+            : base(v => encryptionProvider.Encrypt(v.ToString("o")),
+               v => DateTimeOffset.Parse(encryptionProvider.Decrypt(v)))
+        {
+        }
+    }
+}
