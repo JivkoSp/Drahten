2a57dfb6d7c3186822e5f353472e972aa7d7329a
JivkoSp <atjspasov@gmail.com>
2024-06-27 18:44:26 +0300

    Added EncryptedDateTimeOffsetConverter to the DateTime property of the BannedUser value object in UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BannedUserConfiguration.cs.

--- UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BannedUserConfiguration.cs
+++ UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/BannedUserConfiguration.cs
@@ -2,11 +2,20 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserService.Domain.ValueObjects;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
 
 namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
 {
     internal sealed class BannedUserConfiguration : IEntityTypeConfiguration<BannedUser>
     {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public BannedUserConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
         public void Configure(EntityTypeBuilder<BannedUser> builder)
         {
             //Table name
@@ -29,6 +38,7 @@ namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.WriteCon
                .IsRequired();
 
             builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
                .IsRequired();
         }
     }
