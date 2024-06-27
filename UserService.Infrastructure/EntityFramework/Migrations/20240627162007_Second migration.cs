928248b7a201f3a36e4139e1afdc98923fb72a3d
JivkoSp <atjspasov@gmail.com>
2024-06-27 19:22:00 +0300

    Added NEW EntityFramework migration in UserService.Infrastructure/EntityFramework/Migrations/. The migration is 20240627162007_Second migration.cs.

---
+++ UserService.Infrastructure/EntityFramework/Migrations/20240627162007_Second migration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Secondmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "UserTracking",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "ContactRequest",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "BannedUser",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "UserTracking",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "ContactRequest",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                schema: "user-service",
+                table: "BannedUser",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
