1a8fa615fab658a43a09a021a5071ce277eb4fe8
JivkoSp <atjspasov@gmail.com>
2024-06-27 13:27:25 +0300

    Added NEW custom EntityFramework converter: TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedArticleCommentDateTimeConverter.cs.

---
+++ TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedArticleCommentDateTimeConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TopicArticleService.Domain.ValueObjects;
+using TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedArticleCommentDateTimeConverter : ValueConverter<ArticleCommentDateTime, string>
+    {
+        public EncryptedArticleCommentDateTimeConverter(IEncryptionProvider encryptionProvider, ConverterMappingHints mappingHints = null)
+             : base(
+                  v => ConvertToDateTimeOffset(v, encryptionProvider),
+                  v => ConvertToArticleCommentDateTime(v, encryptionProvider),
+                  mappingHints)
+        {
+        }
+
+        private static string ConvertToDateTimeOffset(DateTimeOffset dateTime, IEncryptionProvider encryptionProvider)
+        {
+            // Encrypt the DateTime
+            string encryptedValue = encryptionProvider.Encrypt(dateTime.ToString("o"));
+
+            return encryptedValue;
+        }
+
+        private static ArticleCommentDateTime ConvertToArticleCommentDateTime(string value, IEncryptionProvider encryptionProvider)
+        {
+            // Decrypt the string
+            string decryptedValue = encryptionProvider.Decrypt(value);
+
+            // Convert string to ArticleComment
+            return new ArticleCommentDateTime(DateTimeOffset.Parse(decryptedValue));
+        }
+    }
+}
