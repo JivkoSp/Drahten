692926cf97363c2c88e2b0672b11746fe58571b2
JivkoSp <atjspasov@gmail.com>
2024-06-27 17:56:58 +0300

    Added EncryptedStringConverter to the UserFullName and UserEmailAddress properties of the UserReadModel in UserService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs.

--- UserService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs
+++ UserService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
 using UserService.Infrastructure.EntityFramework.Models;
 
 namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
 {
     internal sealed class UserConfiguration : IEntityTypeConfiguration<UserReadModel>
     {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public UserConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
         public void Configure(EntityTypeBuilder<UserReadModel> builder)
         {
             //Table name
@@ -16,12 +25,14 @@ namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.ReadConf
 
             //Property config 
             builder.Property(p => p.UserFullName)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
                 .IsRequired();
 
             builder.Property(p => p.UserNickName)
                 .IsRequired();
 
             builder.Property(p => p.UserEmailAddress)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
                 .IsRequired();
         }
     }
