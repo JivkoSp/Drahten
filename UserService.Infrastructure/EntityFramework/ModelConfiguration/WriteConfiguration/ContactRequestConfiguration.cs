7011ae3ac8135eb4c7c44bf2b8f4da0ebbd6a49c
JivkoSp <atjspasov@gmail.com>
2024-06-27 18:46:14 +0300

    Added EncryptedStringConverter to the Message property and EncryptedDateTimeOffsetConverter to the DateTime property of the ContactRequest value object in UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ContactRequestConfiguration.cs.

--- UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ContactRequestConfiguration.cs
+++ UserService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ContactRequestConfiguration.cs
@@ -2,11 +2,20 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserService.Domain.ValueObjects;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
 
 namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
 {
     internal sealed class ContactRequestConfiguration : IEntityTypeConfiguration<ContactRequest>
     {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ContactRequestConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
         public void Configure(EntityTypeBuilder<ContactRequest> builder)
         {
             //Table name
@@ -28,9 +37,11 @@ namespace UserService.Infrastructure.EntityFramework.ModelConfiguration.WriteCon
                .HasColumnName("ReceiverUserId")
                .IsRequired();
 
-            builder.Property(typeof(string), "Message");
+            builder.Property(typeof(string), "Message")
+               .HasConversion(new EncryptedStringConverter(_encryptionProvider));
 
             builder.Property(typeof(DateTimeOffset), "DateTime")
+               .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
                .IsRequired();
         }
     }
