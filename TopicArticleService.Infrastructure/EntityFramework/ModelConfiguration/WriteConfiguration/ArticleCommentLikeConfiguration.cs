472a2b676a5d73d8ebc9e4c7be0c737721a3fbe3
JivkoSp <atjspasov@gmail.com>
2024-06-27 13:53:21 +0300

    Added EncryptedDateTimeOffsetConverter to the DateTime of the ArticleCommentLike value object in TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleCommentLikeConfiguration.cs.

--- TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleCommentLikeConfiguration.cs
+++ TopicArticleService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ArticleCommentLikeConfiguration.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System.Reflection.Emit;
 using TopicArticleService.Domain.ValueObjects;
+using TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
 
 namespace TopicArticleService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
 {
     internal sealed class ArticleCommentLikeConfiguration : IEntityTypeConfiguration<ArticleCommentLike>
     {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ArticleCommentLikeConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
         public void Configure(EntityTypeBuilder<ArticleCommentLike> builder)
         {
             //Table name
@@ -16,23 +24,20 @@ namespace TopicArticleService.Infrastructure.EntityFramework.ModelConfiguration.
             //Composite primary key
             builder.HasKey(key => new { key.ArticleCommentID, key.UserID });
 
-            var userIdConverter = new ValueConverter<UserID, string>(x => x.Value.ToString(), x => new UserID(Guid.Parse(x)));
-
-            var articleCommentIdConverter = new ValueConverter<ArticleCommentID, Guid>(x => x.Value, x => new ArticleCommentID(x));
-
             //Property config - Start
 
             builder.Property(p => p.ArticleCommentID)
-                .HasConversion(articleCommentIdConverter)
+                .HasConversion(new ValueConverter<ArticleCommentID, Guid>(x => x.Value, x => new ArticleCommentID(x)))
                 .HasColumnName("ArticleCommentId")
                 .IsRequired();
 
             builder.Property(p => p.UserID)
-                .HasConversion(userIdConverter)
+                .HasConversion(new ValueConverter<UserID, string>(x => x.Value.ToString(), x => new UserID(Guid.Parse(x))))
                 .HasColumnName("UserId")
                 .IsRequired();
 
             builder.Property(p => p.DateTime)
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
                 .IsRequired();
 
             //Property config - End
