9d03585567d1df9e5bb81a588a815bd57f8be805
JivkoSp <atjspasov@gmail.com>
2024-06-27 18:42:31 +0300

    Added NEW custom EntityFramework converter: UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedUserNickNameConverter.cs.

---
+++ UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedUserNickNameConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using UserService.Domain.ValueObjects;
+using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    internal sealed class EncryptedUserNickNameConverter : ValueConverter<UserNickName, string>
+    {
+        public EncryptedUserNickNameConverter(IEncryptionProvider encryptionProvider, ConverterMappingHints mappingHints = null)
+            : base(
+                  v => ConvertToString(v, encryptionProvider),
+                  v => ConvertToUserNickName(v, encryptionProvider),
+                  mappingHints)
+        {
+        }
+
+        private static string ConvertToString(string userNickName, IEncryptionProvider encryptionProvider)
+        {
+            // Encrypt the string
+            string encryptedValue = encryptionProvider.Encrypt(userNickName);
+
+            return encryptedValue;
+        }
+
+        private static UserNickName ConvertToUserNickName(string value, IEncryptionProvider encryptionProvider)
+        {
+            // Decrypt the string
+            string decryptedValue = encryptionProvider.Decrypt(value);
+
+            // Convert string to UserNickName
+            return new UserNickName(decryptedValue);
+        }
+    }
+}
