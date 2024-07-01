951f8e8bd703ed24654bfe65287acbd96ea66223
JivkoSp <atjspasov@gmail.com>
2024-07-01 17:07:27 +0300

    Added new view model type: DrahtenWeb/ViewModels/SemanticSearchViewModel.cs

---
+++ DrahtenWeb/ViewModels/SemanticSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrahtenWeb.Dtos.TopicArticleService;
+
+namespace DrahtenWeb.ViewModels
+{
+    public class SemanticSearchViewModel
+    {
+        public List<ArticleDto> Articles { get; set; } = new List<ArticleDto>();
+        public Dictionary<string, List<ReadArticleCommentDto>> ArticleComments { get; set; } = new Dictionary<string, List<ReadArticleCommentDto>>();
+        public Dictionary<string, List<ReadUserArticleDto>> UsersRelatedToArticle { get; set; } = new Dictionary<string, List<ReadUserArticleDto>>();
+    }
+}
