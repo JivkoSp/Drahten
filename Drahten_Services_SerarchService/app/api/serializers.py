612c95e3f2076aae2703f549a02df41019c1873c
JivkoSp <atjspasov@gmail.com>
2024-07-01 17:36:00 +0300

    Added DocumentSerializer and NLPQueryAnswerSerializer in Drahten_Services_SerarchService/app/api/serializers.py

--- Drahten_Services_SerarchService/app/api/serializers.py
+++ Drahten_Services_SerarchService/app/api/serializers.py
@@ -1,16 +1,73 @@
 from rest_framework import serializers
 from app import models, dtos
 
-class NLPQueryAnswerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.NLPQueryAnswer
-        fields = ['document_id',
-                  'document',
-                  'answer', 
-                  'answer_type',
-                  'score',
-                  'context',
-                  'offsets_in_document']
+class DocumentSerializer(serializers.Serializer):
+    ArticlePrevTitle = serializers.CharField(allow_null=False, allow_blank=False)
+    ArticleTitle = serializers.CharField(allow_null=False, allow_blank=False)
+    ArticleData = serializers.CharField(allow_null=False, allow_blank=False)
+    ArticlePublishedDate = serializers.CharField(allow_null=True, allow_blank=True)
+    ArticleAuthor = serializers.CharField(allow_null=True, allow_blank=True)
+    ArticleLink = serializers.CharField(allow_null=True, allow_blank=True)
+
+    def to_representation(self, instance):
+        return {
+            'ArticlePrevTitle': instance.ArticlePrevTitle,
+            'ArticleTitle': instance.ArticleTitle,
+            'ArticleData': instance.ArticleData,
+            'ArticlePublishedDate': instance.ArticlePublishedDate,
+            'ArticleAuthor': instance.ArticleAuthor,
+            'ArticleLink': instance.ArticleLink,
+        }
+
+    def to_internal_value(self, data):
+        return dtos.DocumentDto(
+            articlePrevTitle=data.get('ArticlePrevTitle'),
+            articleTitle=data.get('ArticleTitle'),
+            articleData=data.get('ArticleData'),
+            articlePublishedDate=data.get('ArticlePublishedDate'),
+            articleAuthor=data.get('ArticleAuthor'),
+            articleLink=data.get('ArticleLink')
+        )
+        
+
+class NLPQueryAnswerSerializer(serializers.Serializer):
+    DocumentId = serializers.CharField(max_length=1000)
+    Answer = serializers.CharField(max_length=100000)
+    AnswerType = serializers.CharField(max_length=100)
+    Score = serializers.FloatField(min_value=0, max_value=1)
+    Context = serializers.CharField(max_length=1000000)
+    Document = DocumentSerializer()
+    
+    def create(self, validated_data):
+        document_data = validated_data.pop('Document')
+        document_instance = dtos.DocumentDto(**document_data)
+        return models.NLPQueryAnswer({
+            'document_ids': [validated_data['DocumentId']],
+            'answer': validated_data['Answer'],
+            'type': validated_data['AnswerType'],
+            'score': validated_data['Score'],
+            'context': validated_data['Context'],
+            'meta': document_instance.to_dict(),
+            'offsets_in_document': [],
+            'offsets_in_context': [],
+        })
+
+    def update(self, instance, validated_data):
+        instance.DocumentId = validated_data.get('DocumentId', instance.DocumentId)
+        instance.Answer = validated_data.get('Answer', instance.Answer)
+        instance.AnswerType = validated_data.get('AnswerType', instance.AnswerType)
+        instance.Score = validated_data.get('Score', instance.Score)
+        instance.Context = validated_data.get('Context', instance.Context)
+        
+        document_data = validated_data.get('Document')
+        instance.Document.ArticlePrevTitle = document_data.get('ArticlePrevTitle', instance.Document.ArticlePrevTitle)
+        instance.Document.ArticleTitle = document_data.get('ArticleTitle', instance.Document.ArticleTitle)
+        instance.Document.ArticleData = document_data.get('ArticleData', instance.Document.ArticleData)
+        instance.Document.ArticlePublishedDate = document_data.get('ArticlePublishedDate', instance.Document.ArticlePublishedDate)
+        instance.Document.ArticleAuthor = document_data.get('ArticleAuthor', instance.Document.ArticleAuthor)
+        instance.Document.ArticleLink = document_data.get('ArticleLink', instance.Document.ArticleLink)
+
+        return instance
         
 
 
