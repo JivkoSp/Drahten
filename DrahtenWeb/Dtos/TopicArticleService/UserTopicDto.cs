7e8eddbf5d3ccff331c346fa8b8f4a405ec964d2
JivkoSp <atjspasov@gmail.com>
2024-07-01 17:27:42 +0300

    Added TopicFullName property of type string in DrahtenWeb/Dtos/TopicArticleService/UserTopicDto.cs.

--- DrahtenWeb/Dtos/TopicArticleService/UserTopicDto.cs
+++ DrahtenWeb/Dtos/TopicArticleService/UserTopicDto.cs
@@ -5,6 +5,7 @@
         public string UserId { get; set; }
         public Guid TopicId { get; set; }
         public string TopicName { get; set; }
+        public string TopicFullName { get; set; }
         public DateTimeOffset SubscriptionTime { get; set; }
     }
 }
