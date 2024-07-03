1bbe052c78c767b9ce0a52701e11a1a206524396
JivkoSp <atjspasov@gmail.com>
2024-07-03 18:18:44 +0300

    Added TopicFullName property of type string in TopicArticleService.Application/Dtos/TopicDto.cs

--- TopicArticleService.Application/Dtos/TopicDto.cs
+++ TopicArticleService.Application/Dtos/TopicDto.cs
@@ -5,6 +5,7 @@ namespace TopicArticleService.Application.Dtos
     {
         public Guid TopicId { get; set; }
         public string TopicName { get; set; }
+        public string TopicFullName { get; set; }
         public Guid? ParentTopicId { get; set; }
         public List<TopicDto> Children { get; set; }
     }
