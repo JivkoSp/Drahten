e1b30c91d9155fe3244c00fa25f8cbbbe7c14a34
JivkoSp <atjspasov@gmail.com>
2024-07-05 18:39:48 +0300

    Added new custom domain exception in PublicHistoryService.Domain/Exceptions/. The exceptions are CommentedArticleAlreadyExistsException.cs, CommentedArticleNotFoundException.cs, SearchedArticleDataNotFoundException.cs, SearchedTopicDataAlreadyExistsException.cs and SearchedTopicDataNotFoundException.cs.

---
+++ PublicHistoryService.Domain/Exceptions/CommentedArticleAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class CommentedArticleAlreadyExistsException : DomainException
+    {
+        internal CommentedArticleAlreadyExistsException(Guid userId, DateTimeOffset dateTime)
+            : base(message: $"There is already the same article comment made by user #{userId} on {dateTime}!")
+        {
+        }
+    }
+}
