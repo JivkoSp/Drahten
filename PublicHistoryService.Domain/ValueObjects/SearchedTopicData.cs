2c047b44c7c4a369267327f457881f86f62bd67c
JivkoSp <atjspasov@gmail.com>
2024-07-05 15:05:32 +0300

    Added new domain value objects in PublicHistoryService.Domain/ValueObjects/. The value objects are SearchedTopicData.cs and TopicID.cs

---
+++ PublicHistoryService.Domain/ValueObjects/SearchedTopicData.cs
@@ -0,0 +1,42 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record SearchedTopicData
+    {
+        public TopicID TopicID { get; }
+        public UserID UserID { get; }
+        internal SearchedData SearchedData { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        private SearchedTopicData() { }
+
+        public SearchedTopicData(TopicID topicId, UserID userId, SearchedData searchedData, DateTimeOffset dateTime)
+        {
+            if (topicId == null)
+            {
+                throw new NullTopicIdException();
+            }
+
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (searchedData == null)
+            {
+                throw new NullSearchedDataException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidSearchedTopicDataDateTimeException();
+            }
+
+            TopicID = topicId;
+            UserID = userId;
+            SearchedData = searchedData;
+            DateTime = dateTime;
+        }
+    }
+}
