be074b52469d8574149c3bf582e3bfb4e0beb526
JivkoSp <atjspasov@gmail.com>
2024-07-05 16:29:46 +0300

    Added new domain event types in PublicHistoryService.Domain/Events/. The events are: SearchedArticleDataAdded.cs, ViewedArticleAdded.cs, ViewedArticleRemoved.cs, ViewedUserAdded.cs and ViewedUserRemoved.cs

---
+++ PublicHistoryService.Domain/Events/SearchedArticleDataAdded.cs
@@ -0,0 +1,7 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Domain.Events
+{
+    public record SearchedArticleDataAdded(User User, SearchedArticleData SearchedArticleData) : IDomainEvent;
+}
