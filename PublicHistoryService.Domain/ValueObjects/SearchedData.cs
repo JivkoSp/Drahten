927989f690a5339486e3a4c1feb216db821d0f50
JivkoSp <atjspasov@gmail.com>
2024-07-05 14:53:23 +0300

    Added new domain value object: PublicHistoryService.Domain/ValueObjects/SearchedData.cs

---
+++ PublicHistoryService.Domain/ValueObjects/SearchedData.cs
@@ -0,0 +1,27 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record SearchedData
+    {
+        internal string Value { get; }
+
+        public SearchedData(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptySearchedDataException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to String.
+        public static implicit operator string(SearchedData searchedData)
+            => searchedData.Value;
+
+        //Conversion from String to ValueObject.
+        public static implicit operator SearchedData(string searchedData)
+            => new SearchedData(searchedData);
+    }
+}
