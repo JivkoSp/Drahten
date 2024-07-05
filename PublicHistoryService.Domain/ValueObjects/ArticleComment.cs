c3e3ec277116535e82ab1943e982c57346b056d2
JivkoSp <atjspasov@gmail.com>
2024-07-05 15:32:33 +0300

    Added new domain value objects in PublicHistoryService.Domain/ValueObjects/. The value objects are ArticleComment.cs and CommentedArticle.cs.

---
+++ PublicHistoryService.Domain/ValueObjects/ArticleComment.cs
@@ -0,0 +1,27 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record ArticleComment
+    {
+        internal string Value { get; }
+
+        public ArticleComment(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new EmptyArticleCommentException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to String.
+        public static implicit operator string(ArticleComment comment)
+            => comment.Value;
+
+        //Conversion from String to ValueObject.
+        public static implicit operator ArticleComment(string comment)
+            => new ArticleComment(comment);
+    }
+}
---
+++ PublicHistoryService.Domain/ValueObjects/CommentedArticle.cs
@@ -0,0 +1,42 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record CommentedArticle
+    {
+        public ArticleID ArticleID { get; }
+        public UserID UserID { get; }
+        internal ArticleComment ArticleComment { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        private CommentedArticle() { }
+
+        public CommentedArticle(ArticleID articleId, UserID userId, ArticleComment articleComment, DateTimeOffset dateTime)
+        {
+            if (articleId == null)
+            {
+                throw new NullArticleIdException();
+            }
+
+            if (userId == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (articleComment == null)
+            {
+                throw new NullArticleCommentException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidCommentedArticleDateTimeException();
+            }
+
+            ArticleID = articleId;
+            UserID = userId;
+            ArticleComment = articleComment;
+            DateTime = dateTime;
+        }
+    }
+}
