2bfdd70b479d29996d9d320ac4ffff78483c2155
JivkoSp <atjspasov@gmail.com>
2024-07-05 16:28:22 +0300

    Added new custom domain exceptions in PublicHistoryService.Domain/Exceptions/. The exceptions are: SearchedArticleDataAlreadyExistsException.cs, ViewedArticleAlreadyExistsException.cs, ViewedArticleNotFoundException.cs, ViewedUserAlreadyExistsException.cs and ViewedUserNotFoundException.cs.

---
+++ PublicHistoryService.Domain/Exceptions/ViewedUserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class ViewedUserNotFoundException : DomainException
+    {
+        internal ViewedUserNotFoundException(Guid viewedUserID, Guid viewerUserID, DateTimeOffset dateTime)
+            : base(message: $"There is no viewed user #{viewedUserID} by user #{viewerUserID} on {dateTime}!")
+        {
+        }
+    }
+}
