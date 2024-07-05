583289ef8ae5c26403d73ba88353d8f875df026a
JivkoSp <atjspasov@gmail.com>
2024-07-05 20:01:07 +0300

    Added implementation of the IUserFactory interface: PublicHistoryService.Domain/Factories/UserFactory.cs.

---
+++ PublicHistoryService.Domain/Factories/UserFactory.cs
@@ -0,0 +1,12 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.Factories.Interfaces;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Domain.Factories
+{
+    public sealed class UserFactory : IUserFactory
+    {
+        public User Create(UserID userId)
+            => new User(userId);
+    }
+}
