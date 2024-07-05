2f03a94d4cd347e5175cdd431439d699ac4e8100
JivkoSp <atjspasov@gmail.com>
2024-07-05 15:33:45 +0300

    Added new custom domain exceptions in PublicHistoryService.Domain/Exceptions/. The exceptions are EmptyArticleCommentException.cs, InvalidCommentedArticleDateTimeException.cs and NullArticleCommentException.cs

---
+++ PublicHistoryService.Domain/Exceptions/EmptyArticleCommentException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class EmptyArticleCommentException : DomainException
+    {
+        internal EmptyArticleCommentException() : base(message: "Article comment cannot be empty!")
+        {
+        }
+    }
+}
---
+++ PublicHistoryService.Domain/Exceptions/NullArticleCommentException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class NullArticleCommentException : DomainException
+    {
+        internal NullArticleCommentException() : base(message: "Article comment cannot be null!")
+        {
+        }
+    }
+}
