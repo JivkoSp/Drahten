374dd8b6d7f13a06ef349a8c917850f6b618e78a
JivkoSp <atjspasov@gmail.com>
2024-07-05 14:31:10 +0300

    Added new domain value object: PublicHistoryService.Domain/ValueObjects/ViewedUser.cs

---
+++ PublicHistoryService.Domain/ValueObjects/ViewedUser.cs
@@ -0,0 +1,35 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record ViewedUser
+    {
+        public UserID ViewerUserId { get; }
+        public UserID ViewedUserId { get; }
+        internal DateTimeOffset DateTime { get; }
+
+        private ViewedUser() { }
+
+        public ViewedUser(UserID viewerUserID, UserID viewedUserID, DateTimeOffset dateTime)
+        {
+            if (viewerUserID == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (viewedUserID == null)
+            {
+                throw new NullUserIdException();
+            }
+
+            if (dateTime == default || dateTime > DateTimeOffset.Now)
+            {
+                throw new InvalidViewedUserDateTimeException();
+            }
+
+            ViewerUserId = viewerUserID;
+            ViewedUserId = viewedUserID;
+            DateTime = dateTime;
+        }
+    }
+}
