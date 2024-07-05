920ad56c31c4988a0b34a5ddcbb2a81e989693b0
JivkoSp <atjspasov@gmail.com>
2024-07-05 19:58:52 +0300

    Added interface for User factory: PublicHistoryService.Domain/Factories/Interfaces/IUserFactory.cs (For creating User entities).

---
+++ PublicHistoryService.Domain/Factories/Interfaces/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Domain.Factories.Interfaces
+{
+    public interface IUserFactory
+    {
+        User Create(UserID userId);
+    }
+}
