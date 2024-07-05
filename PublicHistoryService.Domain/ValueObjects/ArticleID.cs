7991979efa06f042f592c18a06708932d6c604aa
JivkoSp <atjspasov@gmail.com>
2024-07-05 14:21:11 +0300

    Added new value domain value objects: PublicHistoryService.Domain/ValueObjects/ArticleID.cs, PublicHistoryService.Domain/ValueObjects/UserID.cs, PublicHistoryService.Domain/ValueObjects/ViewedArticle.cs

---
+++ PublicHistoryService.Domain/ValueObjects/ArticleID.cs
@@ -0,0 +1,27 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record ArticleID
+    {
+        public Guid Value { get; }
+
+        public ArticleID(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyArticleIdException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to Guid.
+        public static implicit operator Guid(ArticleID articleId)
+            => articleId.Value;
+
+        //Conversion from Guid to ValueObject.
+        public static implicit operator ArticleID(Guid articleId)
+            => new ArticleID(articleId);
+    }
+}
