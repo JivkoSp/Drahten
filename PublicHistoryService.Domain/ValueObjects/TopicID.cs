422159b4d42256211ed5b3aa0961c6d48de8a652
JivkoSp <atjspasov@gmail.com>
2024-07-05 15:05:32 +0300

    Added new domain value objects in PublicHistoryService.Domain/ValueObjects/. The value objects are SearchedTopicData.cs and TopicID.cs

---
+++ PublicHistoryService.Domain/ValueObjects/TopicID.cs
@@ -0,0 +1,27 @@
+﻿using PublicHistoryService.Domain.Exceptions;
+
+namespace PublicHistoryService.Domain.ValueObjects
+{
+    public record TopicID
+    {
+        public Guid Value { get; }
+
+        public TopicID(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyTopicIdException();
+            }
+
+            Value = value;
+        }
+
+        //Conversion from ValueObject to Guid.
+        public static implicit operator Guid(TopicID topicId)
+            => topicId.Value;
+
+        //Conversion from Guid to ValueObject.
+        public static implicit operator TopicID(Guid topicId)
+            => new TopicID(topicId);
+    }
+}
