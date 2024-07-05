937ed73a228683a18ab4c91ffa264a2dbe8d5b4a
JivkoSp <atjspasov@gmail.com>
2024-07-05 20:04:29 +0300

    Added interface that represents the possible operations with the User domain entity: PublicHistoryService.Domain/Repositories/IUserRepository.cs in the domain layer.

---
+++ PublicHistoryService.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(UserID userId);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+    }
+}
