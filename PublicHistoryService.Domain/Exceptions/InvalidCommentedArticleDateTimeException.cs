363c8d99dea4a4d9b57e67c30bb72cec05f08831
JivkoSp <atjspasov@gmail.com>
2024-07-05 15:33:45 +0300

    Added new custom domain exceptions in PublicHistoryService.Domain/Exceptions/. The exceptions are EmptyArticleCommentException.cs, InvalidCommentedArticleDateTimeException.cs and NullArticleCommentException.cs

---
+++ PublicHistoryService.Domain/Exceptions/InvalidCommentedArticleDateTimeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class InvalidCommentedArticleDateTimeException : DomainException
+    {
+        internal InvalidCommentedArticleDateTimeException() : base(message: "Invalid datetime for commented article!")
+        {
+        }
+    }
+}
