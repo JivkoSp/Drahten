3d211859ee7721eb9e2a36437e916c652b3c3c44
JivkoSp <atjspasov@gmail.com>
2024-07-05 14:30:24 +0300

    Added new custom domain exception: PublicHistoryService.Domain/Exceptions/InvalidViewedUserDateTimeException.cs

---
+++ PublicHistoryService.Domain/Exceptions/InvalidViewedUserDateTimeException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PublicHistoryService.Domain.Exceptions
+{
+    public sealed class InvalidViewedUserDateTimeException : DomainException
+    {
+        internal InvalidViewedUserDateTimeException() : base(message: "Invalid datetime for viewed user!")
+        {
+        }
+    }
+}
