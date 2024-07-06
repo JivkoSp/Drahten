9b5b1cbf8b6dd8b60eeb1b9ecd8106252c8d21b8
JivkoSp <atjspasov@gmail.com>
2024-07-06 13:23:23 +0300

    Added unit tests for the AddCommentedArticle method that is defined in the User domain entity. The unit tests are added in PublicHistoryService.Tests.Unit/Domain/Entities/UserTests/AddCommentedArticle.cs.

---
+++ PublicHistoryService.Tests.Unit/Domain/Entities/UserTests/AddCommentedArticle.cs
@@ -0,0 +1,95 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.Events;
+using PublicHistoryService.Domain.Exceptions;
+using PublicHistoryService.Domain.Factories;
+using PublicHistoryService.Domain.Factories.Interfaces;
+using PublicHistoryService.Domain.ValueObjects;
+using Shouldly;
+using Xunit;
+
+namespace PublicHistoryService.Tests.Unit.Domain.Entities.UserTests
+{
+    public sealed class AddCommentedArticle
+    {
+        #region GLOBAL ARRANGE
+
+        private readonly IUserFactory _userFactory;
+
+        private User GetUser()
+        {
+            var user = _userFactory.Create(Guid.NewGuid());
+
+            return user;
+        }
+
+        private CommentedArticle GetCommentedArticle()
+        {
+            var articleComment = new ArticleComment("...");
+
+            var commentedArticle = new CommentedArticle(articleId: Guid.NewGuid(), userId: Guid.NewGuid(),
+                articleComment: articleComment, dateTime: DateTimeOffset.Now);
+
+            return commentedArticle;
+        }
+
+        public AddCommentedArticle()
+        {
+            _userFactory = new UserFactory();
+        }
+
+        #endregion
+
+        //Should throw CommentedArticleAlreadyExistsException when the following condition is met:
+        //Another CommentedArticle value object with the same values already exists.
+        [Fact]
+        public void Duplicate_CommentedArticle_Throws_CommentedArticleAlreadyExistsException()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var commentedArticle = GetCommentedArticle();
+
+            user.AddCommentedArticle(commentedArticle);
+
+            //ACT
+            var exception = Record.Exception(() => user.AddCommentedArticle(commentedArticle));
+
+            //ASSERT
+            exception.ShouldNotBeNull();
+
+            exception.ShouldBeOfType<CommentedArticleAlreadyExistsException>();
+        }
+
+        //Should add CommentedArticle value object to internal collection of CommentedArticle value objects
+        //and produce CommentedArticleAdded domain event.
+        //The CommentedArticleAdded domain event should contain:
+        //1. The same user entity to which the CommentedArticle value object was added.
+        //2. The same CommentedArticle value object that was added to the internal collection of CommentedArticle value objects.
+        [Fact]
+        public void Adds_CommentedArticle_And_Produces_CommentedArticleAdded_Domain_Event_On_Success()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var commentedArticle = GetCommentedArticle();
+
+            //ACT
+            var exception = Record.Exception(() => user.AddCommentedArticle(commentedArticle));
+
+            //ASSERT
+            exception.ShouldBeNull();
+
+            user.DomainEvents.Count().ShouldBe(1);
+
+            user.CommentedArticles.Count().ShouldBe(1);
+
+            var commentedArticleAddedEvent = user.DomainEvents.FirstOrDefault() as CommentedArticleAdded;
+
+            commentedArticleAddedEvent.ShouldNotBeNull();
+
+            commentedArticleAddedEvent.User.ShouldBeSameAs(user);
+
+            commentedArticleAddedEvent.CommentedArticle.ShouldBeSameAs(commentedArticle);
+        }
+    }
+}
