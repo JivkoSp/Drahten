336edf929519904fe13544074f6da10aad63829b
JivkoSp <atjspasov@gmail.com>
2024-07-06 10:37:42 +0300

    Added unit tests for the RemoveSearchedArticleData method that is defined in the User domain entity. The unit tests are added in PublicHistoryService.Tests.Unit/Domain/Entities/UserTests/RemoveSearchedArticleData.cs.

---
+++ PublicHistoryService.Tests.Unit/Domain/Entities/UserTests/RemoveSearchedArticleData.cs
@@ -0,0 +1,103 @@
+﻿using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.Events;
+using PublicHistoryService.Domain.Exceptions;
+using PublicHistoryService.Domain.Factories;
+using PublicHistoryService.Domain.Factories.Interfaces;
+using PublicHistoryService.Domain.ValueObjects;
+using Shouldly;
+using Xunit;
+
+namespace PublicHistoryService.Tests.Unit.Domain.Entities.UserTests
+{
+    public sealed class RemoveSearchedArticleData
+    {
+        #region GLOBAL ARRANGE
+
+        private readonly IUserFactory _userFactory;
+
+        private User GetUser()
+        {
+            var user = _userFactory.Create(Guid.NewGuid());
+
+            return user;
+        }
+
+        private SearchedArticleData GetSearchedArticleData()
+        {
+            var searchedArticleData = new SearchedArticleData(articleId: Guid.NewGuid(), userId: Guid.NewGuid(),
+                searchedData: "...", dateTime: DateTimeOffset.Now);
+
+            return searchedArticleData;
+        }
+
+        public RemoveSearchedArticleData()
+        {
+            _userFactory = new UserFactory();
+        }
+
+        #endregion
+
+        //Should throw SearchedArticleDataNotFoundException when the following condition is met:
+        //There is no SearchedArticleData value object with the same values.
+        [Fact]
+        public void SearchedArticleData_NotFound_Throws_SearchedArticleDataNotFoundException()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var searchedArticleData = GetSearchedArticleData();
+
+            //ACT
+            var exception = Record.Exception(() => user.RemoveSearchedArticleData(searchedArticleData));
+
+            //ASSERT
+            exception.ShouldNotBeNull();
+
+            exception.ShouldBeOfType<SearchedArticleDataNotFoundException>();
+        }
+
+        //Should remove SearchedArticleData value object from internal collection of SearchedArticleData value objects
+        //and produce SearchedArticleDataRemoved domain event.
+        //The SearchedArticleDataRemoved domain event should contain:
+        //1. The same user entity that the SearchedArticleData value object was removed from.
+        //2. The same SearchedArticleData value object that was removed from the internal collection of SearchedArticleData value objects.
+        [Fact]
+        public void Removes_SearchedArticleData_And_Produces_SearchedArticleDataRemoved_Domain_Event_On_Success()
+        {
+            //ARRANGE
+            var user = GetUser();
+
+            var searchedArticleData = GetSearchedArticleData();
+
+            user.AddSearchedArticleData(searchedArticleData);
+
+            //ACT
+            var exception = Record.Exception(() => user.RemoveSearchedArticleData(searchedArticleData));
+
+            //ASSERT
+            exception.ShouldBeNull();
+
+            user.DomainEvents.Count().ShouldBe(2);
+
+            user.SearchedArticleInformation.Count().ShouldBe(0);
+
+            var searchedArticleDataAddedEvent = user.DomainEvents.FirstOrDefault(x =>
+                 x.GetType() == typeof(SearchedArticleDataAdded)) as SearchedArticleDataAdded;
+
+            var searchedArticleDataRemovedEvent = user.DomainEvents.FirstOrDefault(x =>
+                x.GetType() == typeof(SearchedArticleDataRemoved)) as SearchedArticleDataRemoved;
+
+            searchedArticleDataAddedEvent.ShouldNotBeNull();
+
+            searchedArticleDataAddedEvent.User.ShouldBeSameAs(user);
+
+            searchedArticleDataAddedEvent.SearchedArticleData.ShouldBeSameAs(searchedArticleData);
+
+            searchedArticleDataRemovedEvent.ShouldNotBeNull();
+
+            searchedArticleDataRemovedEvent.User.ShouldBeSameAs(user);
+
+            searchedArticleDataRemovedEvent.SearchedArticleData.ShouldBeSameAs(searchedArticleData);
+        }
+    }
+}
