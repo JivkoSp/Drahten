02ecb3c72f4d37d27e648ecc2c13d4d7bb6ba482
JivkoSp <atjspasov@gmail.com>
2024-07-06 19:22:05 +0300

    Added custom application layer root (base) exception type: PublicHistoryService.Application/Exceptions/ApplicationException.cs.

---
+++ PublicHistoryService.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PublicHistoryService.Application.Exceptions
+{
+    public abstract class ApplicationException : Exception
+    {
+        public virtual string ErrorCode { get; set; }
+
+        public ApplicationException(string message) : base(message) { }
+    }
+}
