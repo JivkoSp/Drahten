fa7cbbf2f8e4bcbb79924f645e8d27cef610f57e
JivkoSp <atjspasov@gmail.com>
2024-07-06 15:36:55 +0300

    Added command handler interface: PublicHistoryService.Application/Commands/Handlers/ICommandHandler.cs

---
+++ PublicHistoryService.Application/Commands/Handlers/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PublicHistoryService.Application.Commands.Handlers
+{
+    public interface ICommandHandler<in TCommand> where TCommand : class, ICommand
+    {
+        Task HandleAsync(TCommand command);
+    }
+}
