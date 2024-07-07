780c7f1d58f80b63ba711126c1765b9f36056fac
JivkoSp <atjspasov@gmail.com>
2024-07-07 16:06:24 +0300

    Added new application layer exception type: PublicHistoryService.Application/Exceptions/ViewedArticleNotFoundException.cs

---
+++ PublicHistoryService.Application/Exceptions/ViewedArticleNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PublicHistoryService.Application.Exceptions
+{
+    public sealed class ViewedArticleNotFoundException : ApplicationException
+    {
+        internal ViewedArticleNotFoundException(Guid viewedArticleId)
+            : base(message: $"Viewed article ${viewedArticleId} was NOT found!")
+        {
+        }
+    }
+}
