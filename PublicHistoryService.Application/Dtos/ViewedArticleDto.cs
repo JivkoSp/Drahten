0fbc74f9de47f24bf3edc8ca935fb4ac048f5fe1
JivkoSp <atjspasov@gmail.com>
2024-07-07 16:05:46 +0300

    Added new dto type for the ViewedArticle domain value object: PublicHistoryService.Application/Dtos/ViewedArticleDto.cs

---
+++ PublicHistoryService.Application/Dtos/ViewedArticleDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PublicHistoryService.Application.Dtos
+{
+    public class ViewedArticleDto
+    {
+        public Guid ViewedArticleId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
