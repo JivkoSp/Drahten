23aadf159735242ae60412f5c235c54b614bb2d1
JivkoSp <atjspasov@gmail.com>
2024-07-07 10:42:45 +0300

    Added new command handler for the AddSearchedArticleDataCommand command type. The command handler is: PublicHistoryService.Application/Commands/Handlers/AddSearchedArticleDataHandler.cs.

---
+++ PublicHistoryService.Application/Commands/Handlers/AddSearchedArticleDataHandler.cs
@@ -0,0 +1,33 @@
+﻿using PublicHistoryService.Application.Exceptions;
+using PublicHistoryService.Domain.Repositories;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddSearchedArticleDataHandler : ICommandHandler<AddSearchedArticleDataCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddSearchedArticleDataHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddSearchedArticleDataCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var searchedArticleData = new SearchedArticleData(command.ArticleId, command.UserId, command.SearchedData,
+                command.DateTime.ToUniversalTime());
+
+            user.AddSearchedArticleData(searchedArticleData);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
