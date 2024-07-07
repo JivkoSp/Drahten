d08ef455bcb5b901e38753af934e2b1a3f06e947
JivkoSp <atjspasov@gmail.com>
2024-07-07 14:46:48 +0300

    Added new dto type for SearchedArticleData: PublicHistoryService.Application/Dtos/SearchedArticleDataDto.cs.

---
+++ PublicHistoryService.Application/Dtos/SearchedArticleDataDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PublicHistoryService.Application.Dtos
+{
+    public class SearchedArticleDataDto
+    {
+        public Guid SearchedArticleDataId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
