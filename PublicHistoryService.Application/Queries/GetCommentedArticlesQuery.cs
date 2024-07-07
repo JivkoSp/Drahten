c5a41ba79301afb89f2fd868c5e329c48afcba67
JivkoSp <atjspasov@gmail.com>
2024-07-07 17:23:27 +0300

    Added query type: PublicHistoryService.Application/Queries/GetCommentedArticlesQuery.cs

---
+++ PublicHistoryService.Application/Queries/GetCommentedArticlesQuery.cs
@@ -0,0 +1,6 @@
+﻿using PublicHistoryService.Application.Dtos;
+
+namespace PublicHistoryService.Application.Queries
+{
+    public record GetCommentedArticlesQuery(Guid UserId) : IQuery<List<CommentedArticleDto>>;
+}
