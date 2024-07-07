f284b1fd28e002fa0286307738b28fc40c872510
JivkoSp <atjspasov@gmail.com>
2024-07-07 16:07:02 +0300

    Added new read service interface: PublicHistoryService.Application/Services/ReadServices/IViewedArticleReadService.cs

---
+++ PublicHistoryService.Application/Services/ReadServices/IViewedArticleReadService.cs
@@ -0,0 +1,9 @@
+﻿using PublicHistoryService.Application.Dtos;
+
+namespace PublicHistoryService.Application.Services.ReadServices
+{
+    public interface IViewedArticleReadService
+    {
+        Task<ViewedArticleDto> GetViewedArticleByIdAsync(Guid viewedArticleId);
+    }
+}
