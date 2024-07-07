e41d1da5ffa1c0a1eefc18ec8feb85ed3ff7d06c
JivkoSp <atjspasov@gmail.com>
2024-07-07 17:25:48 +0300

    Added new query type: PublicHistoryService.Application/Queries/GetSearchedArticlesDataQuery.cs.

---
+++ PublicHistoryService.Application/Queries/GetSearchedArticlesDataQuery.cs
@@ -0,0 +1,6 @@
+﻿using PublicHistoryService.Application.Dtos;
+
+namespace PublicHistoryService.Application.Queries
+{
+    public record GetSearchedArticlesDataQuery(Guid UserId) : IQuery<List<SearchedArticleDataDto>>;
+}
