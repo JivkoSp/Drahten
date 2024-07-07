923ba8ecd63e5b96522bff137f89f7c83e1eb44d
JivkoSp <atjspasov@gmail.com>
2024-07-07 11:13:27 +0300

    Added new command handler for the command type: AddViewedArticleCommand. The command handler is: PublicHistoryService.Application/Commands/Handlers/AddViewedArticleHandler.cs

---
+++ PublicHistoryService.Application/Commands/Handlers/AddViewedArticleHandler.cs
@@ -0,0 +1,32 @@
+﻿using PublicHistoryService.Application.Exceptions;
+using PublicHistoryService.Domain.Repositories;
+using PublicHistoryService.Domain.ValueObjects;
+
+namespace PublicHistoryService.Application.Commands.Handlers
+{
+    internal sealed class AddViewedArticleHandler : ICommandHandler<AddViewedArticleCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AddViewedArticleHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(AddViewedArticleCommand command)
+        {
+            var user = await _userRepository.GetUserByIdAsync(command.UserId);
+
+            if (user == null)
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var viewedArticle = new ViewedArticle(command.ArticleId, command.UserId, command.DateTime.ToUniversalTime());
+
+            user.AddViewedArticle(viewedArticle);
+
+            await _userRepository.UpdateUserAsync(user);
+        }
+    }
+}
