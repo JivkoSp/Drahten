66508c50f9551c106e5257919866eb03ad4e7008
JivkoSp <atjspasov@gmail.com>
2024-07-07 14:30:25 +0300

    Added dto type for CommentedArticle: PublicHistoryService.Application/Dtos/CommentedArticleDto.cs

---
+++ PublicHistoryService.Application/Dtos/CommentedArticleDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PublicHistoryService.Application.Dtos
+{
+    public class CommentedArticleDto
+    {
+        public Guid CommentedArticleId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public string ArticleComment { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
