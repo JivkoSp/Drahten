93849117b5fdcb89e1892b2b47624ad796ff1618
JivkoSp <atjspasov@gmail.com>
2024-07-07 14:32:10 +0300

    Added read service interface: PublicHistoryService.Application/Services/ReadServices/ICommentedArticleReadService.cs

---
+++ PublicHistoryService.Application/Services/ReadServices/ICommentedArticleReadService.cs
@@ -0,0 +1,9 @@
+﻿using PublicHistoryService.Application.Dtos;
+
+namespace PublicHistoryService.Application.Services.ReadServices
+{
+    public interface ICommentedArticleReadService
+    {
+        Task<CommentedArticleDto> GetCommentedArticleByIdAsync(Guid commentedArticleId);
+    }
+}
