023e343574baf2b7fa400b50fe77cf7e52845bf3
JivkoSp <atjspasov@gmail.com>
2024-07-07 17:32:36 +0300

    Added new query type: PublicHistoryService.Application/Queries/GeViewedUsersQuery.cs.

---
+++ PublicHistoryService.Application/Queries/GeViewedUsersQuery.cs
@@ -0,0 +1,6 @@
+﻿using PublicHistoryService.Application.Dtos;
+
+namespace PublicHistoryService.Application.Queries
+{
+    public record GeViewedUsersQuery(Guid ViewerUserId) : IQuery<List<ViewedUserDto>>;
+}
