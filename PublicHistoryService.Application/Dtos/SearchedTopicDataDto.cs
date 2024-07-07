398c59a94b278ef270cf3449faa1d62e111170b0
JivkoSp <atjspasov@gmail.com>
2024-07-07 15:20:28 +0300

    Added new custom application layer dto type: PublicHistoryService.Application/Dtos/SearchedTopicDataDto.cs.

---
+++ PublicHistoryService.Application/Dtos/SearchedTopicDataDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PublicHistoryService.Application.Dtos
+{
+    public class SearchedTopicDataDto
+    {
+        public Guid SearchedTopicDataId { get; set; }
+        public Guid TopicId { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
