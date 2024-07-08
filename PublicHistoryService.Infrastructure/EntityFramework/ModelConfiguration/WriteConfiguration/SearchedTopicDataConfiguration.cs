f183b20525277dbe2d4778bdfb7e13fdec8fe3f6
JivkoSp <atjspasov@gmail.com>
2024-07-08 19:43:19 +0300

    Added EntityFramework configurations for the SearchedTopicData value object in PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/SearchedTopicDataConfiguration.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/SearchedTopicDataConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class SearchedTopicDataConfiguration : IEntityTypeConfiguration<SearchedTopicData>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public SearchedTopicDataConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<SearchedTopicData> builder)
+        {
+            //Table name
+            builder.ToTable("SearchedTopicData");
+
+            //Shadow property for SearchedTopicDataId.
+            builder.Property<Guid>("SearchedTopicDataId");
+
+            //Primary key
+            builder.HasKey("SearchedTopicDataId");
+
+            //Property config - Start
+
+            builder.Property(p => p.TopicID)
+                .HasConversion(id => id.Value, id => new TopicID(id))
+                .HasColumnName("TopicId")
+                .IsRequired();
+
+            builder.Property(p => p.UserID)
+               .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+               .HasColumnName("UserId")
+               .IsRequired();
+
+            builder.Property(typeof(SearchedData), "SearchedData")
+                .HasConversion(new EncryptedSearchedDataConverter(_encryptionProvider))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+                .IsRequired();
+
+            //Property config - End
+        }
+    }
+}
