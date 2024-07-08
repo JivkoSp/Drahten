c5769e600efea194f38bce03f8be1e26f79fa793
JivkoSp <atjspasov@gmail.com>
2024-07-08 23:49:52 +0300

    Added custom IBatchFormatter json formatter: PublicHistoryService.Infrastructure/Logging/Formatters/SerilogJsonFormatter.cs.

---
+++ PublicHistoryService.Infrastructure/Logging/Formatters/SerilogJsonFormatter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using Serilog.Sinks.Http;
+
+namespace PublicHistoryService.Infrastructure.Logging.Formatters
+{
+    public sealed class SerilogJsonFormatter : IBatchFormatter
+    {
+        public void Format(IEnumerable<string> logEvents, TextWriter output)
+        {
+            foreach (var logEvent in logEvents)
+            {
+                // Parse each log event string into a JSON object.
+                var logEventObject = JObject.Parse(logEvent);
+
+                // Serialize the JSON object back to JSON with indentation for readability.
+                var serializedLogEvent = logEventObject.ToString(Newtonsoft.Json.Formatting.None);
+
+                // Write the serialized log event to the output.
+                output.WriteLine(serializedLogEvent);
+            }
+        }
+    }
+}
