5d1d07b250fe00a094b8f2474a6dc9cef84e8561
JivkoSp <atjspasov@gmail.com>
2024-07-08 15:18:34 +0300

    Added EntityFramework models in PublicHistoryService.Infrastructure/EntityFramework/Models/. The models are: CommentedArticleReadModel.cs, SearchedArticleDataReadModel.cs, SearchedTopicDataReadModel.cs, UserReadModel.cs, ViewedArticleReadModel.cs and ViewedUserReadModel.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Models/SearchedArticleDataReadModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PublicHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class SearchedArticleDataReadModel
+    {
+        //Primary key
+        public Guid SearchedArticleDataId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        //Relationships
+        public virtual UserReadModel User { get; set; }
+    }
+}
---
+++ PublicHistoryService.Infrastructure/EntityFramework/Models/SearchedTopicDataReadModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace PublicHistoryService.Infrastructure.EntityFramework.Models
+{
+    internal class SearchedTopicDataReadModel
+    {
+        //Primary key
+        public Guid SearchedTopicDataId { get; set; }
+        public Guid TopicId { get; set; }
+        public string UserId { get; set; }
+        public string SearchedData { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+
+        //Relationships
+        public virtual UserReadModel User { get; set; }
+    }
+}
