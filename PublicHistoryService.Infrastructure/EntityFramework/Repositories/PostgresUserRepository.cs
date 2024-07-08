2911ec5fabc51db3a807b518e976bbc0cbd042eb
JivkoSp <atjspasov@gmail.com>
2024-07-08 21:25:59 +0300

    Added read service - Implementation of the IUserRepository interface: PublicHistoryService.Infrastructure/EntityFramework/Repositories/PostgresUserRepository.cs

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Repositories/PostgresUserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublicHistoryService.Domain.Entities;
+using PublicHistoryService.Domain.Repositories;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Repositories
+{
+    internal sealed class PostgresUserRepository : IUserRepository
+    {
+        private readonly WriteDbContext _writeDbContext;
+
+        public PostgresUserRepository(WriteDbContext writeDbContext)
+        {
+            _writeDbContext = writeDbContext;
+        }
+
+        public Task<User> GetUserByIdAsync(UserID userId)
+            => _writeDbContext.Users
+               .Include(x => x.ViewedArticles)
+               .Include(x => x.ViewedUsers)
+               .Include(x => x.SearchedArticleInformation)
+               .Include(x => x.SearchedTopicInformation)
+               .Include(x => x.CommentedArticles)
+               .SingleOrDefaultAsync(x => x.Id == userId);
+
+        public async Task AddUserAsync(User user)
+        {
+            await _writeDbContext.Users.AddAsync(user);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _writeDbContext.Users.Update(user);
+
+            await _writeDbContext.SaveChangesAsync();
+        }
+    }
+}
