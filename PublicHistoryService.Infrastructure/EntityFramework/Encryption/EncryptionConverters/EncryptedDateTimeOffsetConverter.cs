794c42ae09a0c32e6d9bc3587e1118d906a68780
JivkoSp <atjspasov@gmail.com>
2024-07-08 16:09:21 +0300

    Added custom EntityFramework DateTimeOffset converter: PublicHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
+{
+    //Converts a DateTimeOffset into a string, encrypts that string, and then provides the functionality
+    //to decrypt it back into a DateTimeOffset.
+    internal sealed class EncryptedDateTimeOffsetConverter : ValueConverter<DateTimeOffset, string>
+    {
+        /// <param name="encryptionProvider">An instance of IEncryptionProvider used for encryption and decryption operations.</param>
+        public EncryptedDateTimeOffsetConverter(IEncryptionProvider encryptionProvider)
+                    //Encrypts the DateTimeOffset.
+            : base(v => encryptionProvider.Encrypt(v.ToString("o")),
+               // Decrypts the encrypted string representation of the DateTimeOffset back to DateTimeOffset.
+               v => DateTimeOffset.Parse(encryptionProvider.Decrypt(v)))
+        {
+        }
+    }
+}
