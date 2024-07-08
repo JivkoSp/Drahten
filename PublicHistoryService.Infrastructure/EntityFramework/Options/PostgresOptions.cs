2e4372ed1ec6d6294cf5c633f837014ea958df33
JivkoSp <atjspasov@gmail.com>
2024-07-08 15:23:57 +0300

    Added PublicHistoryService.Infrastructure/EntityFramework/Options/PostgresOptions.cs

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Options/PostgresOptions.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PublicHistoryService.Infrastructure.EntityFramework.Options
+{
+    internal class PostgresOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
