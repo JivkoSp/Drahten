01e2f9cf328a7d4d1fd515d4d85f14bfed115b38
JivkoSp <atjspasov@gmail.com>
2024-07-08 22:16:28 +0300

    Added read service - Implementation of the ICommentedArticleReadService interface: PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresCommentedArticleReadService.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresCommentedArticleReadService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Application.Services.ReadServices;
+using PublicHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresCommentedArticleReadService : ICommentedArticleReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public PostgresCommentedArticleReadService(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<CommentedArticleDto> GetCommentedArticleByIdAsync(Guid commentedArticleId)
+        {
+            var commentedArticleReadModel = await _readDbContext.CommentedArticles
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.CommentedArticleId == commentedArticleId);
+
+            return _mapper.Map<CommentedArticleDto>(commentedArticleReadModel);
+        }
+    }
+}
