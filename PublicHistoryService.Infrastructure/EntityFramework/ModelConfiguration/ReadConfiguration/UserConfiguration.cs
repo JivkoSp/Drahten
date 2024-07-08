11c8b0a8c1e9e5a972af1e410e362a29051654b0
JivkoSp <atjspasov@gmail.com>
2024-07-08 16:50:03 +0300

    Added EntityFramework configurations for the UserReadModel in PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<UserReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public UserConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<UserReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("User");
+
+            //Primary key
+            builder.HasKey(key => key.UserId);
+
+            builder.Property(p => p.UserId)
+                .ValueGeneratedNever();
+        }
+    }
+}
