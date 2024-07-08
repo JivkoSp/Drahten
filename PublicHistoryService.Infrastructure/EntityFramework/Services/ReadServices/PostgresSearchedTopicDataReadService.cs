8019e70bf167dbc772cd0f34a7fb4bf547bdd4ef
JivkoSp <atjspasov@gmail.com>
2024-07-08 22:25:56 +0300

    Added read service - Implementation of the ISearchedTopicDataReadService interface: PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresSearchedTopicDataReadService.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresSearchedTopicDataReadService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Application.Services.ReadServices;
+using PublicHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresSearchedTopicDataReadService : ISearchedTopicDataReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+        private readonly IMapper _mapper;
+
+        public PostgresSearchedTopicDataReadService(ReadDbContext readDbContext, IMapper mapper)
+        {
+            _readDbContext = readDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<SearchedTopicDataDto> GetSearchedTopicDataByIdAsync(Guid searchedTopicDataId)
+        {
+            var searchedTopicDataReadModel = await _readDbContext.SearchedTopics
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.SearchedTopicDataId == searchedTopicDataId);
+
+            return _mapper.Map<SearchedTopicDataDto>(searchedTopicDataReadModel);
+        }
+    }
+}
