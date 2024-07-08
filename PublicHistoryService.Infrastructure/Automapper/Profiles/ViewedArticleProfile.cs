4e3cacdcf291e61fcbca7d9b5df9a0c4c7fe3088
JivkoSp <atjspasov@gmail.com>
2024-07-08 23:35:46 +0300

    Added new automapper profile type with mapping configurations: PublicHistoryService.Infrastructure/Automapper/Profiles/ViewedArticleProfile.cs.

---
+++ PublicHistoryService.Infrastructure/Automapper/Profiles/ViewedArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class ViewedArticleProfile : Profile
+    {
+        public ViewedArticleProfile()
+        {
+            CreateMap<ViewedArticleReadModel, ViewedArticleDto>();
+
+            CreateMap<ViewedArticleDto, ViewedArticle>()
+                .ConstructUsing(source =>
+                    new ViewedArticle(Guid.Parse(source.ArticleId), Guid.Parse(source.UserId), source.DateTime.ToUniversalTime()));
+        }
+    }
+}
