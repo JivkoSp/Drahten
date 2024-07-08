a4ea2c6242be2ea8fc6e6d9748d7d8298368e686
JivkoSp <atjspasov@gmail.com>
2024-07-08 20:04:44 +0300

    Added EntityFramework configurations for the ViewedUser value object in PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ViewedUserConfiguration.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ViewedUserConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class ViewedUserConfiguration : IEntityTypeConfiguration<ViewedUser>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ViewedUserConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<ViewedUser> builder)
+        {
+            //Table name
+            builder.ToTable("ViewedUser");
+
+            //Shadow property for ViewedUserReadModelId.
+            builder.Property<Guid>("ViewedUserReadModelId");
+
+            //Primary key
+            builder.HasKey("ViewedUserReadModelId");
+
+            //Property config - Start
+
+            builder.Property(p => p.ViewerUserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(p => p.ViewedUserId)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+             .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+             .IsRequired();
+
+            //Property config - End
+        }
+    }
+}
