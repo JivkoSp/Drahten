55e6ac3fbb9257e9ee67c4b79707a4458c265d78
JivkoSp <atjspasov@gmail.com>
2024-07-08 22:55:32 +0300

    Added automapper profile type: PublicHistoryService.Infrastructure/Automapper/Profiles/CommentedArticleProfile.cs.

---
+++ PublicHistoryService.Infrastructure/Automapper/Profiles/CommentedArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class CommentedArticleProfile : Profile
+    {
+        public CommentedArticleProfile()
+        {
+            CreateMap<CommentedArticleReadModel, CommentedArticleDto>();
+
+            CreateMap<CommentedArticleDto, CommentedArticle>()
+             .ConstructUsing(source =>
+                 new CommentedArticle(Guid.Parse(source.ArticleId), Guid.Parse(source.UserId), source.ArticleComment, source.DateTime.ToUniversalTime()));
+        }
+    }
+}
