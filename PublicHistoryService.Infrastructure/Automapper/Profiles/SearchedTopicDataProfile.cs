bf34850922a7134a4f3648037f044ae22d9e29d3
JivkoSp <atjspasov@gmail.com>
2024-07-08 23:32:30 +0300

    Added new automapper profile type with mapping configurations: PublicHistoryService.Infrastructure/Automapper/Profiles/SearchedTopicDataProfile.cs

---
+++ PublicHistoryService.Infrastructure/Automapper/Profiles/SearchedTopicDataProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class SearchedTopicDataProfile : Profile
+    {
+        public SearchedTopicDataProfile()
+        {
+            CreateMap<SearchedTopicDataReadModel, SearchedTopicDataDto>();
+        }
+    }
+}
