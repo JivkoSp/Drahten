3c6b46f97c14e56a12cf0ffaa4097521190ee018
JivkoSp <atjspasov@gmail.com>
2024-07-08 19:55:15 +0300

    Added EntityFramework configurations for the ViewedArticle value object in PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ViewedArticleConfiguration.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/WriteConfiguration/ViewedArticleConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.WriteConfiguration
+{
+    internal sealed class ViewedArticleConfiguration : IEntityTypeConfiguration<ViewedArticle>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ViewedArticleConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<ViewedArticle> builder)
+        {
+            //Table name
+            builder.ToTable("ViewedArticle");
+
+            //Shadow property for ViewedArticleId.
+            builder.Property<Guid>("ViewedArticleId");
+
+            //Primary key
+            builder.HasKey("ViewedArticleId");
+
+            //Property config - Start
+
+            #region ConversionToUUID
+            // *** IMPORTANT! ***
+            // Convert Guid to string without hyphens.
+            // Explanation: UUIDs are commonly represented as a 32-character hexadecimal string (such as "37955c6317e6869e90fe0029564078c8")
+            // or as a 36-character string with hyphens separating groups of characters (such as "37955c63-17e6-869e-90fe-0029564078c8").
+            // The Id is NOT stored as Guid in the database, because PostgreSQL will automatically format it with hyphens for readability
+            // and standardization. This will cause problems when accessing documents with their Id's from the SearchService database store
+            // (a Elasticserach instance). The Elasticsearch instance will expect the Id's to be in a 32-character hexadecimal format and 
+            // will not be able to find the document's if it receaves a 36-character hexadecimal format for the Id.
+            // ---------------------------------------
+            // !!! The "N" in the ToString method is used to format the Guid without hyphens (or dashes).
+            #endregion
+            builder.Property(p => p.ArticleID)
+                .HasConversion(id => id.Value.ToString("N"), id => new ArticleID(Guid.ParseExact(id, "N")))
+                .HasColumnName("ArticleId")
+                .IsRequired();
+
+            builder.Property(p => p.UserID)
+                .HasConversion(id => id.Value.ToString(), id => new UserID(Guid.Parse(id)))
+                .HasColumnName("UserId")
+                .IsRequired();
+
+            builder.Property(typeof(DateTimeOffset), "DateTime")
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+                .IsRequired();
+
+            //Property config - End
+        }
+    }
+}
