76b431b2a47a7ed7be18ae52f1d7c675f0cff4d0
JivkoSp <atjspasov@gmail.com>
2024-07-08 16:29:53 +0300

    Added EntityFramework configurations for the SearchedArticleDataReadModel in PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/SearchedArticleDataConfiguration.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/ModelConfiguration/ReadConfiguration/SearchedArticleDataConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration
+{
+    internal sealed class SearchedArticleDataConfiguration : IEntityTypeConfiguration<SearchedArticleDataReadModel>
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public SearchedArticleDataConfiguration(IEncryptionProvider encryptionProvider)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public void Configure(EntityTypeBuilder<SearchedArticleDataReadModel> builder)
+        {
+            //Table name
+            builder.ToTable("SearchedArticleData");
+
+            //Primary key
+            builder.HasKey(key => key.SearchedArticleDataId);
+
+            //Property config - Start
+
+            builder.Property(p => p.ArticleId)
+                .IsRequired();
+
+            builder.Property(p => p.UserId)
+                .IsRequired();
+
+            builder.Property(p => p.SearchedData)
+                .HasConversion(new EncryptedStringConverter(_encryptionProvider))
+                .IsRequired();
+
+            builder.Property(p => p.DateTime)
+                .HasConversion(new EncryptedDateTimeOffsetConverter(_encryptionProvider))
+                .IsRequired();
+
+            //Property config - End
+
+            //Relationships
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.SearchedArticles)
+                .HasForeignKey(p => p.UserId)
+                .HasConstraintName("FK_User_SearchedArticles")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
