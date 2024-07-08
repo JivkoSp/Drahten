f0264b3e917e6d5261765f959b7ba4f787832cfd
JivkoSp <atjspasov@gmail.com>
2024-07-08 20:47:05 +0300

    Added custom infrastructure layer root (base) exception: PublicHistoryService.Infrastructure/Exceptions/InfrastructureException.cs.

---
+++ PublicHistoryService.Infrastructure/Exceptions/InfrastructureException.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PublicHistoryService.Infrastructure.Exceptions
+{
+    internal abstract class InfrastructureException : Exception
+    {
+        public virtual string ErrorCode { get; set; }
+
+        internal InfrastructureException(string message) : base(message)
+        {
+        }
+    }
+}
