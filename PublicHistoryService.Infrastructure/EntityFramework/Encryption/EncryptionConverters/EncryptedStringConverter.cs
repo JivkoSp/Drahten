60df354594eda90d2c566856e5872cad36cc3179
JivkoSp <atjspasov@gmail.com>
2024-07-08 16:13:54 +0300

    Added comments to the PublicHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs

--- PublicHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
+++ PublicHistoryService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
@@ -3,10 +3,14 @@ using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionP
 
 namespace PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
 {
+    //Converts a string into a encrypted string and then provides the functionality to decrypt it back.
     internal sealed class EncryptedStringConverter : ValueConverter<string, string>
     {
+        /// <param name="encryptionProvider">An instance of IEncryptionProvider used for encryption and decryption operations.</param>
         public EncryptedStringConverter(IEncryptionProvider encryptionProvider)
+                   //Encrypts the string.
             : base(v => encryptionProvider.Encrypt(v.ToString()),
+                   // Decrypts the encrypted string.
                    v => encryptionProvider.Decrypt(v))
         {
         }
