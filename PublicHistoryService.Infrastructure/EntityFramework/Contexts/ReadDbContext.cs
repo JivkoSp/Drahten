aab626615f0b8e36f1996bdb79c78fe55cd66b01
JivkoSp <atjspasov@gmail.com>
2024-07-08 20:34:35 +0300

    Added read db context: PublicHistoryService.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Contexts/ReadDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublicHistoryService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
+using PublicHistoryService.Infrastructure.EntityFramework.ModelConfiguration.ReadConfiguration;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Contexts
+{
+    internal sealed class ReadDbContext : DbContext
+    {
+        private readonly IEncryptionProvider _encryptionProvider;
+
+        public ReadDbContext(DbContextOptions<ReadDbContext> options, IEncryptionProvider encryptionProvider) : base(options)
+        {
+            _encryptionProvider = encryptionProvider;
+        }
+
+        public DbSet<UserReadModel> Users { get; set; }
+        public DbSet<CommentedArticleReadModel> CommentedArticles { get; set; }
+        public DbSet<SearchedArticleDataReadModel> SearchedArticles { get; set; }
+        public DbSet<SearchedTopicDataReadModel> SearchedTopics { get; set; }
+        public DbSet<ViewedArticleReadModel> ViewedArticles { get; set; }
+        public DbSet<ViewedUserReadModel> ViewedUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("private-history-service");
+
+            base.OnModelCreating(modelBuilder);
+
+            //Applying configurations for the entity models
+            modelBuilder.ApplyConfiguration(new CommentedArticleConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new SearchedArticleDataConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new SearchedTopicDataConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new UserConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ViewedArticleConfiguration(_encryptionProvider));
+            modelBuilder.ApplyConfiguration(new ViewedUserConfiguration(_encryptionProvider));
+        }
+    }
+}
