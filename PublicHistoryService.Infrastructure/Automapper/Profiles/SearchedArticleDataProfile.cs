421032f77e67b5235d01e174e67d954a6dfb7ddc
JivkoSp <atjspasov@gmail.com>
2024-07-08 22:59:13 +0300

    Added new automapper profile type wuth mapping configurations: PublicHistoryService.Infrastructure/Automapper/Profiles/SearchedArticleDataProfile.cs.

---
+++ PublicHistoryService.Infrastructure/Automapper/Profiles/SearchedArticleDataProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Domain.ValueObjects;
+using PublicHistoryService.Infrastructure.EntityFramework.Models;
+
+namespace PublicHistoryService.Infrastructure.Automapper.Profiles
+{
+    internal sealed class SearchedArticleDataProfile : Profile
+    {
+        public SearchedArticleDataProfile()
+        {
+            CreateMap<SearchedArticleDataReadModel, SearchedArticleDataDto>();
+
+            CreateMap<SearchedArticleDataDto, SearchedArticleData>()
+               .ConstructUsing(source =>
+                   new SearchedArticleData(Guid.Parse(source.ArticleId), Guid.Parse(source.UserId), source.SearchedData, source.DateTime.ToUniversalTime()));
+        }
+    }
+}
