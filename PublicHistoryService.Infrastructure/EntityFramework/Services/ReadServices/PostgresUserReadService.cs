a1a63c2c63000ffe76429832123a687c7fb06887
JivkoSp <atjspasov@gmail.com>
2024-07-08 22:28:15 +0300

    Added read service - Implementation of the IUserReadService interface: PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresUserReadService.cs.

---
+++ PublicHistoryService.Infrastructure/EntityFramework/Services/ReadServices/PostgresUserReadService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublicHistoryService.Application.Services.ReadServices;
+using PublicHistoryService.Infrastructure.EntityFramework.Contexts;
+
+namespace PublicHistoryService.Infrastructure.EntityFramework.Services.ReadServices
+{
+    internal sealed class PostgresUserReadService : IUserReadService
+    {
+        private readonly ReadDbContext _readDbContext;
+
+        public PostgresUserReadService(ReadDbContext readDbContext)
+        {
+            _readDbContext = readDbContext;
+        }
+
+        public Task<bool> ExistsByIdAsync(Guid id)
+            => _readDbContext.Users.AnyAsync(x => x.UserId == id.ToString());
+    }
+}
