8e77ba8747807946923a32cdf18dbd279d0dd21d
JivkoSp <atjspasov@gmail.com>
2024-07-09 19:31:25 +0300

    Added PublicHistoryService.Tests.EndToEnd/Extensions/ServiceCollectionExtensions.cs. This class contains IServiceCollection extension method named RemoveDbContext.

---
+++ PublicHistoryService.Tests.EndToEnd/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PublicHistoryService.Tests.EndToEnd.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        internal static IServiceCollection RemoveDbContext<TContext>(this IServiceCollection services)
+           where TContext : DbContext
+        {
+            var serviceDescriptor = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<TContext>));
+
+            if (serviceDescriptor != null)
+            {
+                //Remove the TContext from the configured services in Program.cs
+                services.Remove(serviceDescriptor);
+            }
+
+            return services;
+        }
+    }
+}
