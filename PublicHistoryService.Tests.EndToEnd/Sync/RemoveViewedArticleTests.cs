d8a9fc82f95897dfb87c34003b5f41e4473a854d
JivkoSp <atjspasov@gmail.com>
2024-07-09 23:27:46 +0300

    Added integration tests for the RemoveViewedArticle endpoint that is defined in PublicHistoryService.Presentation/Controllers/UserController.cs. The integration tests are defined in PublicHistoryService.Tests.EndToEnd/Sync/RemoveViewedArticleTests.cs.

---
+++ PublicHistoryService.Tests.EndToEnd/Sync/RemoveViewedArticleTests.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using PublicHistoryService.Application.Commands;
+using PublicHistoryService.Application.Dtos;
+using PublicHistoryService.Presentation.Dtos;
+using PublicHistoryService.Tests.EndToEnd.Factories;
+using Shouldly;
+using System.Net;
+using Xunit;
+
+namespace PublicHistoryService.Tests.EndToEnd.Sync
+{
+    public class RemoveViewedArticleTests : BaseSyncIntegrationTest
+    {
+        #region GLOBAL ARRANGE
+
+        private async Task<AddViewedArticleCommand> PrepareAddViewedArticleCommandAsync()
+        {
+            var userId = Guid.NewGuid();
+
+            var command = new AddUserCommand(userId);
+
+            await Post(command, $"/publichistory-service/users/{userId}");
+
+            var addViewedArticleCommand = new AddViewedArticleCommand(ArticleId: Guid.NewGuid(), UserId: userId,
+                DateTime: DateTimeOffset.Now);
+
+            return addViewedArticleCommand;
+        }
+
+        private async Task<ViewedArticleDto> GetViewedArticleDtoAsync(Guid userId)
+        {
+            var viewedArticlesResponse = await Get($"/publichistory-service/users/{userId}/viewed-articles/");
+
+            var responseSerializedContent = await viewedArticlesResponse.Content.ReadAsStringAsync();
+
+            var responseDto = JsonConvert.DeserializeObject<ResponseDto>(responseSerializedContent);
+
+            var responseResult = JsonConvert.DeserializeObject<List<ViewedArticleDto>>(Convert.ToString(responseDto.Result));
+
+            return responseResult.FirstOrDefault();
+        }
+
+        private async Task<RemoveViewedArticleCommand> PrepareRemoveViewedArticleCommandAsync()
+        {
+            var command = await PrepareAddViewedArticleCommandAsync();
+
+            await Post(command, $"/publichistory-service/users/{command.UserId}/viewed-articles/{command.ArticleId}");
+
+            var viewedArticleDto = await GetViewedArticleDtoAsync(command.UserId);
+
+            var removeViewedArticleCommand = new RemoveViewedArticleCommand(UserId: command.UserId,
+                ViewedArticleId: viewedArticleDto.ViewedArticleId);
+
+            return removeViewedArticleCommand;
+        }
+
+        public RemoveViewedArticleTests(PrivateHistoryServiceApplicationFactory factory) : base(factory)
+        {
+        }
+
+        #endregion
+
+        //Should return http status code 204 if ViewedArticle value object that has values like the ones that are
+        //specified in RemoveViewedArticleCommand is deleted from the database.
+        [Fact]
+        public async Task Remove_Viewed_Article_Endpoint_Should_Return_Http_Status_Code_NoContent()
+        {
+            //ARRANGE
+            var command = await PrepareRemoveViewedArticleCommandAsync();
+
+            //ACT
+            var response = await Delete($"/publichistory-service/users/{command.UserId}/viewed-articles/{command.ViewedArticleId}");
+
+            //ASSERT
+            response.ShouldNotBeNull();
+
+            response.ReasonPhrase.ShouldBe("No Content");
+
+            response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+        }
+
+        //Should return http status code 404 when the previously deleted ViewedArticle value object is not found in the database.
+        [Fact]
+        public async Task Remove_Viewed_Article_Endpoint_Should_Remove_ViewedArticle_From_The_Database()
+        {
+            //ARRANGE
+            var command = await PrepareRemoveViewedArticleCommandAsync();
+
+            await Delete($"/publichistory-service/users/{command.UserId}/viewed-articles/{command.ViewedArticleId}");
+
+            //ACT
+            var response = await Get($"/publichistory-service/users/{command.UserId}/viewed-articles/");
+
+            //ASSERT
+            response.ShouldNotBeNull();
+
+            response.ReasonPhrase.ShouldBe("Not Found");
+
+            response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        }
+    }
+}
