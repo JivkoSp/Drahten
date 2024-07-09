fa0071dad6519ade0dfd8c690c3d56eeb852d5f3
JivkoSp <atjspasov@gmail.com>
2024-07-09 12:46:11 +0300

    Modified the PublicHistoryService.Presentation/Program.cs. The modifications are: 1. Added Serilog configurations and registration to the DI container; 2. Added rate limiter configurations and registration to the DI container; 3. Added ErrorHandling middleware registration to the DI container; 4. Added authentication and authorization to the DI container.

--- PublicHistoryService.Presentation/Program.cs
+++ PublicHistoryService.Presentation/Program.cs
@@ -1,10 +1,87 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using PublicHistoryService.Application.Extensions;
+using PublicHistoryService.Infrastructure.Extensions;
+using PublicHistoryService.Infrastructure.Logging.Formatters;
+using PublicHistoryService.Presentation.Middlewares;
+using Serilog;
+using System.Threading.RateLimiting;
+
+// Add Serilog configuration
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console() // Add console sink.
+    .WriteTo.Http(requestUri: "http://localhost:5000",
+                  queueLimitBytes: 1000000,
+                  batchFormatter: new SerilogJsonFormatter()) //Add http sink.
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseSerilog();
+
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var options = new TokenBucketRateLimiterOptions
+    {
+        TokenLimit = 100, // Max number of tokens in the bucket
+        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+        QueueLimit = 10, // Allows for brief traffic bursts
+        ReplenishmentPeriod = TimeSpan.FromSeconds(1),
+        TokensPerPeriod = 10, // Tokens added per replenishment period
+        AutoReplenishment = true
+    };
+
+    return new TokenBucketRateLimiter(options);
+});
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddTransient<ErrorHandlerMiddleware>();
+
+builder.Services.AddTransient<RateLimitingMiddleware>();
+
+builder.Services.AddAuthentication(options => {
+
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+
+    options.Authority = "http://127.0.0.1:8080/realms/drahten";
+    // *** IMPORTANT ***
+    //This is for development ONLY. The reason is that the OpenID provider uses http.
+    //In production this should be removed, becouse the OpenID provider must use https.
+    options.RequireHttpsMetadata = false;
+    options.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = authFailedContext =>
+        {
+            if (authFailedContext.HttpContext.Request != null)
+            {
+                Log.Error("PrivateHistoryService --> Jwt token validation failed!");
+            }
+
+            return Task.CompletedTask;
+        }
+    };
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        // *** IMPORTANT ***
+        //For development ONLY
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddAuthorization();
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -18,8 +95,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+app.UseMiddleware<ErrorHandlerMiddleware>();
+
+app.UseMiddleware<RateLimitingMiddleware>();
 
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
