268b1f96581ee035cdf9c870c649aa9a63b56299
JivkoSp <atjspasov@gmail.com>
2024-07-09 22:20:54 +0300

    Added integration tests for the RegisterUser endpoint that is defined in PublicHistoryService.Presentation/Controllers/UserController.cs. The integration tests are defined in PublicHistoryService.Tests.EndToEnd/Sync/RegisterUserTests.cs.

---
+++ PublicHistoryService.Tests.EndToEnd/Sync/RegisterUserTests.cs
@@ -0,0 +1,41 @@
+﻿using PublicHistoryService.Application.Commands;
+using PublicHistoryService.Tests.EndToEnd.Factories;
+using Shouldly;
+using System.Net;
+using Xunit;
+
+namespace PublicHistoryService.Tests.EndToEnd.Sync
+{
+    public class RegisterUserTests : BaseSyncIntegrationTest
+    {
+        #region GLOBAL ARRANGE
+
+        public RegisterUserTests(PrivateHistoryServiceApplicationFactory factory) : base(factory)
+        {
+        }
+
+        #endregion
+
+        //Should return http status code 201 if User domain entity is created from the specified parameters in AddUserCommand.
+        [Fact]
+        public async Task Register_User_Endpoint_Should_Return_Http_Status_Code_Created()
+        {
+            //ARRANGE
+            var userId = Guid.NewGuid();
+
+            var command = new AddUserCommand(userId);
+
+            //ACT
+            var response = await Post(command, $"/publichistory-service/users/{command.UserId}");
+
+            //ASSERT
+            response.ShouldNotBeNull();
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+            response.Headers.Location.ShouldNotBeNull();
+
+            response.Headers.Location.ToString().ShouldBe($"/publichistory-service/users/{command.UserId}");
+        }
+    }
+}
