1744c6165f7f184ed0974a161a3a00bc188e2889
JivkoSp <atjspasov@gmail.com>
2024-07-10 18:55:57 +0300

    Added SearchedArticleData action method in DrahtenWeb/Controllers/PublicHistoryController.cs.

--- DrahtenWeb/Controllers/PublicHistoryController.cs
+++ DrahtenWeb/Controllers/PublicHistoryController.cs
@@ -18,7 +18,7 @@ namespace DrahtenWeb.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CommentedArticle(string articleId, string userId, string articleComment, DateTimeOffset dateTime)
+        public async Task CommentedArticle(string articleId, string userId, string articleComment, DateTimeOffset dateTime)
         {
             var accessToken = await HttpContext.GetTokenAsync("access_token");
             
@@ -31,8 +31,22 @@ namespace DrahtenWeb.Controllers
             };
 
             await _publicHistoryService.AddCommentedArticleAsync<ResponseDto>(writeCommentedArticleDto, accessToken);
+        }
 
-            return new JsonResult(new { });
+        [HttpPost]
+        public async Task SearchedArticleData(string articleId, string userId, string searchedData, DateTimeOffset dateTime)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var writeSearchedArticleDataDto = new WriteSearchedArticleDataDto
+            {
+                ArticleId = Guid.Parse(articleId),
+                UserId = Guid.Parse(userId),
+                SearchedData = searchedData,
+                DateTime = dateTime
+            };
+
+            await _publicHistoryService.AddSearchedArticleDataAsync<ResponseDto>(writeSearchedArticleDataDto, accessToken);
         }
     }
 }
