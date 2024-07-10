3e8f2c7125949f7a67042f49d73cd146df05c8b3
JivkoSp <atjspasov@gmail.com>
2024-07-10 18:07:15 +0300

    Added new custom middleware for user registration: PublicHistoryService.Presentation/Middlewares/UserRegistrationMiddleware.cs

---
+++ PublicHistoryService.Presentation/Middlewares/UserRegistrationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using PublicHistoryService.Application.Commands;
+using PublicHistoryService.Infrastructure.UserRegistration;
+using System.Security.Claims;
+
+namespace PublicHistoryService.Presentation.Middlewares
+{
+    public sealed class UserRegistrationMiddleware : IMiddleware
+    {
+        private readonly IUserSynchronizer _userSynchronizer;
+
+        public UserRegistrationMiddleware(IUserSynchronizer userSynchronizer)
+        {
+            _userSynchronizer = userSynchronizer;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            var addUserCommand = new AddUserCommand(userId);
+
+            //Register the user in the database if it does not already exist.
+            await _userSynchronizer.SynchronizeUserAsync(addUserCommand);
+
+            await next(context);
+        }
+    }
+}
