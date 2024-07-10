4b23fc17c93f37c4c2dfaa23b316526555f0926d
JivkoSp <atjspasov@gmail.com>
2024-07-10 18:11:54 +0300

    Added CommentedArticle action method in DrahtenWeb/Controllers/PublicHistoryController.cs.

--- DrahtenWeb/Controllers/PublicHistoryController.cs
+++ DrahtenWeb/Controllers/PublicHistoryController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DrahtenWeb.Dtos;
+using DrahtenWeb.Dtos.PublicHistoryService;
+using DrahtenWeb.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DrahtenWeb.Controllers
@@ -6,9 +10,29 @@ namespace DrahtenWeb.Controllers
     [Authorize]
     public class PublicHistoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IPublicHistoryService _publicHistoryService;
+
+        public PublicHistoryController(IPublicHistoryService publicHistoryService)
         {
-            return View();
+            _publicHistoryService = publicHistoryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CommentedArticle(string articleId, string userId, string articleComment, DateTimeOffset dateTime)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            
+            var writeCommentedArticleDto = new WriteCommentedArticleDto
+            { 
+                ArticleId = Guid.Parse(articleId),
+                UserId = Guid.Parse(userId),
+                ArticleComment = articleComment,
+                DateTime = dateTime
+            };
+
+            await _publicHistoryService.AddCommentedArticleAsync<ResponseDto>(writeCommentedArticleDto, accessToken);
+
+            return new JsonResult(new { });
         }
     }
 }
