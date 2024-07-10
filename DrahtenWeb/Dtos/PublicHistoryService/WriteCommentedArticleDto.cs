ed93f29dca6ab93faada185aa5f4917345ad688f
JivkoSp <atjspasov@gmail.com>
2024-07-10 12:44:28 +0300

    Added new dto type: DrahtenWeb/Dtos/PublicHistoryService/WriteCommentedArticleDto.cs.

---
+++ DrahtenWeb/Dtos/PublicHistoryService/WriteCommentedArticleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.Dtos.PublicHistoryService
+{
+    public class WriteCommentedArticleDto
+    {
+        public Guid ArticleId {  get; set; } 
+        public Guid UserId { get; set; }
+        public string ArticleComment { get; set; }
+        public DateTimeOffset DateTime {  get; set; }
+    }
+}
