41efe023b1c0b7a096ae8466436667cdfeddbcdb
JivkoSp <atjspasov@gmail.com>
2024-07-11 10:57:26 +0300

    Added new dto type: DrahtenWeb/Dtos/PublicHistoryService/ReadViewedArticleDto.cs

---
+++ DrahtenWeb/Dtos/PublicHistoryService/ReadViewedArticleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DrahtenWeb.Dtos.PublicHistoryService
+{
+    public class ReadViewedArticleDto
+    {
+        public Guid ViewedArticleId { get; set; }
+        public string ArticleId { get; set; }
+        public string UserId { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+    }
+}
