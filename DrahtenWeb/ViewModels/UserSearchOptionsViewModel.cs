18ae123e3da1da4fc50e3e0674cd805fd76bca7c
JivkoSp <atjspasov@gmail.com>
2024-07-11 22:04:46 +0300

    Modified the DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs. The modifications are: Added the following properties: TopicArticles, TopicSubscriptions, TopicSources

--- DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs
+++ DrahtenWeb/ViewModels/UserSearchOptionsViewModel.cs
@@ -8,6 +8,9 @@ namespace DrahtenWeb.ViewModels
         public List<TopicDto> Topics { get; set; } = new List<TopicDto>();
         public Dictionary<string, List<ReadArticleCommentDto>> ArticleComments { get; set; } = new Dictionary<string, List<ReadArticleCommentDto>>();
         public Dictionary<string, List<ReadUserArticleDto>> UsersRelatedToArticle { get; set; } = new Dictionary<string, List<ReadUserArticleDto>>();
+        public Dictionary<Guid, List<ArticleDto>> TopicArticles { get; set; } = new Dictionary<Guid, List<ArticleDto>>();
+        public Dictionary<Guid, List<UserTopicDto>> TopicSubscriptions { get; set; } = new Dictionary<Guid, List<UserTopicDto>>();
+        public Dictionary<Guid, HashSet<KeyValuePair<string, string>>> TopicSources { get; set; } = new Dictionary<Guid, HashSet<KeyValuePair<string, string>>>();
         public List<UserTopicDto> UserTopics { get; set; } = new List<UserTopicDto>();
     }
 }
