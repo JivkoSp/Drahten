8f9bc3478b2c3d76ed639128e8054ccd8271e4b4
JivkoSp <atjspasov@gmail.com>
2024-07-11 21:39:48 +0300

    Added new query type: TopicArticleService.Application/Queries/GetTopicSubscriptionsQuery.cs.

---
+++ TopicArticleService.Application/Queries/GetTopicSubscriptionsQuery.cs
@@ -0,0 +1,9 @@
+﻿using TopicArticleService.Application.Dtos;
+
+namespace TopicArticleService.Application.Queries
+{
+    public class GetTopicSubscriptionsQuery : IQuery<List<UserTopicDto>>
+    {
+        public Guid TopicId { get; set; }
+    }
+}
