2d6fb10bc6eb4fcaa94f7f8c5fc9b6d62d20aa16
JivkoSp <atjspasov@gmail.com>
2024-07-11 10:58:37 +0300

    Added ViewedArticles action method in DrahtenWeb/Controllers/PublicHistoryController.cs

--- DrahtenWeb/Controllers/PublicHistoryController.cs
+++ DrahtenWeb/Controllers/PublicHistoryController.cs
@@ -1,9 +1,15 @@
 ﻿using DrahtenWeb.Dtos;
 using DrahtenWeb.Dtos.PublicHistoryService;
+using DrahtenWeb.Dtos.TopicArticleService;
+using DrahtenWeb.Extensions;
+using DrahtenWeb.Models;
 using DrahtenWeb.Services.IServices;
+using DrahtenWeb.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Security.Claims;
 
 namespace DrahtenWeb.Controllers
 {
@@ -11,10 +17,109 @@ namespace DrahtenWeb.Controllers
     public class PublicHistoryController : Controller
     {
         private readonly IPublicHistoryService _publicHistoryService;
+        private readonly ITopicArticleService _topicArticleService;
+        private readonly IMemoryCache _cache;
+        private readonly MemoryCacheEntryOptions _cacheEntryOptions;
+        private const string ViewedArticlesCacheKey = "ViewedArticlesDataCache";
+        private const string RetrievedViewedArticlesCacheKey = "RetrievedViewedArticlesDataCache";
 
-        public PublicHistoryController(IPublicHistoryService publicHistoryService)
+        public PublicHistoryController(IPublicHistoryService publicHistoryService, ITopicArticleService topicArticleService, IMemoryCache cache)
         {
             _publicHistoryService = publicHistoryService;
+            _topicArticleService = topicArticleService;
+            _cache = cache;
+
+            _cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1),
+                SlidingExpiration = TimeSpan.FromMinutes(10)
+            };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewedArticles(int pageNumber = 1)
+        {
+            try
+            {
+                ArticleDto articleDto;
+                List<ReadViewedArticleDto> allArticles;
+
+                // The response type that will be returned from calling the services.
+                var response = new ResponseDto();
+
+                var historyArticleViewModel = new HistoryArticleViewModel();
+
+                //Get the user id.
+                //Here the NameIdentifier claim type represents the user id.
+                var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                if (!_cache.TryGetValue(ViewedArticlesCacheKey, out allArticles))
+                {
+                    response = await _publicHistoryService.GetViewedArticlesAsync<ResponseDto>(userId, accessToken);
+
+                    var articlesForCaching = new List<ViewedArticleViewModel>();
+
+                    allArticles = response.Map<List<ReadViewedArticleDto>>();
+
+                    foreach (var article in allArticles)
+                    {
+                        response = await _topicArticleService.GetArticleByIdAsync<ResponseDto>(article.ArticleId, accessToken);
+
+                        articleDto = response.Map<ArticleDto>();
+
+                        var viewedArticleViewModel = new ViewedArticleViewModel
+                        {
+                            ViewedArticleId = article.ViewedArticleId,
+                            Article = articleDto,
+                            UserId = article.UserId,
+                            DateTime = article.DateTime
+                        };
+
+                        articlesForCaching.Add(viewedArticleViewModel);
+
+                        // Save data in cache
+                        _cache.Set(article.ArticleId, viewedArticleViewModel, _cacheEntryOptions);
+                    }
+
+                    // Save data in cache
+                    _cache.Set(ViewedArticlesCacheKey, allArticles, _cacheEntryOptions);
+
+                    //This is done in order to be able to have keyword searching for all articles.
+                    _cache.Set(RetrievedViewedArticlesCacheKey, articlesForCaching, _cacheEntryOptions);
+                }
+
+                if (pageNumber < 1)
+                {
+                    pageNumber = 1;
+                }
+
+                const int pageSize = 5;
+
+                int articlesCount = allArticles.Count;
+
+                var pagination = new Pagination(articlesCount, pageNumber, pageSize);
+
+                int skipArticles = (pageNumber - 1) * pageSize;
+
+                var articles = allArticles.Skip(skipArticles).Take(pagination.PageSize).ToList();
+
+                foreach (var article in articles)
+                {
+                    var viewedArticleViewModel = _cache.Get<ViewedArticleViewModel>(article.ArticleId);
+
+                    historyArticleViewModel.Articles.Add(viewedArticleViewModel);
+                }
+
+                historyArticleViewModel.Pagination = pagination;
+
+                return new JsonResult(historyArticleViewModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost]
