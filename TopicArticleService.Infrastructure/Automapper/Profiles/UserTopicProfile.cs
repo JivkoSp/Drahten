bf3092816be49ea2c715bccf25011f9c413bc944
JivkoSp <atjspasov@gmail.com>
2024-07-11 21:40:29 +0300

    Added new automapper profile: TopicArticleService.Infrastructure/Automapper/Profiles/UserTopicProfile.cs.

---
+++ TopicArticleService.Infrastructure/Automapper/Profiles/UserTopicProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TopicArticleService.Application.Dtos;
+using TopicArticleService.Infrastructure.EntityFramework.Models;
+
+namespace TopicArticleService.Infrastructure.Automapper.Profiles
+{
+    public class UserTopicProfile : Profile
+    {
+        public UserTopicProfile()
+        {
+            CreateMap<UserTopicReadModel, UserTopicDto>()
+                .ForMember(dest => dest.TopicName, options => options.MapFrom(source => source.Topic.TopicName))
+                .ForMember(dest => dest.TopicFullName, options => options.MapFrom(source => source.Topic.TopicFullName));
+        }
+    }
+}
