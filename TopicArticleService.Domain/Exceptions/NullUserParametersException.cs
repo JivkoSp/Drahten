a835546d8a907222deaef13316434722b3c3c628
JivkoSp <atjspasov@gmail.com>
2024-07-12 19:54:54 +0300

    Added new custom domain layer exception: TopicArticleService.Domain/Exceptions/NullUserParametersException.cs.

---
+++ TopicArticleService.Domain/Exceptions/NullUserParametersException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TopicArticleService.Domain.Exceptions
+{
+    public sealed class NullUserParametersException : DomainException
+    {
+        internal NullUserParametersException() : base(message: $"The User cannot be initialized with one or more null parameters!")
+        {
+        }
+    }
+}
