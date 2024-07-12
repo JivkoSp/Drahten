6eccf007e953b04dc958f9c2de1dbb1d2f3eb2f5
JivkoSp <atjspasov@gmail.com>
2024-07-12 23:17:39 +0300

    Refactored the TopicArticleService.Application/Queries/GetUsersRelatedToArticleQuery.cs. The query is now a record type.

--- TopicArticleService.Application/Queries/GetUsersRelatedToArticleQuery.cs
+++ TopicArticleService.Application/Queries/GetUsersRelatedToArticleQuery.cs
@@ -2,8 +2,5 @@
 
 namespace TopicArticleService.Application.Queries
 {
-    public class GetUsersRelatedToArticleQuery : IQuery<List<UserArticleDto>> //TODO: Change the dto to UserDto (there is no reason to returns the article dto)
-    {
-        public string ArticleId { get; set; }
-    }
+    public record GetUsersRelatedToArticleQuery(string ArticleId) : IQuery<List<UserDto>>; 
 }
