e3ae5f20fdef2bea9d424e8905a12843516518ec
JivkoSp <atjspasov@gmail.com>
2024-07-12 22:20:01 +0300

    Changed the access level of the Value property that is defined in the TopicArticleService.Domain/ValueObjects/ArticlePrevTitle.cs from public to internal.

--- TopicArticleService.Domain/ValueObjects/ArticlePrevTitle.cs
+++ TopicArticleService.Domain/ValueObjects/ArticlePrevTitle.cs
@@ -4,7 +4,7 @@ namespace TopicArticleService.Domain.ValueObjects
 {
     public record ArticlePrevTitle
     {
-        public string Value { get; }
+        internal string Value { get; }
 
         public ArticlePrevTitle(string value)
         {
