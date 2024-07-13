d4ffcfbf921ad74573a3ce222d02acfa368400b0
JivkoSp <atjspasov@gmail.com>
2024-07-13 15:07:28 +0300

    Added comments to the TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs

--- TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs
+++ TopicArticleService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedDateTimeOffsetConverter.cs
@@ -3,8 +3,11 @@ using TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionPr
 
 namespace TopicArticleService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
 {
+    //Converts a DateTimeOffset value into a string, encrypts that string, and then provides the functionality
+    //to decrypt it back into a DateTimeOffset value.
     internal sealed class EncryptedDateTimeOffsetConverter : ValueConverter<DateTimeOffset, string>
     {
+        /// <param name="encryptionProvider">An instance of IEncryptionProvider used for encryption and decryption operations.</param>
         public EncryptedDateTimeOffsetConverter(IEncryptionProvider encryptionProvider)
             : base(v => encryptionProvider.Encrypt(v.ToString("o")),
                v => DateTimeOffset.Parse(encryptionProvider.Decrypt(v)))
