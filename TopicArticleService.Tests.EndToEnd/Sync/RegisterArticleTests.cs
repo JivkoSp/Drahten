d8b51acd9d0756946d6932db4f1cca1aa172d576
JivkoSp <atjspasov@gmail.com>
2024-07-14 20:43:39 +0300

    Updated the integration tests in TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs

--- TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs
+++ TopicArticleService.Tests.EndToEnd/Sync/RegisterArticleTests.cs
@@ -3,6 +3,7 @@ using Shouldly;
 using System.Net;
 using TopicArticleService.Application.Commands;
 using TopicArticleService.Application.Dtos;
+using TopicArticleService.Presentation.Dtos;
 using TopicArticleService.Tests.EndToEnd.Factories;
 using Xunit;
 
@@ -11,6 +12,16 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
     public class RegisterArticleTests : BaseSyncIntegrationTest
     {
         #region GLOBAL ARRANGE
+
+        //*** IMPORTANT *** - The CreateArticleCommand must have valid TopicId (TopicId for Topic that already exist in the database).
+
+        private CreateArticleCommand PrepareCreateArticleCommand()
+        {
+            var createArticleCommand = new CreateArticleCommand(Guid.NewGuid(), "prev title TEST", "title TEST", "content TEST",
+                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("e0e68a89-8cb2-4602-a10b-2be1a78a9be5"));
+
+            return createArticleCommand;
+        }
 
         public RegisterArticleTests(DrahtenApplicationFactory factory) : base(factory)
         {
@@ -26,11 +37,10 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
         public async Task Register_Article_Endpoint_Should_Return_Http_Status_Code_Created()
         {
             //ARRANGE
-            var command = new CreateArticleCommand(Guid.NewGuid(), "prev title TEST", "title TEST", "content TEST", 
-                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("888a5c96-7c7c-4f98-90b6-91a0c2d401b0"));
+            var command = PrepareCreateArticleCommand();
 
             //ACT
-            var response = await Post(command, "/topic-article-service/articles");
+            var response = await Post(command, "/topic-article-service/articles/");
 
             //ASSERT
             response.ShouldNotBeNull();
@@ -39,7 +49,7 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
 
             response.Headers.Location.ShouldNotBeNull();
 
-            response.Headers.Location.ToString().ShouldBe("/topic-article-service/articles");
+            response.Headers.Location.ToString().ShouldBe("/topic-article-service/articles/");
         }
 
         //Should return http status code 200 when the previously created Article is successfully received from the database.
@@ -47,13 +57,12 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
         public async Task Register_Article_Endpoint_Should_Add_Article_With_Given_ArticleId_To_The_Database()
         {
             //ARRANGE
-            var command = new CreateArticleCommand(Guid.NewGuid(), "prev title TEST", "title TEST", "content TEST",
-                "2022-10-10T14:38:00", "no author", "no link", Guid.Parse("888a5c96-7c7c-4f98-90b6-91a0c2d401b0"));
+            var command = PrepareCreateArticleCommand();
 
             //ACT
-            await Post(command, "/topic-article-service/articles");
+            await Post(command, "/topic-article-service/articles/");
 
-            var response = await Get($"/topic-article-service/articles/{command.ArticleId}");
+            var response = await Get($"/topic-article-service/articles/{command.ArticleId.ToString("N")}");
 
             //ASSERT
             response.ShouldNotBeNull();
@@ -62,12 +71,20 @@ namespace TopicArticleService.Tests.EndToEnd.Sync
 
             var responseSerializedContent = await response.Content.ReadAsStringAsync();
 
-            var articleDto = JsonConvert.DeserializeObject<ArticleDto>(responseSerializedContent);
+            var responseDto = JsonConvert.DeserializeObject<ResponseDto>(responseSerializedContent);
+
+            responseDto.ShouldNotBeNull();
+
+            responseDto.IsSuccess.ShouldBeTrue();
+
+            var articleDto = JsonConvert.DeserializeObject<ArticleDto>(Convert.ToString(responseDto.Result));
+
+            articleDto.ShouldNotBeNull();
 
             //Comparing the values of the command object that is send to the /topic-article-service/articles POST endpoint
             //with the values of the articleDto object that is received by the /topic-article-service/articles/{ArticleId} GET endpoint.
 
-            articleDto.ArticleId.ShouldBe(command.ArticleId.ToString());
+            articleDto.ArticleId.ShouldBe(command.ArticleId.ToString("N"));
 
             articleDto.PrevTitle.ShouldBe(command.PrevTitle);
 
