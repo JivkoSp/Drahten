9e419e3c34ef677736e74ff4a0cbd08315925302
JivkoSp <atjspasov@gmail.com>
2024-07-14 19:34:53 +0300

    Updated the factory TopicArticleService.Tests.EndToEnd/Factories/DrahtenApplicationFactory.cs with RabbitMq container.

--- TopicArticleService.Tests.EndToEnd/Factories/DrahtenApplicationFactory.cs
+++ TopicArticleService.Tests.EndToEnd/Factories/DrahtenApplicationFactory.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
+using Testcontainers.RabbitMq;
+using TopicArticleService.Application.AsyncDataServices;
+using TopicArticleService.Infrastructure.AsyncDataServices;
 using TopicArticleService.Infrastructure.EntityFramework.Contexts;
+using TopicArticleService.Infrastructure.EntityFramework.PrepareDatabase;
 using TopicArticleService.Tests.EndToEnd.Extensions;
+using TopicArticleService.Tests.EndToEnd.Services;
 using Xunit;
 
 namespace TopicArticleService.Tests.EndToEnd.Factories
@@ -19,6 +25,10 @@ namespace TopicArticleService.Tests.EndToEnd.Factories
                 .WithPassword("password")
                 .Build();
 
+        private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5672))
+            .Build();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             base.ConfigureWebHost(builder);
@@ -26,9 +36,9 @@ namespace TopicArticleService.Tests.EndToEnd.Factories
             //Configure dependency injection ONLY for tests that are using this custom factory (e.g DrahtenApplicationFactory).
             builder.ConfigureTestServices(services =>
             {
-                services.RemoveDbContext<ReadDbContext>();
+                services.Remove<DbContextOptions<ReadDbContext>>();
 
-                services.RemoveDbContext<WriteDbContext>();
+                services.Remove<DbContextOptions<WriteDbContext>>();
 
                 services.AddDbContext<ReadDbContext>(options =>
                 {
@@ -39,17 +49,34 @@ namespace TopicArticleService.Tests.EndToEnd.Factories
                 {
                     options.UseNpgsql(_dbContainer.GetConnectionString());
                 });
+
+                services.Remove<IMessageBusPublisher>();
+
+                services.RemoveHostedService<MessageBusSubscriber>();
+
+                services.RemoveHostedService<DbPrepper>();
+
+                services.AddSingleton<IMessageBusPublisher>(sp =>
+                {
+                    var uri = _rabbitMqContainer.GetConnectionString();
+                    return new RabbitMqMessageBusPublisher(uri);
+                });
             });
         }
 
-        public Task InitializeAsync()
+        public async Task InitializeAsync()
         {
-            return _dbContainer.StartAsync();
+            await _dbContainer.StartAsync();
+
+            await _rabbitMqContainer.StartAsync(); 
         }
 
         Task IAsyncLifetime.DisposeAsync()
         {
-            return _dbContainer.StopAsync();
+            return Task.WhenAll(
+                _dbContainer.StopAsync(),
+                _rabbitMqContainer.StopAsync()
+            );
         }
     }
 }
