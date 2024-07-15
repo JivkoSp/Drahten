174d89b9bd297e584c9e5f881bcc54b3059bfac0
JivkoSp <atjspasov@gmail.com>
2024-07-15 23:03:36 +0300

    Added comments in UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs

--- UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
+++ UserService.Infrastructure/EntityFramework/Encryption/EncryptionConverters/EncryptedStringConverter.cs
@@ -3,8 +3,10 @@ using UserService.Infrastructure.EntityFramework.Encryption.EncryptionProvider;
 
 namespace UserService.Infrastructure.EntityFramework.Encryption.EncryptionConverters
 {
+    //Encrypts a string value and then provides the functionality to decrypt it back.
     internal sealed class EncryptedStringConverter : ValueConverter<string, string>
     {
+        /// <param name="encryptionProvider">An instance of IEncryptionProvider used for encryption and decryption operations.</param>
         public EncryptedStringConverter(IEncryptionProvider encryptionProvider)
             : base(v => encryptionProvider.Encrypt(v.ToString()),
                    v => encryptionProvider.Decrypt(v))
