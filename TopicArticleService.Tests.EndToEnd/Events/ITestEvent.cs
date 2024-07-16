e1d8cd77eea3bc1ece5d03675e19ebfecd657823
JivkoSp <atjspasov@gmail.com>
2024-07-16 22:39:10 +0300

    Added custom event type for testing purposes: TopicArticleService.Tests.EndToEnd/Events/ViewedArticleAdded.cs

---
+++ TopicArticleService.Tests.EndToEnd/Events/ITestEvent.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TopicArticleService.Tests.EndToEnd.Events
+{
+    //Marker interface for generic constraint purposes. 
+    public interface ITestEvent
+    {
+    }
+}
---
+++ TopicArticleService.Tests.EndToEnd/Events/ViewedArticleAdded.cs
@@ -0,0 +1,5 @@
+﻿
+namespace TopicArticleService.Tests.EndToEnd.Events
+{
+    public record ViewedArticleAdded(string ArticleId) : ITestEvent;
+}
