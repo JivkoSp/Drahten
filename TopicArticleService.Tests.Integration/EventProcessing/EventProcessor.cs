0c3beb554138ec7d8dd0fca7d0e18682bef9eded
JivkoSp <atjspasov@gmail.com>
2024-07-17 17:47:36 +0300

    Added TopicArticleService.Tests.Integration/EventProcessing/EventProcessor.cs

---
+++ TopicArticleService.Tests.Integration/EventProcessing/EventProcessor.cs
@@ -0,0 +1,97 @@
+﻿using System.Text.Json;
+using TopicArticleService.Application.Dtos.PrivateHistoryService;
+using TopicArticleService.Infrastructure.Dtos;
+using TopicArticleService.Tests.Integration.Events;
+
+namespace TopicArticleService.Tests.Integration.EventProcessing
+{
+    internal enum EventType
+    {
+        ViewedArticle,
+        LikedArticle,
+        DislikedArticle,
+        CommentedArticle,
+        LikedArticleComment,
+        DislikedArticleComment,
+        TopicSubscription,
+        SearchedArticleData,
+        Undetermined
+    }
+
+    internal class EventProcessor : IEventProcessor
+    {
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            var eventType = JsonSerializer.Deserialize<MessageBusEventDto>(notificationMessage);
+
+            switch (eventType.Event)
+            {
+                case "ViewedArticle":
+                    return EventType.ViewedArticle;
+                case "LikedArticle":
+                    return EventType.LikedArticle;
+                case "DislikedArticle":
+                    return EventType.DislikedArticle;
+                case "CommentedArticle":
+                    return EventType.CommentedArticle;
+                case "LikedArticleComment":
+                    return EventType.LikedArticleComment;
+                case "DislikedArticleComment":
+                    return EventType.DislikedArticleComment;
+                case "TopicSubscription":
+                    return EventType.TopicSubscription;
+                case "SearchedArticleData":
+                    return EventType.SearchedArticleData;
+                default:
+                    return EventType.Undetermined;
+            }
+        }
+
+        private void WriteViewedArticle(string message)
+        {
+            var viewedArticleDto = JsonSerializer.Deserialize<ViewedArticleDto>(message);
+
+            IEventProcessor.Events.Add(new ViewedArticleAdded(viewedArticleDto.ArticleId));
+        }
+
+        private void WriteLikedArticle(string message)
+        {
+            var likedArticleDto = JsonSerializer.Deserialize<LikedArticleDto>(message);
+
+            IEventProcessor.Events.Add(new LikedArticleAdded(likedArticleDto.ArticleId));
+        }
+
+        public void ProcessEvent(string message)
+        {
+            var eventType = DetermineEvent(message);
+
+            switch (eventType)
+            {
+                case EventType.ViewedArticle:
+                    WriteViewedArticle(message);
+                    break;
+                case EventType.LikedArticle:
+                    WriteLikedArticle(message);
+                    break;
+                case EventType.DislikedArticle:
+
+                    break;
+                case EventType.CommentedArticle:
+
+                    break;
+                case EventType.LikedArticleComment:
+
+                    break;
+                case EventType.DislikedArticleComment:
+
+                    break;
+                case EventType.TopicSubscription:
+
+                    break;
+                case EventType.SearchedArticleData:
+
+                    break;
+            }
+        }
+    }
+}
