711f10caefe70847be70547e72727597d9945cdd
JivkoSp <atjspasov@gmail.com>
2024-07-17 16:40:34 +0300

    Added postgresql container in TopicArticleService.Tests.Integration/Factories/DrahtenApplicationFactory.cs

--- TopicArticleService.Tests.Integration/Factories/DrahtenApplicationFactory.cs
+++ TopicArticleService.Tests.Integration/Factories/DrahtenApplicationFactory.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
 using Testcontainers.RabbitMq;
 using TopicArticleService.Application.AsyncDataServices;
 using TopicArticleService.Infrastructure.AsyncDataServices;
+using TopicArticleService.Infrastructure.EntityFramework.Contexts;
 using TopicArticleService.Infrastructure.EntityFramework.PrepareDatabase;
 using TopicArticleService.Tests.Integration.Extensions;
 using TopicArticleService.Tests.Integration.Services;
@@ -15,6 +18,13 @@ namespace TopicArticleService.Tests.Integration.Factories
 {
     public sealed class DrahtenApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
+        private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+               .WithImage("postgres:14.2")
+               .WithDatabase("TopicArticleServiceDb")
+               .WithUsername("TopicArticleServiceAdmin")
+               .WithPassword("password")
+               .Build();
+
         private readonly RabbitMqContainer _rabbitMqContainer = new RabbitMqBuilder()
             .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5672))
             .Build();
@@ -26,6 +36,20 @@ namespace TopicArticleService.Tests.Integration.Factories
             //Configure dependency injection ONLY for tests that are using this custom factory (e.g DrahtenApplicationFactory).
             builder.ConfigureTestServices(services =>
             {
+                services.Remove<DbContextOptions<ReadDbContext>>();
+
+                services.Remove<DbContextOptions<WriteDbContext>>();
+
+                services.AddDbContext<ReadDbContext>(options =>
+                {
+                    options.UseNpgsql(_dbContainer.GetConnectionString());
+                });
+
+                services.AddDbContext<WriteDbContext>(options =>
+                {
+                    options.UseNpgsql(_dbContainer.GetConnectionString());
+                });
+
                 services.Remove<IMessageBusPublisher>();
 
                 services.RemoveHostedService<MessageBusSubscriber>();
@@ -51,12 +75,15 @@ namespace TopicArticleService.Tests.Integration.Factories
 
         public async Task InitializeAsync()
         {
+            await _dbContainer.StartAsync();
+
             await _rabbitMqContainer.StartAsync();
         }
 
         Task IAsyncLifetime.DisposeAsync()
         {
             return Task.WhenAll(
+                _dbContainer.StopAsync(),
                 _rabbitMqContainer.StopAsync()
             );
         }
