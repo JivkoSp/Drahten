dd0495e32f60db02f9f02a321f4368ab96d61c6c
JivkoSp <atjspasov@gmail.com>
2024-07-17 12:23:42 +0300

    Added IServiceCollection extension methods: TopicArticleService.Tests.Integration/Extensions/ServiceCollectionExtensions.cs

---
+++ TopicArticleService.Tests.Integration/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TopicArticleService.Tests.Integration.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void Remove<TService>(this IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(x => x.ServiceType == typeof(TService));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+
+        public static void RemoveHostedService<TService>(this IServiceCollection services) where TService : class, IHostedService
+        {
+            var descriptor = services.SingleOrDefault(x =>
+                x.ServiceType == typeof(IHostedService) && x.ImplementationType == typeof(TService));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+    }
+}
