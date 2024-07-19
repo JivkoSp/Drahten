41965302d94a38e7ad5e31b644e5ef655be2072e
JivkoSp <atjspasov@gmail.com>
2024-07-19 15:16:26 +0300

    Added service for capturing console output: TopicArticleService.Tests.Integration/Services/ConsoleOutput.cs

---
+++ TopicArticleService.Tests.Integration/Services/ConsoleOutput.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TopicArticleService.Tests.Integration.Services
+{
+    public class ConsoleOutput : IDisposable
+    {
+        private readonly StringWriter _stringWriter;
+        private readonly TextWriter _originalOutput;
+
+        public ConsoleOutput()
+        {
+            _stringWriter = new StringWriter();
+            _originalOutput = Console.Out;
+            Console.SetOut(_stringWriter);
+        }
+
+        public string GetOutput() => _stringWriter.ToString();
+
+        public void Dispose()
+        {
+            Console.SetOut(_originalOutput);
+            _stringWriter.Dispose();
+        }
+    }
+}
