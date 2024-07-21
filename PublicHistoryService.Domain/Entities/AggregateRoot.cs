79555d2f65f9953fa64674b5f0449d05101d8559
JivkoSp <atjspasov@gmail.com>
2024-07-21 08:32:20 +0300

    Added comments for the ValidateConstructorParameters method in PublicHistoryService.Domain/Entities/AggregateRoot.cs

--- PublicHistoryService.Domain/Entities/AggregateRoot.cs
+++ PublicHistoryService.Domain/Entities/AggregateRoot.cs
@@ -36,11 +36,14 @@ namespace PublicHistoryService.Domain.Entities
             _versionIncremented = true;
         }
 
+        // This method validates constructor parameters to ensure none of them are null.
+        // If any parameter is found to be null, it throws an instance of the specified exception type.
         protected void ValidateConstructorParameters<TException>(object[] parameters)
             where TException : DomainException
         {
             if (parameters.Any(x => x == null))
             {
+                // Create an instance of TException using reflection
                 TException exceptionInstance = Activator.CreateInstance(typeof(TException), true) as TException;
 
                 throw exceptionInstance;
